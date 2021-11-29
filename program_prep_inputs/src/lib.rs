a35c4fa6e767a2a39642a639f8c3e84c8536531c
SwenSchaeferjohann <swen.schaeferjohann@code.berlin>
2021-11-29 14:03:35 +0000

    added onchain test, working

--- program_prep_inputs/src/lib.rs
+++ program_prep_inputs/src/lib.rs
@@ -1,76 +1,44 @@
-use ark_crypto_primitives::crh::TwoToOneCRH;
-use ark_crypto_primitives::merkle_tree::{Config, MerkleTree, Path};
-
-pub mod hard_coded_verifying_key_pvk_new_ciruit;
-pub mod instructions_prepare_inputs;
-pub mod parse_verifyingkey_254;
-pub mod parsers_prepare_inputs;
-pub mod pi_254_implementation;
 pub mod pi_254_instructions;
-pub mod pi_254_test;
-// pub mod pi_381_test;
 pub mod pi_254_parsers;
-pub mod poseidon_instructions;
-pub mod poseidon_parsers;
-pub mod poseidon_processor;
-pub mod pre_processor_prep_inputs;
-pub mod prepare_inputs;
-pub mod processor_prepare_inputs;
-pub mod ranges_prepare_inputs;
-pub mod state_merkle_tree_roots;
-pub mod state_prep_inputs;
-// pub mod verifyingkey_254_bytes;
+pub mod pi_254_processor;
 pub mod pi_254_ranges;
+pub mod pi_254_state;
+pub mod pi_254_test;
+pub mod pre_processor;
+
+pub mod parse_verifyingkey_254;
+pub mod state_merkle_tree_roots;
 pub mod verifyingkey_254_hc;
-
-use crate::pi_254_implementation::*;
-use crate::pi_254_instructions::*;
-
-use crate::pre_processor_prep_inputs::_pre_process_instruction_prep_inputs;
-use crate::state_prep_inputs::PrepareInputsBytes;
+use crate::pre_processor::_pre_process_instruction;
 
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
-    log::sol_log_compute_units,
     msg,
     program_error::ProgramError,
-    program_pack::{IsInitialized, Pack, Sealed},
     pubkey::Pubkey,
 };
 
-use ark_ec;
-use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
-use std::convert::TryInto;
-
-use byteorder::ByteOrder;
-use byteorder::LittleEndian;
-
-// prepare inputs/p
-use crate::parsers_prepare_inputs::*;
-use crate::prepare_inputs::*;
-use crate::processor_prepare_inputs::*;
-
+// _pre_process: Logic happens here. Checks ix_id and routes to the right processors files.
+// TODO: Decide whether to put the pre_process logic inside lib.rs file or not.
+// Especially if we split logic into different programs we don't need a preprocessor.
+// _pi_254_process_instruction: Processor for prepared inputs w/ bn254 curve implementation. Calls ix based on ix_id.
 entrypoint!(process_instruction);
-
-use crate::ranges_prepare_inputs::*;
-use ark_ec::{AffineCurve, PairingEngine, ProjectiveCurve};
-use ark_ff::biginteger::{BigInteger256, BigInteger384};
-use ark_ff::bytes::{FromBytes, ToBytes};
-use ark_ff::{Fp256, Fp384};
 
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
+    msg!("IDATa");
+
     let account = &mut accounts.iter();
     let account1 = next_account_info(account)?; // this one isn't needed
-                                                // the remaining storage accounts are being pulled inside each loop
-    if _instruction_data[1] == 3 {
-        _pre_process_instruction_prep_inputs(_instruction_data, accounts);
-    }
+                                                // the remaining storage accounts are being pulled inside inside pre_process/process
+    msg!("IDATA: {:?}", _instruction_data[0..20].to_vec());
+    // println!("I data: {:?}", _instruction_data);
+    _pre_process_instruction(_instruction_data, accounts);
 
     Ok(())
 }
--- program_prep_inputs/src/pi_254_state.rs
+++ program_prep_inputs/src/pi_254_state.rs
@@ -1,44 +1,17 @@
-// parsers:
-use ark_ff::bytes::{FromBytes, ToBytes};
-use ark_ff::{Fp256, Fp384};
-// use ark_ec;
-use ark_bls12_381;
-use ark_ed_on_bls12_381;
-use ark_ff::fields::models::quadratic_extension::{QuadExtField, QuadExtParameters};
-use num_traits::One;
-
-//lib
-
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint,
-    entrypoint::ProgramResult,
-    log::sol_log_compute_units,
     msg,
     program_error::ProgramError,
     program_pack::{IsInitialized, Pack, Sealed},
-    pubkey::Pubkey,
 };
-
-use ark_ec;
-use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 use std::convert::TryInto;
 
-use byteorder::ByteOrder;
-use byteorder::LittleEndian;
-
-pub const X_1_RANGE_INDEX: usize = 1;
-pub const G_IC_Z_RANGE_INDEX: usize = 13;
-
-// Account struct for prepare_inputs/p
 #[derive(Clone)]
-pub struct PrepareInputsBytes {
+pub struct PiBytes {
     is_initialized: bool,
     pub found_root: u8,
     pub found_nullifier: u8,
     pub executed_withdraw: u8,
-    //adding 3
-    //constants
     pub signing_address: Vec<u8>, // is relayer address
     pub relayer_refund: Vec<u8>,
     pub to_address: Vec<u8>,
@@ -57,9 +30,16 @@ pub struct PrepareInputsBytes {
     pub x_2_range: Vec<u8>,
     pub i_3_range: Vec<u8>,
     pub x_3_range: Vec<u8>,
-
     pub i_4_range: Vec<u8>,
     pub x_4_range: Vec<u8>,
+    // added 6 new ranges
+    pub i_5_range: Vec<u8>,
+    pub x_5_range: Vec<u8>,
+    pub i_6_range: Vec<u8>,
+    pub x_6_range: Vec<u8>,
+    pub i_7_range: Vec<u8>,
+    pub x_7_range: Vec<u8>,
+
     pub res_x_range: Vec<u8>,
     pub res_y_range: Vec<u8>,
     pub res_z_range: Vec<u8>,
@@ -68,21 +48,20 @@ pub struct PrepareInputsBytes {
     pub g_ic_z_range: Vec<u8>,
     pub current_instruction_index: usize,
 
-    pub changed_variables: [bool; 14],
-    pub changed_constants: [bool; 11],
+    pub changed_variables: [bool; 20],
+    pub changed_constants: [bool; 17],
 }
-
-impl Sealed for PrepareInputsBytes {}
-impl IsInitialized for PrepareInputsBytes {
+impl Sealed for PiBytes {}
+impl IsInitialized for PiBytes {
     fn is_initialized(&self) -> bool {
         self.is_initialized
     }
 }
-impl Pack for PrepareInputsBytes {
+impl Pack for PiBytes {
     const LEN: usize = 4972; // 1020
 
     fn unpack_from_slice(input: &[u8]) -> Result<Self, ProgramError> {
-        let input = array_ref![input, 0, PrepareInputsBytes::LEN];
+        let input = array_ref![input, 0, PiBytes::LEN];
 
         let (
             is_initialized,
@@ -106,21 +85,28 @@ impl Pack for PrepareInputsBytes {
             x_3_range,
             i_4_range,
             x_4_range,
+            i_5_range,
+            x_5_range,
+            i_6_range,
+            x_6_range,
+            i_7_range,
+            x_7_range,
             res_x_range,
             res_y_range,
             res_z_range,
             g_ic_x_range,
             g_ic_y_range,
-            g_ic_z_range, // 144b 3*48
-            //until here 1020 bytes
+            g_ic_z_range, // 144b 3*48 // actually now 3*32
+            //until here 1020 bytes // actuall 288 more now so 1308 bytes
             unused_remainder,
         ) = array_refs![
-            input, 1, 1, 1, 1, 32, 8, 32, 8, 32, 32, 32, 32, 8, 32, 96, 32, 96, 32, 96, 32, 96, 48,
-            48, 48, 48, 48, 48, 3952
+            input, 1, 1, 1, 1, 32, 8, 32, 8, 32, 32, 32, 32, 8, 32, 64, 32, 64, 32, 64, 32, 64, 32,
+            64, 32, 64, 32, 64, 32, 32, 32, 32, 32, 32,   //  48, 48, 48, 48, 48, 48, replaced
+            3888 // 3792 was without the last 6 change down  // 3952 {128 less (1-4) and 288 more (5-7)}
         ];
         msg!("unpacked");
 
-        Ok(PrepareInputsBytes {
+        Ok(PiBytes {
             is_initialized: true,
 
             found_root: found_root[0],                     //0
@@ -144,19 +130,25 @@ impl Pack for PrepareInputsBytes {
             x_3_range: x_3_range.to_vec(),       //5
             i_4_range: i_4_range.to_vec(),       //6
             x_4_range: x_4_range.to_vec(),       //7
-            res_x_range: res_x_range.to_vec(),   //8
-            res_y_range: res_y_range.to_vec(),   //9
-            res_z_range: res_z_range.to_vec(),   //10
-            g_ic_x_range: g_ic_x_range.to_vec(), //11
-            g_ic_y_range: g_ic_y_range.to_vec(), //12
-            g_ic_z_range: g_ic_z_range.to_vec(), //13
-            changed_variables: [false; 14],
-            changed_constants: [false; 11],
+            i_5_range: i_5_range.to_vec(),       //8
+            x_5_range: x_5_range.to_vec(),       //9
+            i_6_range: i_6_range.to_vec(),       //10
+            x_6_range: x_6_range.to_vec(),       //11
+            i_7_range: i_7_range.to_vec(),       //12
+            x_7_range: x_7_range.to_vec(),       //13
+            res_x_range: res_x_range.to_vec(),   //14
+            res_y_range: res_y_range.to_vec(),   //15
+            res_z_range: res_z_range.to_vec(),   //16
+            g_ic_x_range: g_ic_x_range.to_vec(), //17
+            g_ic_y_range: g_ic_y_range.to_vec(), //18
+            g_ic_z_range: g_ic_z_range.to_vec(), //19
+            changed_variables: [false; 20],
+            changed_constants: [false; 17],
         })
     }
 
     fn pack_into_slice(&self, dst: &mut [u8]) {
-        let dst = array_mut_ref![dst, 0, PrepareInputsBytes::LEN];
+        let dst = array_mut_ref![dst, 0, PiBytes::LEN];
 
         let (
             //constants
@@ -183,6 +175,12 @@ impl Pack for PrepareInputsBytes {
             x_3_range_dst,
             i_4_range_dst,
             x_4_range_dst,
+            i_5_range_dst,
+            x_5_range_dst,
+            i_6_range_dst,
+            x_6_range_dst,
+            i_7_range_dst,
+            x_7_range_dst,
             res_x_range_dst,
             res_y_range_dst,
             res_z_range_dst,
@@ -191,8 +189,11 @@ impl Pack for PrepareInputsBytes {
             g_ic_z_range_dst,
             unused_remainder_dst,
         ) = mut_array_refs![
-            dst, 1, 1, 1, 1, 32, 8, 32, 8, 32, 32, 32, 32, 8, 32, 96, 32, 96, 32, 96, 32, 96, 48,
-            48, 48, 48, 48, 48, 3952
+            dst, 1, 1, 1, 1, 32, 8, 32, 8, 32, 32, 32, 32, 8, 32, 64, 32, 64, 32, 64, 32, 64, 32,
+            64, 32, 64, 32, 64, 32, 32, 32, 32, 32, 32, //  48, 48, 48, 48, 48, 48, replaced
+            3888 // 3792 was without the last 6 change down  // 3952 {128 less (1-4) and 288 more (5-7)}
+                  //dst, 1, 1, 1, 1, 32, 8, 32, 8, 32, 32, 32, 32, 8, 32, 96, 32, 96, 32, 96, 32, 96, 48,
+                  //  48, 48, 48, 48, 48, 3952
         ];
 
         for (i, var_has_changed) in self.changed_variables.iter().enumerate() {
@@ -214,16 +215,28 @@ impl Pack for PrepareInputsBytes {
                 } else if i == 7 {
                     *x_4_range_dst = self.x_4_range.clone().try_into().unwrap();
                 } else if i == 8 {
-                    *res_x_range_dst = self.res_x_range.clone().try_into().unwrap();
+                    *i_5_range_dst = self.i_5_range.clone().try_into().unwrap();
                 } else if i == 9 {
-                    *res_y_range_dst = self.res_y_range.clone().try_into().unwrap();
+                    *x_5_range_dst = self.x_5_range.clone().try_into().unwrap();
                 } else if i == 10 {
-                    *res_z_range_dst = self.res_z_range.clone().try_into().unwrap();
+                    *i_6_range_dst = self.i_6_range.clone().try_into().unwrap();
                 } else if i == 11 {
-                    *g_ic_x_range_dst = self.g_ic_x_range.clone().try_into().unwrap();
+                    *x_6_range_dst = self.x_6_range.clone().try_into().unwrap();
                 } else if i == 12 {
-                    *g_ic_y_range_dst = self.g_ic_y_range.clone().try_into().unwrap();
+                    *i_7_range_dst = self.i_7_range.clone().try_into().unwrap();
                 } else if i == 13 {
+                    *x_7_range_dst = self.x_7_range.clone().try_into().unwrap();
+                } else if i == 14 {
+                    *res_x_range_dst = self.res_x_range.clone().try_into().unwrap();
+                } else if i == 15 {
+                    *res_y_range_dst = self.res_y_range.clone().try_into().unwrap();
+                } else if i == 16 {
+                    *res_z_range_dst = self.res_z_range.clone().try_into().unwrap();
+                } else if i == 17 {
+                    *g_ic_x_range_dst = self.g_ic_x_range.clone().try_into().unwrap();
+                } else if i == 18 {
+                    *g_ic_y_range_dst = self.g_ic_y_range.clone().try_into().unwrap();
+                } else if i == 19 {
                     *g_ic_z_range_dst = self.g_ic_z_range.clone().try_into().unwrap();
                 }
             } else {
@@ -244,16 +257,28 @@ impl Pack for PrepareInputsBytes {
                 } else if i == 7 {
                     *x_4_range_dst = *x_4_range_dst;
                 } else if i == 8 {
-                    *res_x_range_dst = *res_x_range_dst;
+                    *i_5_range_dst = *i_5_range_dst;
                 } else if i == 9 {
-                    *res_y_range_dst = *res_y_range_dst;
+                    *x_5_range_dst = *x_5_range_dst;
                 } else if i == 10 {
-                    *res_z_range_dst = *res_z_range_dst;
+                    *i_6_range_dst = *i_6_range_dst;
                 } else if i == 11 {
-                    *g_ic_x_range_dst = *g_ic_x_range_dst;
+                    *x_6_range_dst = *x_6_range_dst;
                 } else if i == 12 {
-                    *g_ic_y_range_dst = *g_ic_y_range_dst;
+                    *i_7_range_dst = *i_7_range_dst;
                 } else if i == 13 {
+                    *x_7_range_dst = *x_7_range_dst;
+                } else if i == 14 {
+                    *res_x_range_dst = *res_x_range_dst;
+                } else if i == 15 {
+                    *res_y_range_dst = *res_y_range_dst;
+                } else if i == 16 {
+                    *res_z_range_dst = *res_z_range_dst;
+                } else if i == 17 {
+                    *g_ic_x_range_dst = *g_ic_x_range_dst;
+                } else if i == 18 {
+                    *g_ic_y_range_dst = *g_ic_y_range_dst;
+                } else if i == 19 {
                     *g_ic_z_range_dst = *g_ic_z_range_dst;
                 }
             };
@@ -314,118 +339,5 @@ impl Pack for PrepareInputsBytes {
         *current_instruction_index_dst = usize::to_le_bytes(self.current_instruction_index);
         *is_initialized_dst = [1u8; 1];
         *unused_remainder_dst = *unused_remainder_dst;
-    }
-}
-
-// Account structs for merkle tree:
-#[derive(Debug)]
-pub struct PoseidonHashBytesPrepInputs {
-    pub is_initialized: bool,
-    pub state_range_1: Vec<u8>,
-    pub state_range_2: Vec<u8>,
-    pub state_range_3: Vec<u8>,
-    pub fp256_0: Vec<u8>,
-    pub fp256_1: Vec<u8>,
-    // the following 4 are just read not written
-    pub signing_address: Vec<u8>, // is relayer address
-    pub relayer_refund: [u8; 8],
-    pub to_address: Vec<u8>,
-    pub amount: [u8; 8],
-
-    pub tx_integrity_hash: Vec<u8>,
-
-    pub current_instruction_index: usize,
-}
-impl Sealed for PoseidonHashBytesPrepInputs {}
-impl IsInitialized for PoseidonHashBytesPrepInputs {
-    fn is_initialized(&self) -> bool {
-        self.is_initialized
-    }
-}
-impl Pack for PoseidonHashBytesPrepInputs {
-    const LEN: usize = 4972;
-    fn unpack_from_slice(input: &[u8]) -> Result<Self, ProgramError> {
-        let input = array_ref![input, 0, PoseidonHashBytesPrepInputs::LEN];
-
-        let (
-            is_initialized,
-            unused_first,
-            signing_address, // is relayer address
-            relayer_refund,
-            to_address,
-            amount,
-            unused_second,
-            tx_integrity_hash,
-            current_instruction_index,
-            //220
-            unused_third,
-            fp256_0,
-            fp256_1,
-            state_range_1,
-            state_range_2,
-            state_range_3,
-        ) = array_refs![input, 1, 3, 32, 8, 32, 8, 96, 32, 8, 4592, 32, 32, 32, 32, 32];
-
-        assert_eq!(1u8, is_initialized[0]);
-        Ok(PoseidonHashBytesPrepInputs {
-            is_initialized: true,
-            state_range_1: state_range_1.to_vec(),
-            state_range_2: state_range_2.to_vec(),
-            state_range_3: state_range_3.to_vec(),
-            fp256_0: fp256_0.to_vec(),
-            fp256_1: fp256_1.to_vec(),
-            signing_address: signing_address.to_vec(), //3
-            relayer_refund: *relayer_refund,           //4
-            to_address: to_address.to_vec(),           //5
-            amount: *amount,                           //6
-            tx_integrity_hash: tx_integrity_hash.to_vec(),
-            current_instruction_index: usize::from_le_bytes(*current_instruction_index),
-        })
-    }
-
-    fn pack_into_slice(&self, dst: &mut [u8]) {
-        let dst = array_mut_ref![dst, 0, PoseidonHashBytesPrepInputs::LEN];
-        let (
-            is_initialized_dst,
-            unused_first_dst,
-            tx_integrity_hash_dst,
-            current_instruction_index_dst,
-            //219
-            unused_third_dst,
-            fp256_0_dst,
-            fp256_1_dst,
-            state_range_1_dst,
-            state_range_2_dst,
-            state_range_3_dst,
-        ) = mut_array_refs![dst, 1, 179, 32, 8, 4592, 32, 32, 32, 32, 32];
-
-        /*
-        let (
-            is_initialized_dst,
-            unused_first_dst,
-            result_dst,
-            current_instruction_index_dst,
-            unused_second_dst,
-            state_range_1_dst,
-            state_range_2_dst,
-            state_range_3_dst,
-        ) = mut_array_refs![dst,1, 146, 32, 8, 4478, 32, 32, 32];
-        */
-        *fp256_0_dst = self.fp256_0.clone().try_into().unwrap();
-        //assert_eq!(*state_range_1_dst.to_vec(), self.state_range_1);
-        *fp256_1_dst = self.fp256_1.clone().try_into().unwrap();
-
-        *state_range_1_dst = self.state_range_1.clone().try_into().unwrap();
-        //assert_eq!(*state_range_1_dst.to_vec(), self.state_range_1);
-        *state_range_2_dst = self.state_range_2.clone().try_into().unwrap();
-        //assert_eq!(*state_range_2_dst.to_vec(), self.state_range_2);
-        *state_range_3_dst = self.state_range_3.clone().try_into().unwrap();
-        //assert_eq!(*state_range_3_dst.to_vec(), self.state_range_3);
-        *tx_integrity_hash_dst = self.tx_integrity_hash.clone().try_into().unwrap();
-        msg!("packing {}", self.current_instruction_index);
-        *current_instruction_index_dst = usize::to_le_bytes(self.current_instruction_index);
-        *unused_first_dst = *unused_first_dst;
-        *unused_third_dst = *unused_third_dst;
-        *is_initialized_dst = *is_initialized_dst;
     }
 }
