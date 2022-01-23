9e1323bae9ea333d2991481dbaf061effa5e8b26
ananas-block <flow23@protonmail.com>
2022-01-23 23:20:02 +0000

    cleanup instructions.rs fe

--- program/src/groth16_verifier/final_exponentiation/instructions.rs
+++ program/src/groth16_verifier/final_exponentiation/instructions.rs
@@ -66,15 +66,10 @@ pub fn mul_assign_1(
     _store_cubic: &mut Vec<u8>,
     _store_cubic_range: [usize; 2],
 ) {
-    msg!("mul_assign_1: ------------------------------------------");
-
     let f_c0 = parse_cubic_from_bytes_sub(&_cubic, _cubic_0_range); //30000
-                                                                    //println!("str f_c0 = {:?}", f_c0);
     let other_c0 = parse_cubic_from_bytes_sub(&_cubic_other, _range_other_c0); //30000
-                                                                               //println!("str other_c0 = {:?}", other_c0);
 
     let v0 = f_c0 * &other_c0; //   cost 86548
-                               //println!("str v0 = {:?}", v0);
     parse_cubic_to_bytes_sub(v0, _store_cubic, _store_cubic_range); //15000
                                                                     //total 162000
 }
@@ -92,7 +87,6 @@ pub fn mul_assign_2(
     let other_c1 = parse_cubic_from_bytes_sub(&_cubic_other, range_other_c1); //30000
 
     let v1 = f_c1 * &other_c1; //86827
-                               //println!("v1 = {:?}", v1);
     parse_cubic_to_bytes_sub(v1, _store_cubic, _store_cubic_range); //15000
                                                                     //total 193000
 }
@@ -103,16 +97,10 @@ pub fn mul_assign_1_2(
     _store_cubic0: &mut Vec<u8>,
     _store_cubic1: &mut Vec<u8>,
 ) {
-    //msg!("mul_assign_1_2: ------------------------------------------");
-
     let f = parse_f_from_bytes(&_f_range);
     let other = parse_f_from_bytes(&_other);
 
     let v0 = f.c0 * &other.c0;
-
-    //let mut f_c1 = parse_cubic_from_bytes_sub(&_cubic1, _cubic_1_range1);
-    //let other_c1 = parse_cubic_from_bytes_sub(&_cubic_other1, range_other_c1);
-
     let v1 = f.c1 * &other.c1;
 
     parse_cubic_to_bytes_sub(v0, _store_cubic0, SOLO_CUBIC_0_RANGE); //15000
@@ -121,7 +109,6 @@ pub fn mul_assign_1_2(
 
 //52545 - 15 = 37
 pub fn mul_assign_3(_f_range: &mut Vec<u8>) {
-    //msg!("mul_assign_3: ------------------------------------------");
 
     let mut f = parse_f_from_bytes(&_f_range); //60000
     f.c1 += &f.c0; //<1000
@@ -135,18 +122,13 @@ pub fn mul_assign_3_4_5(
     _cubic_range_1: &Vec<u8>,
     _f_range: &mut Vec<u8>,
 ) {
-    //msg!("mul_assign_3_4_5: ------------------------------------------");
-
     //3
     let mut f = parse_f_from_bytes(&_f_range);
     f.c1 += &f.c0;
 
-    //4
     let other = parse_f_from_bytes(&_f_range_other);
-
     f.c1 *= &(other.c0 + &other.c1);
 
-    //5
     let v0 = parse_cubic_from_bytes_sub(&_cubic_range_0, SOLO_CUBIC_0_RANGE); //30000
     let v1 = parse_cubic_from_bytes_sub(&_cubic_range_1, SOLO_CUBIC_0_RANGE); //30000
     f.c1 -= &v0;
@@ -157,16 +139,8 @@ pub fn mul_assign_3_4_5(
 }
 //45031 - 8k = 37
 pub fn mul_assign_4_1(_f_range_other: &Vec<u8>, _store_cubic_range: &mut Vec<u8>) {
-    msg!("mul_assign_4_1: ------------------------------------------");
 
-    //let mut f = parse_f_from_bytes(&account[..], range_f);
-    //let mut f_c1 = parse_cubic_from_bytes_sub(&account[..], _f1_r_cubic_1_range); //30000
     let other = parse_f_from_bytes(&_f_range_other); //60000
-                                                     //println!("str Other res : {:?}", (other.c0 + other.c1));
-
-    //f.c1 += &f.c0;
-    //
-    //////msg!"4");
     let tmp = &(other.c0 + &other.c1); //87075
     parse_cubic_to_bytes_sub(*tmp, _store_cubic_range, SOLO_CUBIC_0_RANGE); //15000
                                                                             //195000
@@ -178,31 +152,23 @@ pub fn mul_assign_4_2(
     _f1_r_cubic_1_range: [usize; 2],
     _store_cubic_range: &Vec<u8>,
 ) {
-    msg!("mul_assign_4_2: ------------------------------------------");
 
     //let mut f = parse_f_from_bytes(&account[..], range_f);
     let mut f_c1 = parse_cubic_from_bytes_sub(&_f1_r_range, _f1_r_cubic_1_range); //30000
     let other_cs = parse_cubic_from_bytes_sub(&_store_cubic_range, SOLO_CUBIC_0_RANGE); //60000
-                                                                                        //println!("str loaded other res : {:?}", other_cs);
-
-    //f.c1 += &f.c0;
     //if these logs are removed a stackoverflow might occur in the overall program
     sol_log_compute_units();
-    //msg!("mulassing 4_2 0");
     f_c1 *= other_cs;
     sol_log_compute_units();
     //87075
     parse_cubic_to_bytes_sub(f_c1, _f1_r_range, _f1_r_cubic_1_range);
-    //msg!("mulassing 4_2 1");
     sol_log_compute_units();
 
-    //assert_eq!(f_c1, parse_cubic_from_bytes_sub(&_f1_r_range, _f1_r_cubic_1_range));           //15000
     //195000
 }
 
 //81001 - 27
 pub fn mul_assign_5(_f1_r_range: &mut Vec<u8>, _cubic_range_0: &Vec<u8>, _cubic_range_1: &Vec<u8>) {
-    msg!("mul_assign_5: ------------------------------------------");
 
     //--------------- additional split -------------------------
     let mut f = parse_f_from_bytes(&_f1_r_range); //60000
@@ -222,7 +188,6 @@ pub fn mul_assign_5(_f1_r_range: &mut Vec<u8>, _cubic_range_0: &Vec<u8>, _cubic_
                                       //153000
 }
 
-//frobenius_map should work only wrapper
 pub fn custom_frobenius_map_1(account: &mut Vec<u8>) {
     let mut f = parse_f_from_bytes(&account);
     f.frobenius_map(1);
@@ -241,17 +206,7 @@ pub fn custom_frobenius_map_3(account: &mut Vec<u8>) {
     parse_f_to_bytes(f, account);
 }
 
-/*exp_by_neg_x is cyclotomic_exp plus possible conjugate
-* cyclotomic_exp is:
-* - conjugate
-* - loop
-*   - if naf non-zero -> cyclotomic_square_in_place
-*       - only enters if characteristic_square_mod_6_is_one
-*   - if part with mul_assign
-*/
-
 pub fn custom_cyclotomic_square_in_place(_store_f_range: &mut Vec<u8>) {
-    msg!("custom_cyclotomic_square_in_place: ------------------------------------------ ");
 
     let mut f = parse_f_from_bytes(&_store_f_range);
     // cost 46000
@@ -259,22 +214,17 @@ pub fn custom_cyclotomic_square_in_place(_store_f_range: &mut Vec<u8>) {
 
     parse_f_to_bytes(f, _store_f_range);
 }
-//cyclotomic_square should work only wrapper
 
 pub fn custom_cyclotomic_square(_f_range: &Vec<u8>, _store_f_range: &mut Vec<u8>) {
-    msg!("custom_cyclotomic_square: ------------------------------------------ ");
     let f = parse_f_from_bytes(&_f_range);
     // cost 90464
     let y0 = f.cyclotomic_square();
-    //assert!(f != y0);
 
     parse_f_to_bytes(y0, _store_f_range);
 }
 
-//inverse works
 pub fn custom_f_inverse_1(_f_f2_range: &Vec<u8>, _cubic_range_1: &mut Vec<u8>) {
     //first part of calculating the inverse to f
-    msg!("custom_f_inverse_1: ------------------------------------------ ");
 
     let f = parse_f_from_bytes(_f_f2_range);
     let v1 = f.c1.square();
@@ -286,7 +236,6 @@ pub fn custom_f_inverse_2(
     _cubic_range_0: &mut Vec<u8>,
     _cubic_range_1: &Vec<u8>,
 ) {
-    msg!("custom_f_inverse_2: ------------------------------------------ ");
 
     let f = parse_f_from_bytes(_f_f2_range);
     let v1 = parse_cubic_from_bytes_sub(_cubic_range_1, SOLO_CUBIC_0_RANGE);
@@ -300,19 +249,15 @@ pub fn custom_f_inverse_3(
     _cubic_range_0: &Vec<u8>,
     _f_f2_range: &Vec<u8>,
 ) {
-    msg!("custom_f_inverse_3: ------------------------------------------ ");
-
     let v1 = parse_cubic_from_bytes_sub(_cubic_range_0, SOLO_CUBIC_0_RANGE);
     let f_c0 = parse_cubic_from_bytes_sub(_f_f2_range, F_CUBIC_0_RANGE);
 
     let c0 = f_c0 * &v1; //      cost 87545
 
     parse_cubic_to_bytes_sub(c0, _cubic_range_1, SOLO_CUBIC_0_RANGE);
-    //assert_eq!(c0, parse_cubic_from_bytes_sub(account, cubic_range_1));
 }
 
 pub fn custom_f_inverse_4(_cubic: &mut Vec<u8>, _f_f2_range: &Vec<u8>) {
-    msg!("custom_f_inverse_4: ------------------------------------------ ");
 
     let v1 = parse_cubic_from_bytes_sub(_cubic, SOLO_CUBIC_0_RANGE); //30
     let f_c1 = parse_cubic_from_bytes_sub(_f_f2_range, F_CUBIC_1_RANGE); //30
@@ -321,7 +266,6 @@ pub fn custom_f_inverse_4(_cubic: &mut Vec<u8>, _f_f2_range: &Vec<u8>) {
 }
 
 pub fn custom_f_inverse_5(_cubic_0: &Vec<u8>, _cubic_1: &Vec<u8>, _f_f2_range: &mut Vec<u8>) {
-    msg!("custom_f_inverse_5: ------------------------------------------ ");
 
     let c0 = parse_cubic_from_bytes_sub(_cubic_1, SOLO_CUBIC_0_RANGE); //30
     let c1 = parse_cubic_from_bytes_sub(_cubic_0, SOLO_CUBIC_0_RANGE); //30
@@ -338,58 +282,33 @@ pub fn custom_cubic_inverse_1(
     _quad_range_2: &mut Vec<u8>,
     _quad_range_3: &mut Vec<u8>,
 ) {
-    msg!("custom_cubic_inverse_1: ------------------------------------------ ");
 
     let f = parse_cubic_from_bytes_sub(_cubic_range_0, SOLO_CUBIC_0_RANGE);
     // From "High-Speed Software Implementation of the Optimal Ate AbstractPairing
-    // over
     // Barreto-Naehrig Curves"; Algorithm 17
-    ////msg!"cubic inverse ");
-    ////sol_log_compute_units();
-    ////msg!"t0 ");
+
     let t0 = f.c0.square(); //  cost 9188
-                            ////sol_log_compute_units();
-                            ////msg!"t1 ");
     let t1 = f.c1.square(); //  cost 9255 cumulative 18443
-                            ////sol_log_compute_units();
-                            ////msg!"t2 ");
     let t2 = f.c2.square(); //   cost 9255 cumulative 27698
-                            ////sol_log_compute_units();
-                            ////msg!"t3 ");
     let t3 = f.c0 * &f.c1; //    cost 13790 cumulative 41488
-                           ////sol_log_compute_units();
-                           ////msg!"t4 ");
     let t4 = f.c0 * &f.c2; //    cost 13789 cumulative 55277
-                           ////sol_log_compute_units();
-                           ////msg!"t5 ");
+
     let t5 = f.c1 * &f.c2; //    cost 13791 cumulative 69068
-                           ////sol_log_compute_units();
-                           ////msg!"n5 ");
-                           //    cost 270 cumulative 69350
+
     let n5 = <ark_ff::fields::models::fp6_3over2::Fp6ParamsWrapper::<ark_bn254::Fq6Parameters>  as CubicExtParameters>::mul_base_field_by_nonresidue(&t5);
-    ////sol_log_compute_units();
-    ////msg!"s0");
+
     let s0 = t0 - &n5; //      cost 255 cumulative 69593
-                       ////sol_log_compute_units();
-                       ////msg!"s1 ");
-                       //    cost 505 cumulative 70098
+
     let s1 = <ark_ff::fields::models::fp6_3over2::Fp6ParamsWrapper::<ark_bn254::Fq6Parameters>  as CubicExtParameters>::mul_base_field_by_nonresidue(&t2) - &t3;
-    ////sol_log_compute_units();
-    ////msg!"s2 ");        //      cost 265 cumulative 70363
+
     let s2 = t1 - &t4; // typo in paper referenced above. should be "-" as per Scott, but is "*"
 
-    ////sol_log_compute_units();
-    ////msg!"a1 ");
     let a1 = f.c2 * &s1; //     cost 13791 cumulative 84154
-                         ////sol_log_compute_units();
-                         ////msg!"a2 ");
+
     let a2 = f.c1 * &s2; //      cost 13791 cumulative 97945
-                         ////sol_log_compute_units();
-                         ////msg!"a3 ");
+
     let mut a3 = a1 + &a2; //    cost 182 cumulative 98127
-                           ////sol_log_compute_units();
-                           ////msg!"a3.1 ");
-                           // cost 297 cumulative 98424
+
     a3 = <ark_ff::fields::models::fp6_3over2::Fp6ParamsWrapper::<ark_bn254::Fq6Parameters>  as CubicExtParameters>::mul_base_field_by_nonresidue(&a3);
 
     //13895
@@ -410,20 +329,15 @@ pub fn custom_cubic_inverse_2(
     _quad_range_2: &Vec<u8>,
     _quad_range_3: &Vec<u8>,
 ) {
-    msg!("custom_cubic_inverse_2: ------------------------------------------ ");
-
     let t6 = parse_quad_from_bytes(_quad_range_3);
     let s0 = parse_quad_from_bytes(_quad_range_0);
     let s1 = parse_quad_from_bytes(_quad_range_1);
     let s2 = parse_quad_from_bytes(_quad_range_2);
     let c0 = t6 * &s0; //      cost 13698
-                       ////sol_log_compute_units();
-                       ////msg!"c1 ");
+
     let c1 = t6 * &s1; //      cost 13790
-                       ////sol_log_compute_units();
-                       ////msg!"c2 ");
+
     let c2 = t6 * &s2; //      cost 13790
-                       ////sol_log_compute_units();
     parse_cubic_to_bytes_sub(
         ark_ff::fields::models::cubic_extension::CubicExtField::<
             ark_ff::fields::models::fp6_3over2::Fp6ParamsWrapper<ark_bn254::Fq6Parameters>,
@@ -438,7 +352,6 @@ pub fn custom_quadratic_fp256_inverse_1(_quad_range_3: &Vec<u8>, _fp384_range: &
     //returns none in arkworks maybe we should add a byte to signal a crash
     //} else {
     // Guide to Pairing-based Cryptography, Algorithm 5.19.
-    msg!("custom_quadratic_fp256_inverse_1: ------------------------------------------ ");
     let f = parse_quad_from_bytes(_quad_range_3);
     let v1 = f.c1.square(); //      cost 3659
 
@@ -449,7 +362,6 @@ pub fn custom_quadratic_fp256_inverse_1(_quad_range_3: &Vec<u8>, _fp384_range: &
 }
 
 pub fn custom_quadratic_fp256_inverse_2(_quad_range_3: &mut Vec<u8>, _fp384_range: &Vec<u8>) {
-    msg!("custom_quadratic_fp256_inverse_2: ------------------------------------------ ");
 
     let v0 = parse_fp256_from_bytes(_fp384_range);
     let f = parse_quad_from_bytes(_quad_range_3);
@@ -459,65 +371,13 @@ pub fn custom_quadratic_fp256_inverse_2(_quad_range_3: &mut Vec<u8>, _fp384_rang
         //   cost 184974
 
         let c0 = f.c0 * &v1; //      cost 4367
-                             ////sol_log_compute_units();
-                             ////msg!"c1");
+
         let c1 = -(f.c1 * &v1); //   cost 4471
-                                ////sol_log_compute_units();
 
         let res = QuadExtField::<ark_ff::Fp2ParamsWrapper<ark_bn254::Fq2Parameters>>::new(c0, c1);
         parse_quad_to_bytes(res, _quad_range_3);
     });
 }
-
-/*
-pub fn verify_result_and_withdraw(_f1_r_range: &Vec<u8>, account_from: &AccountInfo, account_to: &AccountInfo) {
-
-    //let verifyingkey = get_alpha_g1_beta_g2(); // new verif key val
-    let result = parse_f_from_bytes(_f1_r_range);
-
-
-    assert_eq!(
-        result,
-        QuadExtField::<ark_ff::Fp12ParamsWrapper::<ark_bls12_381::Fq12Parameters>>::new(
-        CubicExtField::<ark_ff::Fp6ParamsWrapper::<ark_bls12_381::Fq6Parameters>>::new(
-        QuadExtField::<ark_ff::Fp2ParamsWrapper::<ark_bls12_381::Fq2Parameters>>::new(
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([9450009823341693778, 7567869168370272456, 18197991852398659280, 14366248363560943150, 1846449964006099810, 679186062217654437])),
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([14423346876892333681, 6730281363705116644, 7424630442455670057, 8270520680913369906, 129074939192573571, 1189318454514002421]))
-        ),
-        QuadExtField::<ark_ff::Fp2ParamsWrapper::<ark_bls12_381::Fq2Parameters>>::new(
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([15087111543975820103, 4158933278356405436, 18118066973472597158, 16470418406097340038, 7290763036760663413, 1432986065586497656])),
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([13142509635494830254, 1063120006913468487, 9502319634834262306, 4432211635827318749, 14796654634504989696, 1613239985088916413]))
-        ),
-        QuadExtField::<ark_ff::Fp2ParamsWrapper::<ark_bls12_381::Fq2Parameters>>::new(
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([8540514875150680179, 14211944784949309652, 11503029521577459949, 7848771183824012788, 12423583728904914117, 214651426791184375])),
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([12184631478503290601, 3754734004613789564, 18017661725911600748, 2523454429955899187, 477613221123976217, 1872837268341302997]))
-        )
-        ),
-        CubicExtField::<ark_ff::Fp6ParamsWrapper::<ark_bls12_381::Fq6Parameters>>::new(
-        QuadExtField::<ark_ff::Fp2ParamsWrapper::<ark_bls12_381::Fq2Parameters>>::new(
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([3978527081928063346, 16585342381569337288, 5542417872902215541, 14433809004088446654, 4002383117313680726, 1825637594027485805])),
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([12282572081966365605, 4130176373697380094, 11274978495552272244, 12480637929685418438, 8247324291841073554, 1292512561380557691]))
-        ),
-        QuadExtField::<ark_ff::Fp2ParamsWrapper::<ark_bls12_381::Fq2Parameters>>::new(
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([14568481364640261896, 13024973709310556877, 388942695468569842, 5165814615778255344, 5461422625881874726, 998729394980441256])),
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([2915405947247214443, 16476827183007826276, 397625141714225917, 7683936378748485792, 7647979677775928833, 1072503992333817859]))
-        ),
-        QuadExtField::<ark_ff::Fp2ParamsWrapper::<ark_bls12_381::Fq2Parameters>>::new(
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([16050027345311255108, 10820284375719774599, 7851447608520829924, 16313935448547715969, 6657343977525303700, 224196233976581852])),
-        Fp384::<ark_bls12_381::FqParameters>::new(BigInteger384::new([16673765787484129697, 12992430675039131019, 2485465101194175871, 15715616086842934831, 1003336842210694614, 1058233574352048271]))
-    ))), "VERIFICATION FAILED"
-    );
-
-
-    **account_from.try_borrow_mut_lamports().unwrap()   -= 1000000000; // 1 SOL
-
-    **account_to.try_borrow_mut_lamports().unwrap()     += 1000000000;
-    msg!("Final Exp successful");
-
-    msg!("Transfer of 1 Sol successful to {:?}", account_to.key);
-
-}
-*/
 
 #[cfg(test)]
 mod tests {
