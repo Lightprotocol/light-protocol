82a2dbc53cd49a9a836d627ee5136edea8029eb6
ananas-block <flow23@protonmail.com>
2022-01-29 15:33:14 +0000

    refactored fees

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1,8 +1,7 @@
-use crate::instructions::{check_and_insert_nullifier, create_and_check_account, token_transfer};
+use crate::instructions::{check_and_insert_nullifier, create_and_check_account, token_transfer, check_external_amount};
 use crate::poseidon_merkle_tree::processor::MerkleTreeProcessor;
 use crate::poseidon_merkle_tree::state_roots::{check_root_hash_exists, MERKLE_TREE_ACC_BYTES};
 use crate::state::ChecksAndTransferState;
-use std::convert::{TryFrom, TryInto};
 
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
@@ -11,9 +10,7 @@ use solana_program::{
     program_pack::Pack,
     pubkey::Pubkey,
 };
-
-use ark_ed_on_bn254::FqParameters;
-use ark_ff::{biginteger::BigInteger256, bytes::FromBytes, fields::FpParameters, BigInteger};
+use std::convert::TryInto;
 
 // Processor for deposit and withdraw logic.
 pub fn process_instruction(
@@ -59,13 +56,16 @@ pub fn process_instruction(
         let merkle_tree_pda_token = next_account_info(account)?;
         let system_program_account = next_account_info(account)?;
         let token_program_account = next_account_info(account)?;
-        let ext_amount =
-            i64::from_le_bytes(tmp_storage_pda_data.ext_amount.clone().try_into().unwrap());
+
         let user_pda_token = next_account_info(account)?;
         let authority = next_account_info(account)?;
-    if ext_amount != 0 {
-
+        if *merkle_tree_pda.key != solana_program::pubkey::Pubkey::new(&MERKLE_TREE_ACC_BYTES) {
+            msg!("Recipient has to be merkle tree account for deposit.");
+            return Err(ProgramError::InvalidInstructionData);
         }
+        let (ext_amount_checked, relayer_fees) = check_external_amount(&tmp_storage_pda_data)?;
+        let ext_amount =
+            i64::from_le_bytes(tmp_storage_pda_data.ext_amount.clone().try_into().unwrap());
 
         let authority_seed = [7u8;32];
         let (expected_authority_pubkey, authority_bump_seed) = Pubkey::find_program_address(&[&authority_seed], program_id);
@@ -102,54 +102,12 @@ pub fn process_instruction(
         msg!("Inserting new merkle root.");
         let mut merkle_tree_processor = MerkleTreeProcessor::new(Some(tmp_storage_pda), None)?;
 
-        // ext_amount includes the substracted relayer_fees
-        //TODO implement relayer_fees
-        let relayer_fees =
-            u64::from_le_bytes(tmp_storage_pda_data.relayer_fees.clone().try_into().unwrap());
-        // pub_amount is the public amount included in public inputs for proof verification
-        let pub_amount =
-            <BigInteger256 as FromBytes>::read(&tmp_storage_pda_data.amount[..]).unwrap();
-
-
 
         if ext_amount > 0 {
-            if *merkle_tree_pda.key != solana_program::pubkey::Pubkey::new(&MERKLE_TREE_ACC_BYTES) {
-                msg!("Recipient has to be merkle tree account for deposit.");
-                return Err(ProgramError::InvalidInstructionData);
-            }
 
-            if pub_amount.0[1] != 0 || pub_amount.0[2] != 0 || pub_amount.0[3] != 0 {
-                msg!("Public amount is larger than u64.");
-                return Err(ProgramError::InvalidInstructionData);
-            }
-
-            let pub_amount_fits_i64 = i64::try_from(pub_amount.0[0]);
-            if pub_amount_fits_i64.is_err() == true {
-                msg!("Public amount is larger than i64.");
-                return Err(ProgramError::InvalidInstructionData);
-            }
-            let ext_amount_from_pub = pub_amount.0[0] - relayer_fees;
-            //check amount
-            if  ext_amount_from_pub != ext_amount.try_into().unwrap() {
-                msg!("Invalid external amount (relayer_fees)");
-                return Err(ProgramError::InvalidInstructionData);
-            }
-
-            msg!("Creating two_leaves_pda.");
-            create_and_check_account(
-                program_id,
-                signer_account,
-                two_leaves_pda,
-                system_program_account,
-                &tmp_storage_pda_data.proof_a_b_c_leaves_and_nullifiers[320..352],
-                &b"leaves"[..],
-                106u64,                             //bytes
-                ext_amount_from_pub,                //lamports
-                true,                               //rent_exempt
-            )?;
             msg!("Created two_leaves_pda successfully.");
 
-            msg!("Deposited {}", ext_amount);
+            msg!("Deposited {}", ext_amount_checked);
             token_transfer(
                 token_program_account,
                 user_pda_token,
@@ -159,7 +117,7 @@ pub fn process_instruction(
                 &authority,
                 &authority_seed[..],
                 &[authority_bump_seed],
-                u64::try_from(ext_amount).unwrap(),
+                ext_amount_checked,
             )?;
 
         } else if ext_amount < 0 {
@@ -191,28 +149,6 @@ pub fn process_instruction(
             )?;
             msg!("Created two_leaves_pda successfully.");
 
-            // calculate ext_amount from pubAmount:
-            let mut field = FqParameters::MODULUS;
-            field.sub_noborrow(&pub_amount);
-
-            if field.0[1] != 0 || field.0[2] != 0 || field.0[3] != 0 {
-                msg!("Public amount is larger than u64.");
-                return Err(ProgramError::InvalidInstructionData);
-            }
-            let pub_amount_fits_i64 = i64::try_from(pub_amount.0[0]);
-            if pub_amount_fits_i64.is_err() {
-                msg!("Public amount is larger than i64.");
-                return Err(ProgramError::InvalidInstructionData);
-            }
-            // field is the positive value
-            let ext_amount_from_pub = field.0[0] - relayer_fees;
-
-            //check amount
-            if  ext_amount_from_pub != (-ext_amount).try_into().unwrap() {
-                msg!("Invalid external amount (relayer_fees)");
-                return Err(ProgramError::InvalidInstructionData);
-            }
-
             token_transfer(
                 token_program_account,
                 merkle_tree_pda_token,
@@ -222,30 +158,42 @@ pub fn process_instruction(
                 &authority,
                 &authority_seed[..],
                 &[authority_bump_seed],
-                ext_amount_from_pub,
+                ext_amount_checked,
             )?;
 
-            if relayer_fees > 0 {
-                if Pubkey::new(&tmp_storage_pda_data.signing_address) != *signer_account.key {
-                    msg!("wrong relayer");
-                    return Err(ProgramError::InvalidArgument);
-                }
-                let relayer_pda_token = next_account_info(account)?;
-
-                token_transfer(
-                    token_program_account,
-                    merkle_tree_pda_token,
-                    //destination,
-                    relayer_pda_token,
-                    &authority,
-                    &authority_seed[..],
-                    &[authority_bump_seed],
-                    relayer_fees,
-                )?;
-            }
 
         }
 
+        if relayer_fees > 0 {
+            if Pubkey::new(&tmp_storage_pda_data.signing_address) != *signer_account.key {
+                msg!("wrong relayer");
+                return Err(ProgramError::InvalidArgument);
+            }
+            let relayer_pda_token = next_account_info(account)?;
+
+            token_transfer(
+                token_program_account,
+                merkle_tree_pda_token,
+                //destination,
+                relayer_pda_token,
+                &authority,
+                &authority_seed[..],
+                &[authority_bump_seed],
+                relayer_fees,
+            )?;
+        }
+        msg!("Creating two_leaves_pda.");
+        create_and_check_account(
+            program_id,
+            signer_account,
+            two_leaves_pda,
+            system_program_account,
+            &tmp_storage_pda_data.proof_a_b_c_leaves_and_nullifiers[320..352],
+            &b"leaves"[..],
+            106u64,                             //bytes
+            0,                                  //lamports
+            true,                               //rent_exempt
+        )?;
         //insert Merkle root
         merkle_tree_processor.process_instruction(accounts)?;
 
