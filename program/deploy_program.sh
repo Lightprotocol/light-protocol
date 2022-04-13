248a1f4895204219707ebd8e6a800d9499cc9f22
ananas-block <flow23@protonmail.com>
2022-04-13 02:11:11 -0400

    changed merkle tree pubkeys

--- program/deploy_program.sh
+++ program/deploy_program.sh
@@ -1,2 +1,2 @@
 cargo build-bpf --manifest-path=./Cargo.toml --bpf-out-dir=dist/program
-solana program deploy ./dist/program/light_protocol_program_v1.so
+solana program deploy ./dist/program/light_protocol_program.so
