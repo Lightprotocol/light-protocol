0296337cc66f0f25e667e2de2f53c8fe7e6beacd
ananas-block <flow23@protonmail.com>
2022-05-25 01:27:42 +0100

    test_cyclotomic_exp works

--- verifier_program/programs/verifier_program/src/groth16_verifier/final_exponentiation/processor.rs
+++ verifier_program/programs/verifier_program/src/groth16_verifier/final_exponentiation/processor.rs
@@ -34,18 +34,18 @@ mod tests {
     #[test]
     pub fn test_final_exp() {
         let miller_loop_bytes = [211, 231, 132, 182, 211, 183, 85, 93, 214, 230, 240, 197, 144, 18, 159, 29, 215, 214, 234, 67, 95, 178, 102, 151, 20, 106, 95, 248, 19, 185, 138, 46, 143, 162, 146, 137, 88, 99, 10, 48, 115, 148, 32, 133, 73, 162, 157, 239, 70, 74, 182, 191, 122, 199, 89, 79, 122, 26, 156, 169, 142, 101, 134, 27, 116, 130, 173, 228, 156, 165, 45, 207, 206, 200, 148, 179, 174, 210, 104, 75, 22, 219, 230, 1, 172, 193, 58, 203, 119, 122, 244, 189, 144, 97, 253, 21, 24, 17, 92, 102, 160, 162, 55, 203, 215, 162, 166, 57, 183, 163, 110, 19, 84, 224, 156, 220, 31, 246, 113, 204, 202, 78, 139, 231, 119, 145, 166, 15, 254, 99, 20, 11, 81, 108, 205, 133, 90, 159, 19, 1, 34, 23, 154, 191, 145, 244, 200, 23, 134, 68, 115, 80, 204, 3, 103, 147, 138, 46, 209, 7, 193, 175, 158, 214, 181, 81, 199, 155, 0, 116, 245, 216, 123, 103, 158, 94, 223, 110, 67, 229, 241, 109, 206, 202, 182, 0, 198, 163, 38, 130, 46, 42, 171, 209, 162, 32, 94, 175, 225, 106, 236, 15, 175, 222, 148, 48, 109, 157, 249, 181, 178, 110, 7, 67, 62, 108, 161, 22, 95, 164, 182, 209, 239, 16, 20, 128, 5, 48, 243, 240, 178, 241, 163, 223, 28, 209, 150, 111, 200, 93, 251, 126, 27, 14, 104, 15, 53, 159, 130, 76, 192, 229, 243, 32, 108, 42, 0, 125, 241, 245, 15, 92, 208, 73, 181, 236, 35, 87, 26, 191, 179, 217, 219, 68, 92, 3, 192, 99, 197, 100, 25, 51, 99, 77, 230, 151, 200, 46, 246, 151, 83, 228, 105, 44, 4, 147, 182, 120, 15, 33, 135, 118, 63, 198, 244, 162, 237, 56, 207, 180, 150, 87, 97, 43, 82, 147, 14, 199, 189, 17, 217, 254, 191, 173, 73, 110, 84, 4, 131, 245, 240, 198, 22, 69, 2, 114, 178, 112, 239, 3, 86, 132, 221, 38, 217, 88, 59, 174, 221, 178, 108, 37, 46, 60, 51, 59, 68, 40, 207, 120, 174, 184, 227, 5, 91, 175, 145, 131, 36, 165, 197, 98, 135, 77, 53, 152, 100, 65, 101, 253, 2, 182, 145, 39];
-
+        let mut state = FinalExponentiationState::new(miller_loop_bytes);
         let f = parse_f_from_bytes(&miller_loop_bytes.to_vec());
         let res = final_exponentiation(&f);
-        let mut state = FinalExponentiationComputeState::new(f);
+        let mut compute_state = FinalExponentiationComputeState::new(&state);
         let mut res_custom;
 
         for i in 0..6 {
-            res_custom = state.final_exponentiation_custom();
+            res_custom = compute_state.final_exponentiation_custom(&mut state);
             state.current_compute = 0;
         }
         assert_eq!(res.unwrap(),  parse_f_from_bytes(&ALPHA_G1_BETA_G2.to_vec()));
-        assert_eq!(state.f2,  parse_f_from_bytes(&ALPHA_G1_BETA_G2.to_vec()));
+        assert_eq!(compute_state.f2,  parse_f_from_bytes(&ALPHA_G1_BETA_G2.to_vec()));
         // assert_eq!(state.current_instruction_index, 5);
 
     }
@@ -117,22 +117,68 @@ mod tests {
         Some(y16)
         //})
     }
-    pub struct FinalExponentiationComputeState {
-        f:  <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
-        f1: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
-        f2: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
-        f3: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
-        f4: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
-        f5: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+
+    pub struct FinalExponentiationState {
+        f:  [u8;384],
+        f1: [u8;384],
+        f2: [u8;384],
+        f3: [u8;384],
+        f4: [u8;384],
+        i: [u8;384],
         current_instruction_index: u64,
         max_compute: u64,
         current_compute: u64,
         first_exp_by_neg_x: u64,
         second_exp_by_neg_x:u64,
         third_exp_by_neg_x: u64,
+        initialized: u64,
+        outer_loop: u64,
+        cyclotomic_square_in_place:u64
+    }
+    impl FinalExponentiationState {
+        pub fn new(f: [u8;384]) ->  FinalExponentiationState {
+            FinalExponentiationState {
+                f:  f,
+                f1: [0;384],
+                f2: [0;384],
+                f3: [0;384],
+                f4: [0;384],
+                i: [0;384],
+                current_instruction_index: 0,
+                max_compute: 1,
+                current_compute:0,
+                first_exp_by_neg_x: 0,
+                second_exp_by_neg_x:0,
+                third_exp_by_neg_x: 0,
+                initialized: 0,
+                outer_loop: 1,
+                cyclotomic_square_in_place:0,
+            }
+        }
+
+        fn check_compute_units(&self)-> bool {
+            if self.current_compute < self.max_compute {
+                println!("check_compute_units: {}", true);
+                true
+            } else {
+                println!("check_compute_units: {}", false);
+                false
+            }
+
+        }
+    }
+    pub struct FinalExponentiationComputeState {
+        f:  <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        f1: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        f2: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        f3: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        f4: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        i: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+
     }
     impl FinalExponentiationComputeState {
-        pub fn new(f: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk ) -> FinalExponentiationComputeState {
+        pub fn new(state: &FinalExponentiationState ) -> FinalExponentiationComputeState {
+            let f = parse_f_from_bytes(&state.f.to_vec());
             let mut f1 = f.clone();
             f1.conjugate();
             FinalExponentiationComputeState {
@@ -141,19 +187,15 @@ mod tests {
                 f2: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk::zero(),
                 f3: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk::zero(),
                 f4: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk::zero(),
-                f5: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk::zero(),
-                current_instruction_index: 0,
-                max_compute: 1,
-                current_compute:0,
-                first_exp_by_neg_x: 0,
-                second_exp_by_neg_x:0,
-                third_exp_by_neg_x: 0,
+                i: <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk::zero(),
+
             }
         }
 
             #[allow(clippy::let_and_return)]
             pub fn final_exponentiation_custom(
-                &mut self
+                &mut self,
+                state: &mut FinalExponentiationState
             ) -> Result<Option<<ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk>, ()> {
                 // Easy part: result = elt^((q^6-1)*(q^2+1)).
                 // Follows, e.g., Beuchat et al page 9, by computing result as follows:
@@ -162,60 +204,60 @@ mod tests {
                 // f1 = r.conjugate() = f^(p^6)
                 //let mut f1 = *f;
 
-                if self.current_instruction_index == 0 && self.check_compute_units(){
+                if state.current_instruction_index == 0 && state.check_compute_units(){
                     println!("entered" );
                     self.f = self.f.inverse().unwrap();//.map(|mut f2| {
-                    self.current_compute+=1;
-                    self.current_instruction_index+=1;
+                    state.current_compute+=1;
+                    state.current_instruction_index+=1;
                 }
-                if !self.check_compute_units() {
+                if !state.check_compute_units() {
                     return Ok(Some(self.f));
                 }
 
 
 
-                if self.current_instruction_index == 1 && self.check_compute_units(){
+                if state.current_instruction_index == 1 && state.check_compute_units(){
                     // f2 = f^(-1);
                     // r = f^(p^6 - 1)
                     self.f1 = self.f1 * self.f;
 
-                    self.current_compute+=1;
-                    self.current_instruction_index+=1;
+                    state.current_compute+=1;
+                    state.current_instruction_index+=1;
                 }
-                if !self.check_compute_units() {
+                if !state.check_compute_units() {
                     return Ok(Some(self.f));
                 }
 
 
-                if self.current_instruction_index == 2 && self.check_compute_units(){
+                if state.current_instruction_index == 2 && state.check_compute_units(){
                     // f2 = f^(p^6 - 1)
                     self.f = self.f1;
-                    self.current_compute+=1;
-                    self.current_instruction_index+=1;
+                    state.current_compute+=1;
+                    state.current_instruction_index+=1;
                 }
-                if !self.check_compute_units() {
+                if !state.check_compute_units() {
                     return Ok(Some(self.f));
                 }
 
 
-                if self.current_instruction_index == 3 && self.check_compute_units(){
+                if state.current_instruction_index == 3 && state.check_compute_units(){
                     // r = f^((p^6 - 1)(p^2))
                     self.f1.frobenius_map(2);
-                    self.current_compute+=1;
-                    self.current_instruction_index+=1;
+                    state.current_compute+=1;
+                    state.current_instruction_index+=1;
                 }
-                if !self.check_compute_units() {
+                if !state.check_compute_units() {
                     return Ok(Some(self.f));
                 }
 
-                if self.current_instruction_index == 4 && self.check_compute_units(){
+                if state.current_instruction_index == 4 && state.check_compute_units(){
                     // r = f^((p^6 - 1)(p^2) + (p^6 - 1))
                     // r = f^((p^6 - 1)(p^2 + 1))
                     self.f1 *= self.f;
-                    self.current_compute+=1;
-                    self.current_instruction_index+=1;
+                    state.current_compute+=1;
+                    state.current_instruction_index+=1;
                 }
-                if !self.check_compute_units() {
+                if !state.check_compute_units() {
                     return Ok(Some(self.f));
                 }
 
@@ -261,44 +303,54 @@ mod tests {
                 //})
             }
 
-            fn check_compute_units(&self)-> bool {
-                if self.current_compute < self.max_compute {
-                    println!("check_compute_units: {}", true);
-                    true
-                } else {
-                    println!("check_compute_units: {}", false);
-                    false
-                }
-
-            }
 
     }
-    pub fn cyclotomic_exp(fe: &<ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk, res: &mut <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk) {
+    pub fn cyclotomic_exp(
+        fe: &<ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        res: &mut <ark_ec::models::bn::Bn<ark_bn254::Parameters> as ark_ec::PairingEngine>::Fqk,
+        state: &mut FinalExponentiationState
+    ) -> bool {
+        if state.initialized == 0 {
+            *res = fe.clone();
+            state.initialized +=1;
+        }
 
-        *res = fe.clone();
-        println!("*fe {:?}", fe);
-        let mut self_inverse = fe.clone();
-        self_inverse.conjugate();
-        let mut found_nonzero = false;
         // let naf = crate::biginteger::arithmetic::find_wnaf(exponent.as_ref());
 
         // skip first iteration for it is the assignment
-        for i in 1..63 {
-            res.cyclotomic_square_in_place();
+        for i in (state.outer_loop as usize)..63 {
+            if !state.check_compute_units() {
+                return false;
+            }
+            if state.cyclotomic_square_in_place == 0 {
+                res.cyclotomic_square_in_place();
+                state.cyclotomic_square_in_place = 1;
+                state.current_compute+=1;
+            }
+            if !state.check_compute_units() {
+                return false;
+            }
 
             println!("res {:?}", res);
             if NAF_VEC[i] != 0 {
-                found_nonzero = true;
 
                 if NAF_VEC[i] > 0 {
                     *res *= fe;
                 } else {
-
+                    let mut self_inverse = fe.clone();
+                    self_inverse.conjugate();
                     *res *= &self_inverse;
                 }
+                state.current_compute+=1;
+
             }
+            state.cyclotomic_square_in_place = 0;
+            state.outer_loop +=1;
         }
         res.conjugate();
+        state.outer_loop = 1;
+        state.first_exp_by_neg_x = 1;
+        true
     }
 
     pub fn exp_by_neg_x(
@@ -317,13 +369,19 @@ mod tests {
         let miller_loop_bytes = [211, 231, 132, 182, 211, 183, 85, 93, 214, 230, 240, 197, 144, 18, 159, 29, 215, 214, 234, 67, 95, 178, 102, 151, 20, 106, 95, 248, 19, 185, 138, 46, 143, 162, 146, 137, 88, 99, 10, 48, 115, 148, 32, 133, 73, 162, 157, 239, 70, 74, 182, 191, 122, 199, 89, 79, 122, 26, 156, 169, 142, 101, 134, 27, 116, 130, 173, 228, 156, 165, 45, 207, 206, 200, 148, 179, 174, 210, 104, 75, 22, 219, 230, 1, 172, 193, 58, 203, 119, 122, 244, 189, 144, 97, 253, 21, 24, 17, 92, 102, 160, 162, 55, 203, 215, 162, 166, 57, 183, 163, 110, 19, 84, 224, 156, 220, 31, 246, 113, 204, 202, 78, 139, 231, 119, 145, 166, 15, 254, 99, 20, 11, 81, 108, 205, 133, 90, 159, 19, 1, 34, 23, 154, 191, 145, 244, 200, 23, 134, 68, 115, 80, 204, 3, 103, 147, 138, 46, 209, 7, 193, 175, 158, 214, 181, 81, 199, 155, 0, 116, 245, 216, 123, 103, 158, 94, 223, 110, 67, 229, 241, 109, 206, 202, 182, 0, 198, 163, 38, 130, 46, 42, 171, 209, 162, 32, 94, 175, 225, 106, 236, 15, 175, 222, 148, 48, 109, 157, 249, 181, 178, 110, 7, 67, 62, 108, 161, 22, 95, 164, 182, 209, 239, 16, 20, 128, 5, 48, 243, 240, 178, 241, 163, 223, 28, 209, 150, 111, 200, 93, 251, 126, 27, 14, 104, 15, 53, 159, 130, 76, 192, 229, 243, 32, 108, 42, 0, 125, 241, 245, 15, 92, 208, 73, 181, 236, 35, 87, 26, 191, 179, 217, 219, 68, 92, 3, 192, 99, 197, 100, 25, 51, 99, 77, 230, 151, 200, 46, 246, 151, 83, 228, 105, 44, 4, 147, 182, 120, 15, 33, 135, 118, 63, 198, 244, 162, 237, 56, 207, 180, 150, 87, 97, 43, 82, 147, 14, 199, 189, 17, 217, 254, 191, 173, 73, 110, 84, 4, 131, 245, 240, 198, 22, 69, 2, 114, 178, 112, 239, 3, 86, 132, 221, 38, 217, 88, 59, 174, 221, 178, 108, 37, 46, 60, 51, 59, 68, 40, 207, 120, 174, 184, 227, 5, 91, 175, 145, 131, 36, 165, 197, 98, 135, 77, 53, 152, 100, 65, 101, 253, 2, 182, 145, 39];
 
         let f = parse_f_from_bytes(&miller_loop_bytes.to_vec());
+        let mut state = FinalExponentiationState::new(miller_loop_bytes);
+        let mut compute_state = FinalExponentiationComputeState::new(&state);
 
-        let mut state = FinalExponentiationComputeState::new(f);
-        let mut state_ref = FinalExponentiationComputeState::new(f);
+        for i in 0..150 {
+            cyclotomic_exp(&f, &mut compute_state.f1,&mut state);
+            state.current_instruction_index += state.current_compute;
+            state.current_compute = 0;
+            if state.first_exp_by_neg_x == 1 {break}
 
-        cyclotomic_exp(&f, &mut state.f1);
+        }
+        println!("current_instruction_index: {:?}", state.current_instruction_index);
         println!("\n\n-------------------------------\n\n");
-        assert_eq!(state.f1, exp_by_neg_x(f));
+        assert_eq!(compute_state.f1, exp_by_neg_x(f));
 
     }
 
