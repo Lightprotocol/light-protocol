7a0d3c86a35ca2097d1e8bab9b592784d5acd738
ananas-block <flow23@protonmail.com>
2022-05-31 00:05:14 +0100

    deleted old program

--- program/src/groth16_verifier/miller_loop/mod.rs
+++
@@ -1,5 +0,0 @@
-//miller loop
-pub mod instructions;
-pub mod processor;
-pub mod ranges;
-pub mod state;
--- program/src/groth16_verifier/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod final_exponentiation;
-pub mod groth16_processor;
-pub mod miller_loop;
-pub mod parsers;
-pub mod prepare_inputs;
