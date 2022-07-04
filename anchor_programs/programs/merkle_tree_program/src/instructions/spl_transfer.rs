84dc0f57fbe843a422ec0a81e039d4c0c4e48346
ananas-block <flow23@protonmail.com>
2022-07-04 22:43:25 +0100

    stash merkle tree tests working

--- anchor_programs/programs/merkle_tree_program/src/instructions/spl_transfer.rs
+++ anchor_programs/programs/merkle_tree_program/src/instructions/spl_transfer.rs
@@ -1,21 +1,19 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Token, TokenAccount, Transfer};
-use solana_program::program_pack::Pack;
-use crate::utils::config::{self,MERKLE_TREE_INIT_AUTHORITY};
+use crate::utils::config;
 
 #[derive(Accounts)]
 #[instruction(data: Vec<u8>,_verifier_index: u64, _merkle_tree_index: u64)]
 pub struct WithdrawSpl<'info> {
     /// CHECK:` Signer is registered verifier program.
-    // #[account(mut, address=solana_program::pubkey::Pubkey::new(&config::REGISTERED_VERIFIER_KEY_ARRAY[_verifier_index as usize]))]
-    #[account(mut)]
+    #[account(mut, address=solana_program::pubkey::Pubkey::new(&config::REGISTERED_VERIFIER_KEY_ARRAY[_verifier_index as usize]))]
     pub authority: Signer<'info>,
     /// CHECK:` That the merkle tree token belongs to a registered Merkle tree.
     #[account(mut, constraint = merkle_tree_token.key() == Pubkey::new(&config::MERKLE_TREE_ACC_BYTES_ARRAY[_merkle_tree_index as usize].1))]
     pub merkle_tree_token: Account<'info, TokenAccount>,
     pub token_program: Program<'info, Token>,
     /// CHECK:` that the token authority is derived in the correct way.
-    #[account(mut)]
+    #[account(mut, seeds=[b"spl"], bump)]
     pub token_authority: AccountInfo<'info>,
     // Recipients are specified in remaining accounts and checked in the verifier program.
 }
@@ -30,7 +28,7 @@ pub fn process_spl_transfer<'info> (ctx: Context<'_, '_, '_, 'info, WithdrawSpl<
         let amount = u64::from_le_bytes(amount_u8.try_into().unwrap());
         let to = next_account_info(account)?.clone();
         msg!("Withdrawing {}", amount);
-        let (address, bump) = solana_program::pubkey::Pubkey::find_program_address(
+        let (_, bump) = solana_program::pubkey::Pubkey::find_program_address(
             &[&b"spl".as_ref()], ctx.program_id );
         let bump = &[bump][..];
         let seeds = &[&[&b"spl".as_ref(), bump][..]];
