d7b79b2ec05972fb3c8dd6d3ef8f7eedc3456c89
ananas-block <flow23@protonmail.com>
2022-10-02 03:59:03 +0100

    created groth16-solana crate

---
+++ groth16-solana/src/errors.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+#[derive(Debug, Error, Clone, PartialEq, Eq)]
+pub enum Groth16Error {
+    #[error("Incompatible Verifying Key with number of public inputs")]
+    IncompatibleVerifyingKeyWithNrPublicInputs,
+    #[error("ProofVerificationFailed")]
+    ProofVerificationFailed,
+    #[error("InvalidG1Length")]
+    InvalidG1Length,
+    #[error("InvalidG2Length")]
+    InvalidG2Length,
+    #[error("InvalidPublicInputsLength")]
+    InvalidPublicInputsLength
+}
