56e295541693a0b2bf2604ea941d68d88cf782e5
Darjusch <Darjusch.schrand@code.berlin>
2022-10-10 20:43:31 +0100

    first basicConfig

---
+++ anchor_programs/tests/utils/config.ts
@@ -0,0 +1,26 @@
+import { Program } from '@project-serum/anchor';
+import { MerkleTreeProgram } from '../../target/types/merkle_tree_program';
+import { VerifierProgram } from '../../target/types/verifier_program';
+
+export function getConfig(network: Network) {
+	if(Network.MAINNET === network) {
+    console.log("Not implemented yet");
+	}
+	else if(Network.DEVNET === network) {
+    console.log("Not implemented yet");
+  }
+	else if (Network.LOCAL === network) {
+		anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.AnchorProvider.local('http://127.0.0.1:8899', {preflightCommitment: "finalized", commitment: "finalized"});//anchor.getProvider(); // Obv. replace local and 127.0.0.1 with mainnet info
+    const verifierProgram = anchor.workspace.VerifierProgram as Program<VerifierProgram>;
+		const merkleTreeProgram = anchor.workspace.MerkleTreeProgram as Program<MerkleTreeProgram>;
+		return {provider, verifierProgram, merkleTreeProgram}
+	}
+}
+
+
+export enum Network {
+  MAINNET = 'mainnet-beta',
+  DEVNET = 'devnet',
+  LOCAL = 'local',
+}
