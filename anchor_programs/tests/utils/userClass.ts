3e5b3360bdf912b19b4706456a96486d521b73b3
Darjusch <Darjusch.schrand@code.berlin>
2022-10-11 11:18:58 +0100

    minor changes

--- anchor_programs/tests/utils/userClass.ts
+++ anchor_programs/tests/utils/userClass.ts
@@ -1,19 +1,22 @@
-const nacl = require('tweetnacl');
-export const createEncryptionKeypair = () => nacl.box.keyPair();
+import nacl from 'tweetnacl';
+
+let circomlibjs = require('circomlibjs');
 const light = require('../../light-protocol-sdk');
+export const createEncryptionKeypair = () => nacl.box.keyPair();
 
 export class UserClass {
-  encryptionKeypair: any;
+  encryptionKeypair: nacl.BoxKeyPair;
   shieldedKeypair: any;
   userUtxos: any;
-  signature: any;
+  signature: string;
   constructor({ signature}) {
     this.signature = signature;
   }
-  async initUser() {
-    this.encryptionKeypair = deriveEncryptionKeypair(this.signature);
-    this.shieldedKeypair = deriveShieldedKeypair(this.signature);
-    this.userUtxos = fetchUserUtxos(this.shieldedKeypair);
+  async initUser(POSEIDON) {
+    const encryptionKeypair = deriveEncryptionKeypair(this.signature);
+    const shieldedKeypair = await deriveShieldedKeypair(this.signature, POSEIDON);
+    const userUtxos = fetchUserUtxos(shieldedKeypair);
+    return {encryptionKeypair, shieldedKeypair, userUtxos, POSEIDON};
   }
 }
 
@@ -22,8 +25,11 @@ function deriveEncryptionKeypair(signature) {
   return createEncryptionKeypair();
 }
 
-function deriveShieldedKeypair(signature) {
-  return new light.Keypair();
+async function deriveShieldedKeypair(signature, POSEIDON) {
+  // const POSEIDON = await circomlibjs.buildPoseidonOpt();
+  const shieldedKeypair = new light.Keypair(POSEIDON)
+  console.log("Shielded Keypair in userClass", shieldedKeypair)
+  return shieldedKeypair;
 }
 
 function fetchUserUtxos(shieldedKeypair) {
