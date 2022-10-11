8577d1561477285c48626b76155088e9b35b0015
Darjusch <Darjusch.schrand@code.berlin>
2022-10-11 11:19:13 +0100

    minor changes to config

--- anchor_programs/tests/utils/config.ts
+++ anchor_programs/tests/utils/config.ts
@@ -1,8 +1,9 @@
+import * as anchor from "@project-serum/anchor";
 import { Program } from '@project-serum/anchor';
 import { MerkleTreeProgram } from '../../target/types/merkle_tree_program';
 import { VerifierProgram } from '../../target/types/verifier_program';
 
-export function getConfig(network: Network) {
+export async function getConfig(network: Network) {
 	if(Network.MAINNET === network) {
     console.log("Not implemented yet");
 	}
@@ -12,8 +13,8 @@ export function getConfig(network: Network) {
 	else if (Network.LOCAL === network) {
 		anchor.setProvider(anchor.AnchorProvider.env());
     const provider = anchor.AnchorProvider.local('http://127.0.0.1:8899', {preflightCommitment: "finalized", commitment: "finalized"});//anchor.getProvider(); // Obv. replace local and 127.0.0.1 with mainnet info
-    const verifierProgram = anchor.workspace.VerifierProgram as Program<VerifierProgram>;
-		const merkleTreeProgram = anchor.workspace.MerkleTreeProgram as Program<MerkleTreeProgram>;
+    const verifierProgram = await anchor.workspace.VerifierProgram as Program<VerifierProgram>;
+		const merkleTreeProgram = await anchor.workspace.MerkleTreeProgram as Program<MerkleTreeProgram>;
 		return {provider, verifierProgram, merkleTreeProgram}
 	}
 }
