d8d7584dbd2899967573f82ed49fa24227d26b8f
Darjusch <Darjusch.schrand@code.berlin>
2022-10-12 18:52:09 +0100

    poseidonhash

--- anchor_programs/tests/utils/v3/poseidonHash.ts
+++ anchor_programs/tests/utils/v3/poseidonHash.ts
@@ -2,6 +2,6 @@ const ethers_1 = require("ethers");
 const circomlibjs = require('circomlibjs');
 export const poseidonHash = async (items) => {
   let poseidon = await circomlibjs.buildPoseidonOpt();
-  ethers_1.BigNumber.from(poseidon(items))
+  return ethers_1.BigNumber.from(poseidon(items))
 };
 export const poseidonHash2 = (a, b) => (0, exports.poseidonHash)([a, b]);
