4cc2074e5101bc991d63125c45af24c4b54df0d3
Swen <swen.schaeferjohann@code.berlin>
2023-01-20 16:57:11 +0000

    removed sdk-ts/lib folder, added cli staging

--- cli/build/commands/register.js
+++ cli/build/commands/register.js
@@ -2,19 +2,48 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var saveUser = function () {
-    fs.writeFile("secret.txt", "private key : 121323232322323232323\n    \npublic key: 2323", function (err) {
+    var signature = "23232323232323";
+    fs.writeFile("signature.txt", signature, function (err) {
         if (err)
             throw err;
-        console.log("new user created. Back up your secret.txt file!");
+        console.log("signature cached");
+    });
+    var decryptedUtxos = [{ test: "testString" }, 232323, "string"];
+    fs.writeFile("utxos.txt", JSON.stringify(decryptedUtxos), function (err) {
+        if (err)
+            throw err;
+        console.log("decrypted utxos cached");
     });
 };
 var readUser = function () {
-    fs.readFile("secret.txt", "utf8", function (err, data) {
-        if (err)
-            throw err;
-        console.log(data);
-    });
+    // read secret
+    var signature;
+    var decryptedUtxos = [];
+    try {
+        fs.readFile("signature.txt", "utf8", function (err, data) {
+            if (err)
+                throw err;
+            console.log(data);
+            signature = data;
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+    try {
+        fs.readFile("utxos.txt", "utf8", function (err, data) {
+            if (err)
+                throw err;
+            console.log(JSON.parse(data));
+            decryptedUtxos = JSON.parse(data);
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+    // read encrypted user utxos (indices good enough!)
 };
-saveUser();
 readUser();
+console.log("save ?");
+saveUser();
 //# sourceMappingURL=register.js.map
