0934affe0d4e1b60b65561de6861f88688641cab
ananas-block <flow23@protonmail.com>
2023-01-24 17:56:38 +0000

    added rust tests to test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 sh build-sdk.sh;
-cd light-system-programs && anchor build && npm test && npm run test-merkle-tree && npm run test-verifiers && cd -;
+cd light-system-programs && anchor build && cargo test && npm test && npm run test-merkle-tree && npm run test-verifiers && cd -;
 cd light-sdk-ts && npm test && sleep 1 && cd -;
 cd light-circuits && npm run test && cd -;
+cd groth16-solana && cargo test && cd -;
+cd light-verifier-sdk && cargo test && cd -;
