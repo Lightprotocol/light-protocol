db61a4ba0cc29a678bebcca6082802803867b254
Michal Rostecki <vadorovsky@gmail.com>
2023-01-28 11:35:12 +0800

    light-sdk-ts: Autogenerate IDL with Anchor

--- light-sdk-ts/src/idls/index.ts
+++ light-sdk-ts/src/idls/index.ts
@@ -1,4 +1,16 @@
-export * from "./merkle_tree_program";
-export * from "./verifier_program_one";
-export * from "./verifier_program_two";
-export * from "./verifier_program_zero";
+export {
+  MerkleTreeProgram,
+  IDL as MERKLE_TREE_PROGRAM_IDL,
+} from "./merkle_tree_program";
+export {
+  VerifierProgramOne,
+  IDL as VERIFIER_PROGRAM_ONE_IDL,
+} from "./verifier_program_one";
+export {
+  VerifierProgramTwo,
+  IDL as VERIFIER_PROGRAM_TWO_IDL,
+} from "./verifier_program_two";
+export {
+  VerifierProgramZero,
+  IDL as VERIFIER_PROGRAM_ZERO_IDL,
+} from "./verifier_program_zero";
--- light-sdk-ts/src/idls/verifier_program_one.ts
+++ light-sdk-ts/src/idls/verifier_program_one.ts
@@ -1,191 +1,23 @@
-export const VerifierProgramOne: VerifierProgramOneIdl = {
-  version: "0.1.0",
-  name: "verifier_program_one",
-  instructions: [
-    {
-      name: "shieldedTransferFirst",
-      accounts: [
-        {
-          name: "signingAddress",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "systemProgram",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "verifierState",
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [
-        {
-          name: "publicAmount",
-          type: "bytes",
-        },
-        {
-          name: "nullifiers",
-          type: {
-            array: [
-              {
-                array: ["u8", 32],
-              },
-              10,
-            ],
-          },
-        },
-        {
-          name: "leaves",
-          type: {
-            array: [
-              {
-                array: ["u8", 32],
-              },
-              2,
-            ],
-          },
-        },
-        {
-          name: "feeAmount",
-          type: "bytes",
-        },
-        {
-          name: "rootIndex",
-          type: "u64",
-        },
-        {
-          name: "relayerFee",
-          type: "u64",
-        },
-        {
-          name: "encryptedUtxos",
-          type: "bytes",
-        },
-      ],
-    },
-    {
-      name: "shieldedTransferSecond",
-      accounts: [
-        {
-          name: "signingAddress",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "verifierState",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "systemProgram",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "programMerkleTree",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "merkleTree",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "preInsertedLeavesIndex",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "authority",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "tokenProgram",
-          isMut: false,
-          isSigner: false,
-        },
-        {
-          name: "sender",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "recipient",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "senderFee",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "recipientFee",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "relayerRecipient",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "escrow",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "tokenAuthority",
-          isMut: true,
-          isSigner: false,
-        },
-        {
-          name: "registeredVerifierPda",
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [
-        {
-          name: "proof",
-          type: "bytes",
-        },
-      ],
-    },
-    {
-      name: "closeVerifierState",
-      accounts: [
-        {
-          name: "signingAddress",
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: "verifierState",
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-  ],
-};
-export type VerifierProgramOneIdl = {
+export type VerifierProgramOne = {
   version: "0.1.0";
   name: "verifier_program_one";
   instructions: [
     {
       name: "shieldedTransferFirst";
+      docs: [
+        "This instruction is the first step of a shielded transaction with 10 inputs and 2 outputs.",
+        "It creates and initializes a verifier state account which stores public inputs and other data",
+        "such as leaves, amounts, recipients, nullifiers, etc. to execute the verification and",
+        "protocol logicin the second transaction.",
+      ];
       accounts: [
         {
           name: "signingAddress";
           isMut: true;
           isSigner: true;
+          docs: [
+            "First transaction, therefore the signing address is not checked but saved to be checked in future instructions.",
+          ];
         },
         {
           name: "systemProgram";
@@ -245,6 +77,11 @@ export type VerifierProgramOneIdl = {
     },
     {
       name: "shieldedTransferSecond";
+      docs: [
+        "This instruction is the second step of a shieled transaction.",
+        "The proof is verified with the parameters saved in the first transaction.",
+        "At successful verification protocol logic is executed.",
+      ];
       accounts: [
         {
           name: "signingAddress";
@@ -325,6 +162,9 @@ export type VerifierProgramOneIdl = {
           name: "registeredVerifierPda";
           isMut: true;
           isSigner: false;
+          docs: [
+            "Verifier config pda which needs ot exist Is not checked the relayer has complete freedom.",
+          ];
         },
       ];
       args: [
@@ -336,6 +176,9 @@ export type VerifierProgramOneIdl = {
     },
     {
       name: "closeVerifierState";
+      docs: [
+        "Close the verifier state to reclaim rent in case the proofdata is wrong and does not verify.",
+      ];
       accounts: [
         {
           name: "signingAddress";
@@ -352,4 +195,201 @@ export type VerifierProgramOneIdl = {
     },
   ];
 };
-export default VerifierProgramOne;
+
+export const IDL: VerifierProgramOne = {
+  version: "0.1.0",
+  name: "verifier_program_one",
+  instructions: [
+    {
+      name: "shieldedTransferFirst",
+      docs: [
+        "This instruction is the first step of a shielded transaction with 10 inputs and 2 outputs.",
+        "It creates and initializes a verifier state account which stores public inputs and other data",
+        "such as leaves, amounts, recipients, nullifiers, etc. to execute the verification and",
+        "protocol logicin the second transaction.",
+      ],
+      accounts: [
+        {
+          name: "signingAddress",
+          isMut: true,
+          isSigner: true,
+          docs: [
+            "First transaction, therefore the signing address is not checked but saved to be checked in future instructions.",
+          ],
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "verifierState",
+          isMut: true,
+          isSigner: false,
+        },
+      ],
+      args: [
+        {
+          name: "publicAmount",
+          type: "bytes",
+        },
+        {
+          name: "nullifiers",
+          type: {
+            array: [
+              {
+                array: ["u8", 32],
+              },
+              10,
+            ],
+          },
+        },
+        {
+          name: "leaves",
+          type: {
+            array: [
+              {
+                array: ["u8", 32],
+              },
+              2,
+            ],
+          },
+        },
+        {
+          name: "feeAmount",
+          type: "bytes",
+        },
+        {
+          name: "rootIndex",
+          type: "u64",
+        },
+        {
+          name: "relayerFee",
+          type: "u64",
+        },
+        {
+          name: "encryptedUtxos",
+          type: "bytes",
+        },
+      ],
+    },
+    {
+      name: "shieldedTransferSecond",
+      docs: [
+        "This instruction is the second step of a shieled transaction.",
+        "The proof is verified with the parameters saved in the first transaction.",
+        "At successful verification protocol logic is executed.",
+      ],
+      accounts: [
+        {
+          name: "signingAddress",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "verifierState",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "programMerkleTree",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "merkleTree",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "preInsertedLeavesIndex",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "authority",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "tokenProgram",
+          isMut: false,
+          isSigner: false,
+        },
+        {
+          name: "sender",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "recipient",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "senderFee",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "recipientFee",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "relayerRecipient",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "escrow",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "tokenAuthority",
+          isMut: true,
+          isSigner: false,
+        },
+        {
+          name: "registeredVerifierPda",
+          isMut: true,
+          isSigner: false,
+          docs: [
+            "Verifier config pda which needs ot exist Is not checked the relayer has complete freedom.",
+          ],
+        },
+      ],
+      args: [
+        {
+          name: "proof",
+          type: "bytes",
+        },
+      ],
+    },
+    {
+      name: "closeVerifierState",
+      docs: [
+        "Close the verifier state to reclaim rent in case the proofdata is wrong and does not verify.",
+      ],
+      accounts: [
+        {
+          name: "signingAddress",
+          isMut: true,
+          isSigner: true,
+        },
+        {
+          name: "verifierState",
+          isMut: true,
+          isSigner: false,
+        },
+      ],
+      args: [],
+    },
+  ],
+};
