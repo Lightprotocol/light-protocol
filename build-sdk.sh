d20a1cd627097338b497d612fc99214a8b32a661
Swen <swen.schaeferjohann@code.berlin>
2023-02-15 17:48:16 +0000

    build-sdk.sh script: no rm just overwrite with npm i; other minor changes

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,4 +1,5 @@
-cd light-sdk-ts && npm run build & sleep 5 && kill $! && cd ..  ;
-cd light-circuits && rm node_modules/light-sdk/ -r && npm i && cd -;
-cd light-system-programs && rm node_modules/light-sdk/ -r && npm i && cd -;
-cd mock-app-verifier && rm node_modules/light-sdk/ -r && npm i && cd -;
+cd light-sdk-ts && npm run build & sleep 5 && kill $! && cd ..;
+cd light-circuits && npm i && cd -;
+cd light-system-programs && npm i && cd -;
+cd mock-app-verifier && npm i && cd -;
+
