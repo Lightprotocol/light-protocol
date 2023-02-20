05d077880b806939b70582b008cc22c1ed5b3a84
ananas-block <flow23@protonmail.com>
2023-02-20 22:02:29 +0000

    removed mostly undefined errors

--- light-sdk-ts/src/transaction.ts
+++ light-sdk-ts/src/transaction.ts
@@ -1289,6 +1289,7 @@ export class Transaction {
       merkleTreeProgram.programId,
     )[0];
   }
+  is_token?: boolean;
 
   // TODO: check why this is called encr keypair but account class
   async checkBalances(account?: Account) {
