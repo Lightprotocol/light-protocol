2a4757a9c6a89dd743d46a529dadcf885489c0c7
Swen <swen.schaeferjohann@code.berlin>
2023-03-07 17:35:43 +0000

    switch npm->yarn for scripts. adding: lockfiles

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,4 +1,4 @@
-cd light-sdk-ts && npm run build & sleep 5 && kill $! && cd ..;
-cd light-circuits && npm i && cd -;
-cd light-system-programs && npm i && cd -;
-cd mock-app-verifier && npm i && cd -;
+cd light-sdk-ts && yarn run build & sleep 5 && kill $! && cd ..;
+cd light-circuits && yarn && cd -;
+cd light-system-programs && yarn && cd -;
+cd mock-app-verifier && yarn && cd -;
