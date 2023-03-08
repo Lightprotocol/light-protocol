5eaad7cd1f7400a7e4897ddd4cbe989d9c8bf919
Swen <swen.schaeferjohann@code.berlin>
2023-03-08 00:48:46 +0000

    try revert idl changes

--- light-sdk-ts/src/idls/verifier_program_storage.ts
+++ light-sdk-ts/src/idls/verifier_program_storage.ts
@@ -1,213 +1,209 @@
 export type VerifierProgramStorage = {
-  "version": "0.1.0",
-  "name": "verifier_program_storage",
-  "instructions": [
+  version: "0.1.0";
+  name: "verifier_program_storage";
+  instructions: [
     {
-      "name": "shieldedTransferFirst",
-      "docs": [
-        "Saves the provided message in a temporary PDA."
-      ],
-      "accounts": [
+      name: "shieldedTransferFirst";
+      docs: ["Saves the provided message in a temporary PDA."];
+      accounts: [
         {
-          "name": "signingAddress",
-          "isMut": true,
-          "isSigner": true
+          name: "signingAddress";
+          isMut: true;
+          isSigner: true;
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: "systemProgram";
+          isMut: false;
+          isSigner: false;
         },
         {
-          "name": "verifierState",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": [
+          name: "verifierState";
+          isMut: true;
+          isSigner: false;
+        },
+      ];
+      args: [
         {
-          "name": "msg",
-          "type": "bytes"
-        }
-      ]
+          name: "msg";
+          type: "bytes";
+        },
+      ];
     },
     {
-      "name": "shieldedTransferClose",
-      "docs": [
+      name: "shieldedTransferClose";
+      docs: [
         "Close the temporary PDA. Should be used when we don't intend to perform",
-        "the second transfer and want to reclaim the funds."
-      ],
-      "accounts": [
+        "the second transfer and want to reclaim the funds.",
+      ];
+      accounts: [
         {
-          "name": "signingAddress",
-          "isMut": true,
-          "isSigner": true
+          name: "signingAddress";
+          isMut: true;
+          isSigner: true;
         },
         {
-          "name": "verifierState",
-          "isMut": true,
-          "isSigner": false
-        }
-      ],
-      "args": []
+          name: "verifierState";
+          isMut: true;
+          isSigner: false;
+        },
+      ];
+      args: [];
     },
     {
-      "name": "shieldedTransferSecond",
-      "docs": [
+      name: "shieldedTransferSecond";
+      docs: [
         "Stores the provided message in a compressed account, closes the",
-        "temporary PDA."
-      ],
-      "accounts": [
+        "temporary PDA.",
+      ];
+      accounts: [
         {
-          "name": "signingAddress",
-          "isMut": true,
-          "isSigner": true
+          name: "signingAddress";
+          isMut: true;
+          isSigner: true;
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: "systemProgram";
+          isMut: false;
+          isSigner: false;
         },
         {
-          "name": "verifierState",
-          "isMut": true,
-          "isSigner": false
+          name: "verifierState";
+          isMut: true;
+          isSigner: false;
         },
         {
-          "name": "logWrapper",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": []
-    }
-  ],
-  "accounts": [
+          name: "logWrapper";
+          isMut: false;
+          isSigner: false;
+        },
+      ];
+      args: [];
+    },
+  ];
+  accounts: [
     {
-      "name": "verifierState",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "verifierState";
+      type: {
+        kind: "struct";
+        fields: [
           {
-            "name": "msg",
-            "type": "bytes"
-          }
-        ]
-      }
-    }
-  ],
-  "errors": [
+            name: "msg";
+            type: "bytes";
+          },
+        ];
+      };
+    },
+  ];
+  errors: [
     {
-      "code": 6000,
-      "name": "NoopProgram",
-      "msg": "The provided program is not the noop program."
-    }
-  ]
+      code: 6000;
+      name: "NoopProgram";
+      msg: "The provided program is not the noop program.";
+    },
+  ];
 };
 
 export const IDL: VerifierProgramStorage = {
-  "version": "0.1.0",
-  "name": "verifier_program_storage",
-  "instructions": [
+  version: "0.1.0",
+  name: "verifier_program_storage",
+  instructions: [
     {
-      "name": "shieldedTransferFirst",
-      "docs": [
-        "Saves the provided message in a temporary PDA."
-      ],
-      "accounts": [
+      name: "shieldedTransferFirst",
+      docs: ["Saves the provided message in a temporary PDA."],
+      accounts: [
         {
-          "name": "signingAddress",
-          "isMut": true,
-          "isSigner": true
+          name: "signingAddress",
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "verifierState",
-          "isMut": true,
-          "isSigner": false
-        }
+          name: "verifierState",
+          isMut: true,
+          isSigner: false,
+        },
       ],
-      "args": [
+      args: [
         {
-          "name": "msg",
-          "type": "bytes"
-        }
-      ]
+          name: "msg",
+          type: "bytes",
+        },
+      ],
     },
     {
-      "name": "shieldedTransferClose",
-      "docs": [
+      name: "shieldedTransferClose",
+      docs: [
         "Close the temporary PDA. Should be used when we don't intend to perform",
-        "the second transfer and want to reclaim the funds."
+        "the second transfer and want to reclaim the funds.",
       ],
-      "accounts": [
+      accounts: [
         {
-          "name": "signingAddress",
-          "isMut": true,
-          "isSigner": true
+          name: "signingAddress",
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "verifierState",
-          "isMut": true,
-          "isSigner": false
-        }
+          name: "verifierState",
+          isMut: true,
+          isSigner: false,
+        },
       ],
-      "args": []
+      args: [],
     },
     {
-      "name": "shieldedTransferSecond",
-      "docs": [
+      name: "shieldedTransferSecond",
+      docs: [
         "Stores the provided message in a compressed account, closes the",
-        "temporary PDA."
+        "temporary PDA.",
       ],
-      "accounts": [
+      accounts: [
         {
-          "name": "signingAddress",
-          "isMut": true,
-          "isSigner": true
+          name: "signingAddress",
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: "systemProgram",
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "verifierState",
-          "isMut": true,
-          "isSigner": false
+          name: "verifierState",
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "logWrapper",
-          "isMut": false,
-          "isSigner": false
-        }
+          name: "logWrapper",
+          isMut: false,
+          isSigner: false,
+        },
       ],
-      "args": []
-    }
+      args: [],
+    },
   ],
-  "accounts": [
+  accounts: [
     {
-      "name": "verifierState",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "verifierState",
+      type: {
+        kind: "struct",
+        fields: [
           {
-            "name": "msg",
-            "type": "bytes"
-          }
-        ]
-      }
-    }
+            name: "msg",
+            type: "bytes",
+          },
+        ],
+      },
+    },
   ],
-  "errors": [
+  errors: [
     {
-      "code": 6000,
-      "name": "NoopProgram",
-      "msg": "The provided program is not the noop program."
-    }
-  ]
+      code: 6000,
+      name: "NoopProgram",
+      msg: "The provided program is not the noop program.",
+    },
+  ],
 };
