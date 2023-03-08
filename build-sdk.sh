6e3ae1a4f61f71cf2cb50e307725a41c2bd83a1e
Swen <swen.schaeferjohann@code.berlin>
2023-03-08 14:39:02 +0000

    added: rm -rf node_modules in scripts, disable failing tests

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,4 +1,4 @@
 cd light-sdk-ts && yarn run build & sleep 5 && kill $! && cd ..;
-cd light-circuits && yarn && cd -;
-cd light-system-programs && yarn && cd -;
-cd mock-app-verifier && yarn && cd -;
+cd light-circuits && rm -rf ./node_modules && yarn && cd -;
+cd light-system-programs && rm -rf ./node_modules && yarn && cd -;
+cd mock-app-verifier && rm -rf ./node_modules && yarn && cd -;
