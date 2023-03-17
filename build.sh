929604a987c716c6c0ac639789b000b93a78ef9c
vadorovsky <vadorovsky@protonmail.com>
2023-03-17 20:22:04 +0100

    Add Github Actions CI jobs (#32)
    * Add Github Actions CI jobs
    
    * Fix cargo fmt issues
    
    * tests: Provide output UTXOs as transaction parameters

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
-cd ..;
-if [ ! -f ./solana ]; then
-    git clone git@github.com:ananas-block/solana.git;
-    cd solana && git fetch -a &&  git checkout audit &&  cd validator/ && cargo build && cd ../..;
-fi
-cd light-protocol-onchain/light-sdk-ts && yarn i && yarn run build; \
-cd ../light-system-programs && yarn install && anchor build; \
-cd ../light-circuits && yarn i && cd ..;
+#!/usr/bin/env bash
 
+set -e
+
+pushd light-sdk-ts
+yarn install
+yarn run build
+popd
+
+pushd light-system-programs
+yarn install
+anchor build
+popd
+
+pushd light-circuits
+yarn install
+popd
--- mock-app-verifier/runTest.sh
+++ mock-app-verifier/runTest.sh
@@ -1,14 +1,26 @@
-#!/bin/bash -e
-../../solana/validator/solana-test-validator \
+#!/bin/bash
+
+set -eux
+
+docker rm -f solana-validator || true
+docker run -d \
+    --name solana-validator \
+    --net=host \
+    -v $HOME/.config/solana/id.json:/root/.config/solana/id.json \
+    -v $(git rev-parse --show-toplevel)/light-system-programs/target/deploy:/usr/local/lib/light-protocol-onchain \
+    -v $(pwd)/target/deploy:/usr/local/lib/mock-app-verifier \
+    vadorovsky/solana:audit \
     --reset \
     --limit-ledger-size 500000000 \
-    --bpf-program J1RRetZ4ujphU75LP8RadjXMf3sA12yC2R44CF7PmU7i ../light-system-programs/target/deploy/verifier_program_zero.so \
-    --bpf-program JA5cjkRJ1euVi9xLWsCJVzsRzEkT8vcC4rqw9sVAo5d6 ../light-system-programs/target/deploy/merkle_tree_program.so \
-    --bpf-program 3KS2k14CmtnuVv2fvYcvdrNgC94Y11WETBpMUGgXyWZL ../light-system-programs/target/deploy/verifier_program_one.so \
-    --bpf-program GFDwN8PXuKZG2d2JLxRhbggXYe9eQHoGYoYK5K3G5tV8  ../light-system-programs/target/deploy/verifier_program_two.so  \
-    --bpf-program noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV ../../solana/web3.js/test/fixtures/noop-program/solana_sbf_rust_noop.so \
-    --bpf-program Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS ./target/deploy/mock_verifier.so --quiet & \
-sleep 5
-PID=$!
-$1;
-kill $PID;
+    --bpf-program J1RRetZ4ujphU75LP8RadjXMf3sA12yC2R44CF7PmU7i /usr/local/lib/light-protocol-onchain/verifier_program_zero.so \
+    --bpf-program JA5cjkRJ1euVi9xLWsCJVzsRzEkT8vcC4rqw9sVAo5d6 /usr/local/lib/light-protocol-onchain/merkle_tree_program.so \
+    --bpf-program 3KS2k14CmtnuVv2fvYcvdrNgC94Y11WETBpMUGgXyWZL /usr/local/lib/light-protocol-onchain/verifier_program_one.so \
+    --bpf-program GFDwN8PXuKZG2d2JLxRhbggXYe9eQHoGYoYK5K3G5tV8 /usr/local/lib/light-protocol-onchain/verifier_program_two.so  \
+    --bpf-program noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV /usr/local/lib/solana-program-library/spl_noop.so \
+    --bpf-program Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS /usr/local/lib/mock-app-verifier/mock_verifier.so \
+    --quiet
+
+sleep 15
+$1
+
+docker rm -f solana-validator
