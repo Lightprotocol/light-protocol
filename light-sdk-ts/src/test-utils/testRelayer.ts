9354d3bb02d596783267f4c11dcac9c01b831687
Abdul Qadir Shaikh <sleepyqadir@Abduls-MacBook-Air.local>
2023-03-30 12:21:48 +0500

    versioned transaction unified and action parameter removed

--- light-sdk-ts/src/test-utils/testRelayer.ts
+++ light-sdk-ts/src/test-utils/testRelayer.ts
@@ -59,26 +59,14 @@ export class TestRelayer extends Relayer {
         recentBlockhash: recentBlockhash,
       });
 
-      console.log({ txMsg });
-
       const lookupTableAccount =
         await provider.provider.connection.getAccountInfo(
           provider.lookUpTable!,
           "confirmed",
         );
 
-      console.log({ lookupTableAccount });
-
       const unpackedLookupTableAccount = AddressLookupTableAccount.deserialize(
         lookupTableAccount!.data,
-      );
-
-      console.log({ unpackedLookupTableAccount });
-
-      console.log(
-        "loookup table here =============>",
-        provider.lookUpTable!,
-        provider.relayer.accounts.lookUpTable!,
       );
 
       const compiledTx = txMsg.compileToV0Message([
@@ -91,32 +79,10 @@ export class TestRelayer extends Relayer {
         },
       ]);
 
-      console.log({ compiledTx });
-
       compiledTx.addressTableLookups[0].accountKey = provider.lookUpTable!;
-      let tx = new VersionedTransaction(compiledTx);
-      let retries = 3;
-      let res;
-      while (retries > 0) {
-        tx = await provider.wallet.signTransaction(tx);
-        try {
-          let serializedTx = tx.serialize();
-          console.log("tx: ", serializedTx);
-          res = await provider.provider.connection.sendRawTransaction(
-            serializedTx,
-            confirmConfig,
-          );
-          retries = 0;
-        } catch (e: any) {
-          retries--;
-          if (retries == 0 || e.logs !== undefined) {
-            console.log(e);
-            return e;
-          }
-        }
-      }
 
-      return res;
+      const response = await sendVersionedTransaction(compiledTx, provider);
+      return response;
     } catch (err) {
       console.error("erorr here =========>", { err });
       throw err;
