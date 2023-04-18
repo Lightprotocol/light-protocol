ad546973577276db164324d58e86f1ae5a0cfe49
Abdul Qadir <a.qadir67@outlook.com>
2023-04-19 02:16:43 +0500

    test: state validator to simplify asserts in user class tests (#80)
    * balanceChecker class
    
    * test: testStateValidator simplifying user class tests asserts
    
    ---------
    
    Co-authored-by: Abdul Qadir Shaikh <sleepyqadir@Abduls-MacBook-Air.local>

--- light-sdk-ts/src/constants.ts
+++ light-sdk-ts/src/constants.ts
@@ -92,6 +92,8 @@ export const DEFAULT_PROGRAMS = {
 // needs to be more than 100k to be rentexempt
 export const MINIMUM_LAMPORTS = 150_000;
 
+export const TOKEN_ACCOUNT_FEE = 500_000;
+
 // TODO: make account object with important accounts
 export const MERKLE_TREE_KEY = new PublicKey(
   "DCxUdYgqjE6AR9m13VvqpkxJqGJYnk8jn8NEeD3QY3BU",
