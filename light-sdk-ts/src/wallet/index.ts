7fab0eab050d5e1844522846b8fb098fcce1c860
ananas-block <flow23@protonmail.com>
2023-04-27 13:12:52 +0100

    createMissingOutUtxos - cleaned up & renamed file

--- light-sdk-ts/src/wallet/index.ts
+++ light-sdk-ts/src/wallet/index.ts
@@ -1,6 +1,6 @@
 export * from "./buildBalance";
 export * from "./user";
 export * from "./provider";
-export * from "./createOutUtxos";
+export * from "./createMissingOutUtxos";
 export * from "./selectInUtxos";
 export * from "./useWallet";
