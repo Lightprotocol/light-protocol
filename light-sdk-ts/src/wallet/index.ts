3dfaa019b741bdd816ae445ef0a99ee5b68c3d5e
ananas-block <flow23@protonmail.com>
2023-04-28 15:26:57 +0100

    reverted name change from createMissingOutUtxos to createOutUtxos

--- light-sdk-ts/src/wallet/index.ts
+++ light-sdk-ts/src/wallet/index.ts
@@ -1,6 +1,6 @@
 export * from "./buildBalance";
 export * from "./user";
 export * from "./provider";
-export * from "./createMissingOutUtxos";
+export * from "./createOutUtxos";
 export * from "./selectInUtxos";
 export * from "./useWallet";
