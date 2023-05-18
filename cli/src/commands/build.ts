522304b4e67a37d00b4a33f40bcc06b089b8fc14
ananas-block <58553958+ananas-block@users.noreply.github.com>
2023-05-18 01:47:45 -0500

    Cli release preparation (#111)
    * cli: added init, build,& test
    
    * cleanup

--- cli/src/commands/build.ts
+++ cli/src/commands/build.ts
@@ -8,17 +8,24 @@ export const builder: CommandBuilder<Options> = (yargs) =>
   yargs.options({
     name: { type: "string" },
     ptau: { type: "number" },
+    circuitDir: { type: "string" },
     // TODO: pass along anchor build options // execsync thingy alt.
   });
 //TODO: move all cli-utils to cli ... -> build into bin buildPsP uses macrocircom...
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
-  let { name, ptau }: any = argv;
+  let { name, ptau, circuitDir }: any = argv;
+  if(!name) {
+    console.log("Project name is undefined add a project name with --name <project-name>");
+    process.exit(0);
+  }
 
-  let circuitDir = "circuit"; // find the dir where the inited psp circuit is
+  if (!circuitDir) {
+    circuitDir = "circuit";
+  }
   if (!ptau) {
     ptau = 15;
-  }  
+  }
   console.log("building PSP...");
   await buildPSP(circuitDir, ptau, name);
 
