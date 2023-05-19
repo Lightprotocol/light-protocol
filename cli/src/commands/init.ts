620a26eaaa507bec8fde781562ed5891965de2f1
Swen Schäferjohann <42959314+SwenSchaeferjohann@users.noreply.github.com>
2023-05-19 10:25:34 +0100

    cli: turn explicit flag into positional argument (#127)
    * replace explicit flag with positional param
    
    users arent required to pass a --flag explicitly any longer
    
    * fmting

--- cli/src/commands/init.ts
+++ cli/src/commands/init.ts
@@ -4,18 +4,21 @@ import { Options } from "yargs-parser";
 import { anchorBinUrlMap, downloadFileIfNotExists } from "../utils/downloadBin";
 
 const path = require("path");
-export const command: string = "init";
+export const command: string = "init [name]";
 export const desc: string = "Initialize a PSP project";
 
 export const builder: CommandBuilder<Options> = (yargs) =>
-  yargs.options({
-    name: { type: "string" },
+  yargs.positional(`name`, {
+    type: `string`,
+    describe: `the name of your project`,
   });
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
   let { name }: any = argv;
-  if(!name) {
-    console.log("Project name is undefined add a project name with --name <project-name>");
+  if (!name) {
+    console.log(
+      "Project name is undefined add a project name with init <project-name>"
+    );
     process.exit(0);
   }
 
@@ -23,7 +26,12 @@ export const handler = async (argv: Arguments<Options>): Promise<void> => {
   const anchorPath = path.resolve(__dirname, "../../bin/light-anchor");
   const dirPath = path.resolve(__dirname, "../../bin/");
 
-  await downloadFileIfNotExists(anchorBinUrlMap, anchorPath,dirPath,"light-anchor")
+  await downloadFileIfNotExists(
+    anchorBinUrlMap,
+    anchorPath,
+    dirPath,
+    "light-anchor"
+  );
 
   execSync(`${anchorPath} init-psp ${name}`);
 
