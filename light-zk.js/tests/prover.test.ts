7c108781c6fc666f4d5d7a2a4927f0e28913c04b
ananas-block <flow23@protonmail.com>
2023-07-10 16:24:53 +0100

    zk.js - prover: added proof compression

--- light-zk.js/tests/prover.test.ts
+++ light-zk.js/tests/prover.test.ts
@@ -264,4 +264,70 @@ describe("Test Prover Functional", () => {
     await tx.getRootIndex();
     tx.getPdaAddresses();
   });
+
+  it.only("testing masking ", async () => {
+    const proofA = [
+      45, 206, 255, 166, 152, 55, 128, 138, 79, 217, 145, 164, 25, 74, 120, 234,
+      234, 217, 68, 149, 162, 44, 133, 120, 184, 205, 12, 44, 175, 98, 168, 172,
+      20, 24, 216, 15, 209, 175, 106, 75, 147, 236, 90, 101, 123, 219, 245, 151,
+      209, 202, 218, 104, 148, 8, 32, 254, 243, 191, 218, 122, 42, 81, 193, 84,
+    ];
+    //  g1Bytes = [...g1Bytes.slice(0, 32).reverse(), ...g1Bytes.slice(32, 64).reverse()];
+    // let g1 = bn128.G1.fromObject([
+    //   leBuff2int(Buffer.from(g1le.slice(0,32))),
+    //   leBuff2int(Buffer.from(g1le.slice(32,64))),
+    //   "1"
+    // ]);
+    let proofACompressed = proofA.slice(0, 32);
+    const proofAY = new anchor.BN(proofACompressed.slice(32), 32, "be");
+    const proofAYIsPositive = Prover.yElementIsPositiveG1(proofAY);
+    assert.isTrue(proofAYIsPositive);
+    proofACompressed[0] = Prover.addBitmaskToByte(
+      proofACompressed[0],
+      proofAYIsPositive,
+    );
+    assert.equal(proofACompressed[0], proofA[0]);
+
+    const proofC = [
+      41, 139, 183, 208, 246, 198, 118, 127, 89, 160, 9, 27, 61, 26, 123, 180,
+      221, 108, 17, 166, 47, 115, 82, 48, 132, 139, 253, 65, 152, 92, 209, 53,
+      37, 25, 83, 61, 252, 42, 181, 243, 16, 21, 2, 199, 123, 96, 218, 151, 253,
+      86, 69, 181, 202, 109, 64, 129, 124, 254, 192, 25, 177, 199, 26, 50,
+    ];
+    let proofCCompressed = proofC.slice(0, 32);
+    const proofCY = new anchor.BN(proofC.slice(32, 64), 32, "be");
+    const proofCYIsPositive = Prover.yElementIsPositiveG1(proofCY);
+    assert.isNotTrue(proofCYIsPositive);
+    proofCCompressed[0] = Prover.addBitmaskToByte(
+      proofCCompressed[0],
+      proofCYIsPositive,
+    );
+    assert.equal(proofCCompressed[0], 169);
+
+    const proofB = [
+      40, 57, 233, 205, 180, 46, 35, 111, 215, 5, 23, 93, 12, 71, 118, 225, 7,
+      46, 247, 147, 47, 130, 106, 189, 184, 80, 146, 103, 141, 52, 242, 25, 0,
+      203, 124, 176, 110, 34, 151, 212, 66, 180, 238, 151, 236, 189, 133, 209,
+      17, 137, 205, 183, 168, 196, 92, 159, 75, 174, 81, 168, 18, 86, 176, 56,
+      16, 26, 210, 20, 18, 81, 122, 142, 104, 62, 251, 169, 98, 141, 21, 253,
+      50, 130, 182, 15, 33, 109, 228, 31, 79, 183, 88, 147, 174, 108, 4, 22, 14,
+      129, 168, 6, 80, 246, 254, 100, 218, 131, 94, 49, 247, 211, 3, 245, 22,
+      200, 177, 91, 60, 144, 147, 174, 90, 17, 19, 189, 62, 147, 152, 18,
+    ];
+    let proofBCompressed = proofB.slice(0, 64);
+    const proofBY = [
+      new anchor.BN(proofB.slice(64, 96), 32, "be"),
+      new anchor.BN(proofB.slice(96, 128), 32, "be"),
+    ];
+    const proofBYIsPositive = Prover.yElementIsPositiveG2(
+      proofBY[0],
+      proofBY[1],
+    );
+    assert.isTrue(proofBYIsPositive);
+    proofBCompressed[0] = Prover.addBitmaskToByte(
+      proofBCompressed[0],
+      proofBYIsPositive,
+    );
+    assert.equal(proofBCompressed[0], 40);
+  });
 });
