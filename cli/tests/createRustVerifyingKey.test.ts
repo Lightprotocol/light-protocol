13dcf0de8750f68d0c8c4494acdcf6b673f8b119
matteo-advancedblockchain <matteo@advancedblockchain.com>
2023-07-17 10:38:56 +0200

    scaffolded tests for cli

---
+++ cli/tests/createRustVerifyingKey.test.ts
@@ -0,0 +1,7 @@
+import { assert, expect } from "chai";
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+import { it } from "mocha";
+
+describe("Test createRustVerifiyingKey Functional", () => {});
