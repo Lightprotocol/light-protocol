6fe4f6a4479bb841a53cbc489cfb4bc939c34514
matteo-advancedblockchain <matteo@advancedblockchain.com>
2023-07-17 16:32:42 +0200

    cleaned unused variables + reinforced tsconfig.json with noUnusedLocals and noUnusedParameters

--- relayer/src/setup/index.ts
+++ relayer/src/setup/index.ts
@@ -1,16 +1,8 @@
-import {
-  createTestAccounts,
-  // initLookUpTableFromFile,
-  // setUpMerkleTree,
-} from "@lightprotocol/zk.js";
-import { getLightProvider, setAnchorProvider } from "../utils/provider";
+import { createTestAccounts } from "@lightprotocol/zk.js";
+import { setAnchorProvider } from "../utils/provider";
 
 export const testSetup = async () => {
   const providerAnchor = await setAnchorProvider();
   // TODO: use updated -- buildscript -> add relayer tests
   await createTestAccounts(providerAnchor.connection);
-
-  // await initLookUpTableFromFile(providerAnchor);
-
-  // await setUpMerkleTree(providerAnchor);
 };
