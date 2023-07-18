f83faa4f21a1a29ac10653dc4438bc02ea2c3c4d
matteo-advancedblockchain <matteo@advancedblockchain.com>
2023-07-18 16:23:51 +0200

    reverted some changes on createRustVerifyingKey

--- cli/src/utils/createRustVerifyingKey.ts
+++ cli/src/utils/createRustVerifyingKey.ts
@@ -148,73 +148,73 @@ async function createVerifyingKeyRsFile(
       }
       var mydata = JSON.parse(fd.toString());
 
-      // for (let i in mydata) {
-      //   if (i == "vk_alpha_1") {
-      //     for (let j in mydata[i]) {
-      //       mydata[i][j] = leInt2Buff(
-      //         unstringifyBigInts(mydata[i][j]),
-      //         32
-      //       ).reverse();
-      //     }
-      //   } else if (i == "vk_beta_2") {
-      //     for (let j in mydata[i]) {
-      //       let tmp = Array.from(
-      //         leInt2Buff(unstringifyBigInts(mydata[i][j][0]), 32)
-      //       )
-      //         .concat(
-      //           Array.from(leInt2Buff(unstringifyBigInts(mydata[i][j][1]), 32))
-      //         )
-      //         .reverse();
-      //       mydata[i][j][0] = tmp.slice(0, 32);
-      //       mydata[i][j][1] = tmp.slice(32, 64);
-      //     }
-      //   } else if (i == "vk_gamma_2") {
-      //     for (var j in mydata[i]) {
-      //       let tmp = Array.from(
-      //         leInt2Buff(unstringifyBigInts(mydata[i][j][0]), 32)
-      //       )
-      //         .concat(
-      //           Array.from(leInt2Buff(unstringifyBigInts(mydata[i][j][1]), 32))
-      //         )
-      //         .reverse();
-      //       mydata[i][j][0] = tmp.slice(0, 32);
-      //       mydata[i][j][1] = tmp.slice(32, 64);
-      //     }
-      //   } else if (i == "vk_delta_2") {
-      //     for (var j in mydata[i]) {
-      //       let tmp = Array.from(
-      //         leInt2Buff(unstringifyBigInts(mydata[i][j][0]), 32)
-      //       )
-      //         .concat(
-      //           Array.from(leInt2Buff(unstringifyBigInts(mydata[i][j][1]), 32))
-      //         )
-      //         .reverse();
-      //       mydata[i][j][0] = tmp.slice(0, 32);
-      //       mydata[i][j][1] = tmp.slice(32, 64);
-      //     }
-      //   } else if (i == "vk_alphabeta_12") {
-      //     for (var j in mydata[i]) {
-      //       for (var z in mydata[i][j]) {
-      //         for (var u in mydata[i][j][z]) {
-      //           mydata[i][j][z][u] = leInt2Buff(
-      //             unstringifyBigInts(mydata[i][j][z][u])
-      //           );
-      //         }
-      //       }
-      //     }
-      //   } else if (i == "IC") {
-      //     for (let j in mydata[i]) {
-      //       for (let z in mydata[i][j]) {
-      //         mydata[i][j][z] = leInt2Buff(
-      //           unstringifyBigInts(mydata[i][j][z]),
-      //           32
-      //         ).reverse();
-      //       }
-      //     }
-      //   }
-      // }
+      for (let i in mydata) {
+        if (i == "vk_alpha_1") {
+          for (let j in mydata[i]) {
+            mydata[i][j] = leInt2Buff(
+              unstringifyBigInts(mydata[i][j]),
+              32
+            ).reverse();
+          }
+        } else if (i == "vk_beta_2") {
+          for (let j in mydata[i]) {
+            let tmp = Array.from(
+              leInt2Buff(unstringifyBigInts(mydata[i][j][0]), 32)
+            )
+              .concat(
+                Array.from(leInt2Buff(unstringifyBigInts(mydata[i][j][1]), 32))
+              )
+              .reverse();
+            mydata[i][j][0] = tmp.slice(0, 32);
+            mydata[i][j][1] = tmp.slice(32, 64);
+          }
+        } else if (i == "vk_gamma_2") {
+          for (var j in mydata[i]) {
+            let tmp = Array.from(
+              leInt2Buff(unstringifyBigInts(mydata[i][j][0]), 32)
+            )
+              .concat(
+                Array.from(leInt2Buff(unstringifyBigInts(mydata[i][j][1]), 32))
+              )
+              .reverse();
+            mydata[i][j][0] = tmp.slice(0, 32);
+            mydata[i][j][1] = tmp.slice(32, 64);
+          }
+        } else if (i == "vk_delta_2") {
+          for (var j in mydata[i]) {
+            let tmp = Array.from(
+              leInt2Buff(unstringifyBigInts(mydata[i][j][0]), 32)
+            )
+              .concat(
+                Array.from(leInt2Buff(unstringifyBigInts(mydata[i][j][1]), 32))
+              )
+              .reverse();
+            mydata[i][j][0] = tmp.slice(0, 32);
+            mydata[i][j][1] = tmp.slice(32, 64);
+          }
+        } else if (i == "vk_alphabeta_12") {
+          for (var j in mydata[i]) {
+            for (var z in mydata[i][j]) {
+              for (var u in mydata[i][j][z]) {
+                mydata[i][j][z][u] = leInt2Buff(
+                  unstringifyBigInts(mydata[i][j][z][u])
+                );
+              }
+            }
+          }
+        } else if (i == "IC") {
+          for (let j in mydata[i]) {
+            for (let z in mydata[i][j]) {
+              mydata[i][j][z] = leInt2Buff(
+                unstringifyBigInts(mydata[i][j][z]),
+                32
+              ).reverse();
+            }
+          }
+        }
+      }
 
-      const processSubData = (subData: any) =>
+      /*const processSubData = (subData: any) =>
         leInt2Buff(unstringifyBigInts(subData), 32).reverse();
 
       const processDoubleSubData = (doubleSubData: any) => {
@@ -244,59 +244,61 @@ async function createVerifyingKeyRsFile(
             break;
         }
       }
+      */
 
-      // for (var path of paths) {
-      //   let resFile = await fs.openSync(path, "w");
+      for (var path of paths) {
+        let resFile = await fs.openSync(path, "w");
 
-      //   let s = `use groth16_solana::groth16::Groth16Verifyingkey;\nuse anchor_lang::prelude::*;\n\npub const VERIFYINGKEY: Groth16Verifyingkey = Groth16Verifyingkey {\n\tnr_pubinputs: ${mydata.IC.length},\n`;
-      //   s += "\tvk_alpha_g1: [\n";
-      //   for (let j = 0; j < mydata.vk_alpha_1.length - 1; j++) {
-      //     s += "\t\t" + Array.from(mydata.vk_alpha_1[j]) + ",\n";
-      //   }
-      //   s += "\t],\n\n";
-      //   fs.writeSync(resFile, s);
-      //   s = "\tvk_beta_g2: [\n";
-      //   for (let j = 0; j < mydata.vk_beta_2.length - 1; j++) {
-      //     for (let z = 0; z < 2; z++) {
-      //       s += "\t\t" + Array.from(mydata.vk_beta_2[j][z]) + ",\n";
-      //     }
-      //   }
-      //   s += "\t],\n\n";
-      //   fs.writeSync(resFile, s);
-      //   s = "\tvk_gamme_g2: [\n";
-      //   for (let j = 0; j < mydata.vk_gamma_2.length - 1; j++) {
-      //     for (let z = 0; z < 2; z++) {
-      //       s += "\t\t" + Array.from(mydata.vk_gamma_2[j][z]) + ",\n";
-      //     }
-      //   }
-      //   s += "\t],\n\n";
-      //   fs.writeSync(resFile, s);
+        let s = `use groth16_solana::groth16::Groth16Verifyingkey;\nuse anchor_lang::prelude::*;\n\npub const VERIFYINGKEY: Groth16Verifyingkey = Groth16Verifyingkey {\n\tnr_pubinputs: ${mydata.IC.length},\n`;
+        s += "\tvk_alpha_g1: [\n";
+        for (let j = 0; j < mydata.vk_alpha_1.length - 1; j++) {
+          s += "\t\t" + Array.from(mydata.vk_alpha_1[j]) + ",\n";
+        }
+        s += "\t],\n\n";
+        fs.writeSync(resFile, s);
+        s = "\tvk_beta_g2: [\n";
+        for (let j = 0; j < mydata.vk_beta_2.length - 1; j++) {
+          for (let z = 0; z < 2; z++) {
+            s += "\t\t" + Array.from(mydata.vk_beta_2[j][z]) + ",\n";
+          }
+        }
+        s += "\t],\n\n";
+        fs.writeSync(resFile, s);
+        s = "\tvk_gamme_g2: [\n";
+        for (let j = 0; j < mydata.vk_gamma_2.length - 1; j++) {
+          for (let z = 0; z < 2; z++) {
+            s += "\t\t" + Array.from(mydata.vk_gamma_2[j][z]) + ",\n";
+          }
+        }
+        s += "\t],\n\n";
+        fs.writeSync(resFile, s);
 
-      //   s = "\tvk_delta_g2: [\n";
-      //   for (let j = 0; j < mydata.vk_delta_2.length - 1; j++) {
-      //     for (let z = 0; z < 2; z++) {
-      //       s += "\t\t" + Array.from(mydata.vk_delta_2[j][z]) + ",\n";
-      //     }
-      //   }
-      //   s += "\t],\n\n";
-      //   fs.writeSync(resFile, s);
-      //   s = "\tvk_ic: &[\n";
-      //   let x = 0;
+        s = "\tvk_delta_g2: [\n";
+        for (let j = 0; j < mydata.vk_delta_2.length - 1; j++) {
+          for (let z = 0; z < 2; z++) {
+            s += "\t\t" + Array.from(mydata.vk_delta_2[j][z]) + ",\n";
+          }
+        }
+        s += "\t],\n\n";
+        fs.writeSync(resFile, s);
+        s = "\tvk_ic: &[\n";
+        let x = 0;
 
-      //   for (let ic in mydata.IC) {
-      //     s += "\t\t[\n";
-      //     for (let j = 0; j < mydata.IC[ic].length - 1; j++) {
-      //       s += "\t\t\t" + mydata.IC[ic][j] + ",\n";
-      //     }
-      //     x++;
-      //     s += "\t\t],\n";
-      //   }
-      //   s += "\t]\n};";
-      //   s += appendingString;
+        for (let ic in mydata.IC) {
+          s += "\t\t[\n";
+          for (let j = 0; j < mydata.IC[ic].length - 1; j++) {
+            s += "\t\t\t" + mydata.IC[ic][j] + ",\n";
+          }
+          x++;
+          s += "\t\t],\n";
+        }
+        s += "\t]\n};";
+        s += appendingString;
 
-      //   fs.writeSync(resFile, s);
-      // }
+        fs.writeSync(resFile, s);
+      }
 
+      /*
       for (const path of paths) {
         const resFile = await fs.open(path, "w");
         let s = `use groth16_solana::groth16::Groth16Verifyingkey;
@@ -342,7 +344,7 @@ async function createVerifyingKeyRsFile(
 
         await fs.write(resFile, s);
         await fs.close(resFile);
-      }
+      }*/
     }
   );
 }
