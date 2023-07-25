586cbddc5a6a79d12e90917f404a76dfd403af87
ananas-block <flow23@protonmail.com>
2023-07-25 21:53:01 +0100

    cli: release prep 0.1.1-alpha.13

--- cli/scripts/copyLocalProgramBinaries.sh
+++ cli/scripts/copyLocalProgramBinaries.sh
@@ -1,15 +1,12 @@
-
 #!/usr/bin/env sh
 
-declare -A keys=(
-    ["merkle_tree_program"]="9VtiN5ibfgg27WaxJNpbu23VcsETt6LiirPBcsVXjpsc"
-    ["verifier_program_zero"]="J1RRetZ4ujphU75LP8RadjXMf3sA12yC2R44CF7PmU7i"
-    ["verifier_program_one"]="J85SuNBBsba7FQS66BiBCQjiQrQTif7v249zL2ffmRZc"
-    ["verifier_program_two"]="2cxC8e8uNYLcymH6RTGuJs3N8fXGkwmMpw45pY65Ay86"
-    ["verifier_program_storage"]="2mNCqdntwtm9cTLjgfdS85JTF92mgNerqA9TgGnxFzLt"
-)
+keys="merkle_tree_program verifier_program_zero verifier_program_one verifier_program_two verifier_program_storage"
+
 mkdir -p bin/programs
 
-for key in "${!keys[@]}"; do
+for key in $keys
+do
     cp ../light-system-programs/target/deploy/$key.so ./bin/programs/$key.so
 done
+
+# cp ../test-env/programs/spl_noop.so ./bin/programs/spl_noop.so
--- cli/scripts/runTest.sh
+++ cli/scripts/runTest.sh
@@ -11,66 +11,28 @@ VERIFIER_PROGRAM_ONE_ID="J85SuNBBsba7FQS66BiBCQjiQrQTif7v249zL2ffmRZc"
 VERIFIER_PROGRAM_TWO_ID="2cxC8e8uNYLcymH6RTGuJs3N8fXGkwmMpw45pY65Ay86"
 MOCK_VERIFIER_PROGRAM_ID="Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"
 
+# process id on mac is solana-test-val
+killall solana-test-validator || true
+# process id on ubuntu is solana-test-val
+killall solana-test-val || true
+# sleep for the process to be killed
+sleep 1
+solana-test-validator \
+    --reset \
+    --limit-ledger-size=$LIMIT_LEDGER_SIZE \
+    --quiet \
+    --bpf-program $NOOP_PROGRAM_ID ./bin/programs/spl_noop.so \
+    --bpf-program $MERKLE_TREE_PROGRAM_ID ./bin/programs/merkle_tree_program.so \
+    --bpf-program $VERIFIER_PROGRAM_ZERO_ID ./bin/programs/verifier_program_zero.so \
+    --bpf-program $VERIFIER_PROGRAM_STORAGE_ID ./bin/programs/verifier_program_storage.so \
+    --bpf-program $VERIFIER_PROGRAM_ONE_ID ./bin/programs/verifier_program_one.so \
+    --bpf-program $VERIFIER_PROGRAM_TWO_ID ./bin/programs/verifier_program_two.so \
+    --account-dir ./bin/accounts \
+    --bpf-program $3 ./target/deploy/$4\
+    &
+PID=$!
 
-docker rm -f solana-validator
+sleep 7
+$5
 
-if [ -f /.dockerenv ]; then
-    solana-test-validator \
-        --reset \
-        --limit-ledger-size=$LIMIT_LEDGER_SIZE \
-        --quiet \
-        --bpf-program $NOOP_PROGRAM_ID ~/.local/light-protocol/lib/solana-program-library/spl_noop.so \
-        --bpf-program $MERKLE_TREE_PROGRAM_ID ./target/deploy/merkle_tree_program.so \
-        --bpf-program $VERIFIER_PROGRAM_ZERO_ID ./target/deploy/verifier_program_zero.so \
-        --bpf-program $VERIFIER_PROGRAM_STORAGE_ID ./target/deploy/verifier_program_storage.so \
-        --bpf-program $VERIFIER_PROGRAM_ONE_ID ./target/deploy/verifier_program_one.so \
-        --bpf-program $VERIFIER_PROGRAM_TWO_ID ./target/deploy/verifier_program_two.so \
-        --account-dir ../../test-env/accounts \
-        --bpf-program $3 /usr/local/lib/test_programs/$4\
-        &
-    PID=$!
-
-    sleep 7
-    $5
-
-    kill $PID
-else
-    docker rm -f solana-validator || true
-    docker run -d \
-        --name solana-validator \
-        --pull=always \
-        -p 8899:8899 \
-        -p 8900:8900 \
-        -p 8901:8901 \
-        -p 8902:8902 \
-        -p 9900:9900 \
-        -p 8000:8000 \
-        -p 8001:8001 \
-        -p 8002:8002 \
-        -p 8003:8003 \
-        -p 8004:8004 \
-        -p 8005:8005 \
-        -p 8006:8006 \
-        -p 8007:8007 \
-        -p 8008:8008 \
-        -p 8009:8009 \
-        -v $HOME/.config/solana/id.json:/home/node/.config/solana/id.json \
-        -v $2/target/deploy:/usr/local/lib/test_programs \
-        ghcr.io/lightprotocol/solana-test-validator:main \
-        --reset \
-        --limit-ledger-size=$LIMIT_LEDGER_SIZE \
-        --quiet \
-        --bpf-program $NOOP_PROGRAM_ID /home/node/.local/light-protocol/lib/solana-program-library/spl_noop.so \
-        --bpf-program $MERKLE_TREE_PROGRAM_ID /home/node/.local/light-protocol/lib/light-protocol/merkle_tree_program.so \
-        --bpf-program $VERIFIER_PROGRAM_ZERO_ID /home/node/.local/light-protocol/lib/light-protocol/verifier_program_zero.so \
-        --bpf-program $VERIFIER_PROGRAM_STORAGE_ID /home/node/.local/light-protocol/lib/light-protocol/verifier_program_storage.so \
-        --bpf-program $VERIFIER_PROGRAM_ONE_ID /home/node/.local/light-protocol/lib/light-protocol/verifier_program_one.so \
-        --bpf-program $VERIFIER_PROGRAM_TWO_ID /home/node/.local/light-protocol/lib/light-protocol/verifier_program_two.so \
-        --account-dir /home/node/.local/light-protocol/lib/accounts \
-        --bpf-program $3 /usr/local/lib/test_programs/$4\
-
-    sleep 15
-    $5
-
-    docker rm -f solana-validator
-fi
+kill $PID
