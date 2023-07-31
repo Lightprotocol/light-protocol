41426c2d263078a3d5db2ce2822cfd85cdca0d49
vadorovsky <vadorovsky@protonmail.com>
2023-07-31 22:05:11 +0200

    chore: Remove mock-app-verifier (#208)
    We are not going to use it anymore. Instead, the PSP template should
    be an indicator whether application verifiers work.
    
    Co-authored-by: ananas-block <flow23@protonmail.com>

--- mock-app-verifier/scripts/createRustVerifyingKey.ts
+++
@@ -1,9 +0,0 @@
-import { createVerfyingkeyRsFileArgv } from "../../cli/src/utils/createRustVerifyingKey";
-
-async function run() {
-  await createVerfyingkeyRsFileArgv();
-}
-
-run().catch((e) => {
-  throw new Error(e);
-});
