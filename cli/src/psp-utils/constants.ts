afe64fd28ec251eed8f8176a7cc9069f8bb26958
ananas-block <58553958+ananas-block@users.noreply.github.com>
2023-09-18 21:44:52 +0100

    refactor: build script & remove relative path imports (#260)
    * refactored build sh script and removed relative paths
    
    * versions: bumped versions
    
    * switch branches to main
    
    * skip yarn link

--- cli/src/psp-utils/constants.ts
+++ cli/src/psp-utils/constants.ts
@@ -1,8 +1,8 @@
 export const PSP_TEMPLATE_TAG = "v0.1.2";
 export const PROGRAM_TAG = "v0.3.2";
 export const MACRO_CIRCOM_TAG = "v0.1.6";
-export const ZK_JS_VERSION = "file:../light-zk.js";
-export const PROVER_JS_VERSION = "file:../light-prover.js";
+export const ZK_JS_VERSION = "0.3.2-alpha.15";
+export const PROVER_JS_VERSION = "0.1.0-alpha.2";
 export const PSP_DEFAULT_PROGRAM_ID =
   "Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS";
 export const LIGHT_SYSTEM_PROGRAMS_VERSION =
