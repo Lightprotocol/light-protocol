75a1d285c9758c90ed001ea274dd8fbe8f7702d0
Sergey Timoshin <sergeytimoshin@proton.me>
2023-09-18 13:30:02 +0100

    fix TS2532: Object is possibly undefined

--- light-zk.js/src/test-utils/userTestAssertHelper.ts
+++ light-zk.js/src/test-utils/userTestAssertHelper.ts
@@ -1171,14 +1171,15 @@ export class UserTestAssertHelper {
         balanceSpendable.tokenBalances.get(this.tokenCtx.mint.toBase58())?.utxos
           .size ?? 0;
     }
+    console.log("balance", balanceSpendable.tokenBalances);
 
     assert.equal(
-      balanceSpendable.tokenBalances.get(this.tokenCtx.mint.toBase58())
+      balanceSpendable.tokenBalances.get(this.tokenCtx.mint.toBase58())!
         .committedUtxos.size,
       0,
     );
     assert.equal(
-      balanceSpendable.tokenBalances.get(this.tokenCtx.mint.toBase58()).utxos
+      balanceSpendable.tokenBalances.get(this.tokenCtx.mint.toBase58())!.utxos
         .size,
       1,
     );
