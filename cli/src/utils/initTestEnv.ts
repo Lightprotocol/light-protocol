c555da72827373ccc9689562188c057151b9924e
ananas-block <58553958+ananas-block@users.noreply.github.com>
2023-09-19 07:15:44 +0100

    fix: cli program bin download, test-validator background flag (#236)
    * fix: background flag
    
    * cli: fixed cli tests
    
    * cli - tests: added bigger timeout

--- cli/src/utils/initTestEnv.ts
+++ cli/src/utils/initTestEnv.ts
@@ -20,34 +20,45 @@ const find = require("find-process");
 export async function initTestEnv({
   additonalPrograms,
   skip_system_accounts,
+  background = true,
 }: {
   additonalPrograms?: { address: string; path: string }[];
   skip_system_accounts?: boolean;
+  background?: boolean;
 }) {
   console.log("Performing setup tasks...\n");
 
-  await start_test_validator({ additonalPrograms, skip_system_accounts });
-  const anchorProvider = await setAnchorProvider();
-  await airdropSol({
-    connection: anchorProvider.connection,
-    lamports: 100e9,
-    recipientPublicKey: ADMIN_AUTH_KEYPAIR.publicKey,
-  });
+  const initAccounts = async () => {
+    await sleep(10000);
+    const anchorProvider = await setAnchorProvider();
+    await airdropSol({
+      connection: anchorProvider.connection,
+      lamports: 100e9,
+      recipientPublicKey: ADMIN_AUTH_KEYPAIR.publicKey,
+    });
 
-  await createTestAccounts(anchorProvider.connection);
+    await createTestAccounts(anchorProvider.connection);
 
-  const lookupTable = await initLookUpTableFromFile(anchorProvider);
+    const lookupTable = await initLookUpTableFromFile(anchorProvider);
 
-  setLookUpTable(lookupTable.toString());
+    setLookUpTable(lookupTable.toString());
 
-  const relayerRecipientSol = Keypair.generate().publicKey;
+    const relayerRecipientSol = Keypair.generate().publicKey;
 
-  setRelayerRecipient(relayerRecipientSol.toString());
+    setRelayerRecipient(relayerRecipientSol.toString());
 
-  await anchorProvider.connection.requestAirdrop(
-    relayerRecipientSol,
-    2_000_000_000
-  );
+    await anchorProvider.connection.requestAirdrop(
+      relayerRecipientSol,
+      2_000_000_000
+    );
+  };
+  initAccounts();
+  if (!background) {
+    await start_test_validator({ additonalPrograms, skip_system_accounts });
+  } else {
+    start_test_validator({ additonalPrograms, skip_system_accounts });
+    await sleep(15000);
+  }
 }
 
 export async function initTestEnvIfNeeded({
@@ -63,7 +74,11 @@ export async function initTestEnvIfNeeded({
     await anchorProvider.connection.getBalance(ADMIN_AUTH_KEY);
   } catch (error) {
     // launch local test validator and initialize test environment
-    await initTestEnv({ additonalPrograms, skip_system_accounts });
+    await initTestEnv({
+      additonalPrograms,
+      skip_system_accounts,
+      background: true,
+    });
   }
 }
 
@@ -142,11 +157,10 @@ export async function start_test_validator({
 
   await new Promise((r) => setTimeout(r, 1000));
 
-  executeCommand({
+  await executeCommand({
     command,
     args: [...solanaArgs],
   });
-  await sleep(10000);
 }
 
 export async function killTestValidator() {
