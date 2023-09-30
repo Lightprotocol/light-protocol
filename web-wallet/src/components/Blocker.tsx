83fc05bab40b795736b101c392ff401f1420f05b
Swen <swen.schaeferjohann@code.berlin>
2023-09-30 18:10:38 +0100

    rm old

--- web-wallet/src/components/Blocker.tsx
+++
@@ -1,8 +0,0 @@
-import { Pane } from "evergreen-ui";
-export const Blocker = () => (
-  <Pane>
-    you've been temporarily logged out because your wallet seems to be connected
-    to illicit activity. If you think this is a mistake, please
-    <a href="mailto:friends@lightprotocol.com"> contact us</a>
-  </Pane>
-);
