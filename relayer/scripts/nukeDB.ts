79462b2a521912aadae0e1f13ea77b50ff87d7c2
Swen <swen.schaeferjohann@code.berlin>
2023-10-09 18:31:40 +0100

    rm markers

--- relayer/scripts/nukeDB.ts
+++ relayer/scripts/nukeDB.ts
@@ -1,15 +1,5 @@
 // nuke redis db entries
-
 import { sleep } from "@lightprotocol/zk.js";
-<<<<<<<< HEAD:relayer/src/nukeDB.ts
-import { DB_VERSION } from "./config";
-import { getTransactions } from "./db/redis";
-
-(async () => {
-  console.log("NUKING DB IN 10 SECONDS!");
-  await sleep(1 * 1000);
-  const { job } = await getTransactions(DB_VERSION);
-========
 import { DB_VERSION } from "../src/config";
 import { getTransactions } from "../src/db/redis";
 
@@ -17,7 +7,6 @@ import { getTransactions } from "../src/db/redis";
   console.log("NUKING DB IN 5 SECONDS!");
   await sleep(5 * 1000);
   let { job } = await getTransactions(DB_VERSION);
->>>>>>>> 03523498 (add circom circuits):relayer/scripts/nukeDB.ts
 
   await job.updateData({ transactions: [] });
   const { job: job2 } = await getTransactions(DB_VERSION);
