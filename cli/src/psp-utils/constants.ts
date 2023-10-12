1f5aa63250426b8ac55f647b5f718e226b4e2122
ananas-block <58553958+ananas-block@users.noreply.github.com>
2023-10-12 03:36:02 +0100

    fix merkle tree import to commit (#329)

--- cli/src/psp-utils/constants.ts
+++ cli/src/psp-utils/constants.ts
@@ -9,13 +9,13 @@ export const CIRCUIT_LIB_CIRCOM_VERSION =
   "file:../circuit-lib/circuit-lib.circom"; //"0.1.0-alpha.1";
 export const PSP_DEFAULT_PROGRAM_ID =
   "Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS";
-export const LIGHT_SYSTEM_PROGRAMS_VERSION =
-  '{ git = "https://github.com/lightprotocol/light-protocol", features = ["cpi"], branch = "jorrit/rename-verifier-two" }';
 export const LIGHT_SYSTEM_PROGRAM = "light-psp4in4out-app-storage";
+export const LIGHT_SYSTEM_PROGRAMS_VERSION =
+  '{ git = "https://github.com/lightprotocol/light-protocol", features = ["cpi"], rev = "cf88fdedc3596a78f1e04a08695a29cb0f1b607f" }';
 export const LIGHT_MACROS_VERSION =
-  '{ git = "https://github.com/lightprotocol/light-protocol", branch = "jorrit/rename-verifier-two" }';
+  '{ git = "https://github.com/lightprotocol/light-protocol", rev = "cf88fdedc3596a78f1e04a08695a29cb0f1b607f" }';
 export const LIGHT_VERIFIER_SDK_VERSION =
-  '{ git = "https://github.com/lightprotocol/light-protocol", branch = "jorrit/rename-verifier-two" }';
+  '{ git = "https://github.com/lightprotocol/light-protocol", rev = "cf88fdedc3596a78f1e04a08695a29cb0f1b607f" }';
 export const CONFIG_PATH = "/.config/light/";
 export const CONFIG_FILE_NAME = "config.json";
 
