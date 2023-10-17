05a014ae53996b410e1f1391c5b22fadb2e45bc5
Swen Schäferjohann <42959314+SwenSchaeferjohann@users.noreply.github.com>
2023-10-17 13:20:52 +0100

    fix: test.sh script (#337)
    * source is not compatible with all shells, fix: use "." instead
    * move testDockerRelayer.sh to the end of test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-# run relayer docker build script
-source $(dirname $0)/testDockerRelayer.sh
-
 npx nx run-many --target=test --all --parallel=false
+
+# run relayer docker build script
+. $(dirname $0)/testDockerRelayer.sh
