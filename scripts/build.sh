3514e28301bf6a44487c890220b421de713e6f84
Swen Schäferjohann <42959314+SwenSchaeferjohann@users.noreply.github.com>
2023-10-18 01:23:55 +0100

    wip: add build (#342)

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,3 +8,6 @@ command -v npx >/dev/null 2>&1 || { echo >&2 "npx is not installed.  Aborting.";
 set -eux
 
 pnpm install || { echo >&2 "Failed to install dependencies. Aborting."; exit 1; }
+
+npx nx run-many --target=build --all
+
