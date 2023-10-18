72b4c1aaa2d735d379d34feb31b60dab679aced1
Sergey Timoshin <sergeytimoshin@proton.me>
2023-10-18 10:14:45 +0100

    Changed the command 'run-many' to 'affected' in test.sh and build.sh scripts for optimizing test and build process.

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,4 @@ set -eux
 
 pnpm install || { echo >&2 "Failed to install dependencies. Aborting."; exit 1; }
 
-npx nx run-many --target=build --all
-
+npx nx affected --target=build
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-npx nx run-many --target=test --all --parallel=false
+npx nx affected --target=test --parallel=false
 
 # run relayer docker build script
 . $(dirname $0)/testDockerRelayer.sh
