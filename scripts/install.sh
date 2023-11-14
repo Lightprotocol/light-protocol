0e2c4e7fbb297ae51a66b1e872b268d8b9069cd0
Sergey Timoshin <sergeytimoshin@proton.me>
2023-11-14 23:45:16 +0000

    chore: updated node version to 20.9.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -108,7 +108,7 @@ check_flag() {
     echo false
 }
 
-NODE_VERSION="16.20.1"
+NODE_VERSION="20.9.0"
 PNPM_VERSION="8.8.0"
 SOLANA_VERSION="1.16.13"
 ANCHOR_VERSION=`latest_release Lightprotocol anchor`
