fd7f90e50198a136c8b7ee107732780211fb09a5
Swen <swen.schaeferjohann@code.berlin>
2023-11-16 17:00:36 +0000

    added prebuild to webapp, split precompute (culprit) from build

--- circuit-lib/circuit-lib.js/src/elgamal-babyjubjub/index.ts
+++ circuit-lib/circuit-lib.js/src/elgamal-babyjubjub/index.ts
@@ -1,2 +1,5 @@
 export * from "./elgamal";
 export * from "./pointEncoding";
+/// we're not default exporting precompute because it's a node only process.
+/// that's fine because precompute is only consumed in elgamal.test.ts
+/// we may change this when migrating to a bundler for zk.js
