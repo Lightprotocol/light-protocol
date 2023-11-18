6e6af02cb07466f01541bbf8e5473083a1164532
Sergey Timoshin <sergeytimoshin@proton.me>
2023-11-18 10:14:32 +0000

    formatting + tests

--- relayer/tests/browser_test.ts
+++ relayer/tests/browser_test.ts
@@ -23,7 +23,7 @@ import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 import { waitForBalanceUpdate } from "./test-utils/waitForBalanceUpdate";
 import { RPC_URL } from "../src/config";
 import { getRelayer } from "../src/utils/provider";
-import {Poseidon} from "@lightprotocol/account.rs";
+import { Poseidon } from "@lightprotocol/account.rs";
 
 chai.use(chaiHttp);
 
