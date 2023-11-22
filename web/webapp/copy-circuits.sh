21f9e874fa60c67391a917a4d465ad4953918bca
Swen <swen.schaeferjohann@code.berlin>
2023-11-22 14:50:26 +0000

    add sync, rm sleeps, rm chmods

--- web/webapp/copy-circuits.sh
+++ web/webapp/copy-circuits.sh
@@ -3,12 +3,9 @@ set -eux
 
 echo "ensure that all node_modules are installed before running copy-circuits!"
 
-# copy build-circuits from zkjs into webapp/public
 SOURCE="./node_modules/@lightprotocol/zk.js/build-circuits/"
 DESTINATION="./public/"
-
 cp -LR "$SOURCE" "$DESTINATION"
+sync "$DESTINATION"
 
 echo "Copied circuit files to $DESTINATION"
-
-sleep 5
