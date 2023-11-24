d3f9fec0866ba7cc30989ff0f3feb69d35e48b88
Sergey Timoshin <sergeytimoshin@proton.me>
2023-11-24 12:18:49 +0000

    wip

--- account.rs/zk-rs/scripts/build.sh
+++ account.rs/zk-rs/scripts/build.sh
@@ -1,33 +1,7 @@
-#!/usr/bin/env bash
+rm -rf pkg/node
+rm -rf pkg/bundler
+rm -rf pkg/web
 
-set -e
-
-# Check if jq is installed
-if ! [ -x "$(command -v jq)" ]; then
-    echo "jq is not installed" >& 2
-    exit 1
-fi
-
-# Clean previous packages
-if [ -d "pkg" ]; then
-    rm -rf pkg
-fi
-
-if [ -d "pkg-node" ]; then
-    rm -rf pkg-node
-fi
-
-# Build for both targets
-wasm-pack build -t nodejs -d pkg-node
-wasm-pack build -t browser -d pkg
-
-# Get the package name
-PKG_NAME=$(jq -r .name pkg/package.json | sed 's/\-/_/g')
-echo "PKG_NAME = "$PKG_NAME
-# Merge nodejs & browser packages
-cp "pkg-node/${PKG_NAME}.js" "pkg/${PKG_NAME}_main.js"
-#sed "s/require[\(]'\.\/${PKG_NAME}/require\('\.\/${PKG_NAME}_main/" "pkg-node/${PKG_NAME}_bg.js" > "pkg/${PKG_NAME}_bg.js"
-jq ".files += [\"${PKG_NAME}_bg.js\"]" pkg/package.json \
-    | jq ".main = \"${PKG_NAME}_main.js\"" > pkg/temp.json
-mv pkg/temp.json pkg/package.json
-rm -rf pkg-node
+wasm-pack build --target bundler --out-dir pkg/bundler  
+wasm-pack build --target nodejs --out-dir pkg/node
+wasm-pack build --target web --out-dir pkg/web
