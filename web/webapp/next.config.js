a66cad590edebfbb493bf72d6f3f584adc2d5b3e
Sergey Timoshin <sergeytimoshin@proton.me>
2023-11-24 16:29:20 +0000

    wip

--- web/webapp/next.config.js
+++ web/webapp/next.config.js
@@ -12,11 +12,11 @@ const nextConfig = {
     distDir: "dist",
     cleanDistDir: false,
     webpack: (config, {isServer}) => {        
-        config.plugins.push(new CopyPlugin({
-            patterns: [
-                { from: "public/wasm", to: "./static/wasm" },
-            ],
-        }))
+        // config.plugins.push(new CopyPlugin({
+        //     patterns: [
+        //         { from: "public/wasm", to: "./static/wasm" },
+        //     ],
+        // }))
         // Fixes npm packages that depend on `fs` module
         if (!isServer) {
             config.resolve.fallback = {
@@ -24,7 +24,7 @@ const nextConfig = {
                 child_process: false,
                 readline: false,
             };
-        }        
+        }                
         patchWasmModuleImport(config, isServer);
         return config;
     },
@@ -42,12 +42,12 @@ function patchWasmModuleImport(config, isServer) {
         type: 'webassembly/async',
     });
 
-    // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
-    if (isServer) {
-        config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
-    } else {
-        config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
-    }
+    // // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
+    // if (isServer) {
+    //     config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    // } else {
+    //     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    // }
 }
 
 module.exports = nextConfig;
