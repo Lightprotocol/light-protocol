e9da10271f625b6e67fe92d46b3f1618b76c2bae
Sergey Timoshin <sergeytimoshin@proton.me>
2023-11-26 13:47:12 +0000

    Update import and dependencies for 'assert' package

--- circuit-lib/circuit-lib.js/src/elgamal-babyjubjub/maci-crypto/maci-crypto-utils.ts
+++ circuit-lib/circuit-lib.js/src/elgamal-babyjubjub/maci-crypto/maci-crypto-utils.ts
@@ -23,7 +23,7 @@
  */
 
 import * as crypto from "crypto";
-import * as assert from "node:assert";
+import * as assert from "assert";
 const eddsa = require("./eddsa");
 const ff = require("ffjavascript");
 const createBlakeHash = require("blake-hash");
