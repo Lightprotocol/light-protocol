74bd33879cf084702c9c6f55e7309cc6732fe8f8
Sergey Timoshin <sergeytimoshin@proton.me>
2023-11-28 01:44:18 +0000

    bundling enabled for prover, circom-lib.js & zk.js

---
+++ prover.js/tsup.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, Options } from 'tsup';
+
+export default defineConfig(options => {
+    const commonOptions: Partial<Options> = {
+        entry: {
+            'prover.js': 'src/index.ts'
+        },
+        sourcemap: true,
+        ...options
+    }
+
+    return [
+        // Modern ESM
+        {
+            ...commonOptions,
+            format: ['esm'],
+            outExtension: () => ({ js: '.mjs' }),
+            clean: true,
+            dts: true
+        },
+
+        // Browser-ready ESM, production + minified
+        {
+            ...commonOptions,
+            entry: {
+                'prover.js.browser': 'src/index.ts'
+            },
+            define: {
+                'process.env.NODE_ENV': JSON.stringify('production')
+            },
+            format: ['esm'],
+            outExtension: () => ({ js: '.mjs' }),
+            minify: true
+        },
+        {
+            ...commonOptions,
+            format: 'cjs',
+            outDir: './dist/cjs/',
+            outExtension: () => ({ js: '.cjs' })
+        }
+    ]
+})
---
+++ zk.js/tsup.config.ts
@@ -0,0 +1,65 @@
+import { defineConfig, Options } from 'tsup';
+import fs from 'fs';
+import sh from 'shelljs';
+import type { ExecOptions } from 'shelljs';
+
+function execAsync(cmd: string, opts: ExecOptions = {}) {
+    return new Promise(function (resolve, reject) {
+        // Execute the command, reject if we exit non-zero (i.e. error)
+        sh.exec(cmd, opts, function (code, stdout, stderr) {
+            if (code !== 0) return reject(new Error(stderr))
+            return resolve(stdout)
+        })
+    })
+}
+
+export default defineConfig(options => {
+    const commonOptions: Partial<Options> = {
+        entry: {
+            'zk.js': 'src/index.ts'
+        },
+        sourcemap: true,
+        ...options
+    }
+
+    return [
+        // Modern ESM
+        {
+            ...commonOptions,
+            format: ['esm'],
+            outExtension: () => ({ js: '.mjs' }),
+            dts: true,
+            clean: true
+        },
+        // // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
+        // // and optional chaining compiled away
+        // {
+        //     ...commonOptions,
+        //     entry: {
+        //         'zk.js.legacy-esm': 'src/index.ts'
+        //     },
+        //     format: ['esm'],
+        //     outExtension: () => ({ js: '.js' }),
+        //     target: 'es2017'
+        // },
+        // Browser-ready ESM, production + minified
+        {
+            ...commonOptions,
+            entry: {
+                'zk.js.browser': 'src/index.ts'
+            },
+            define: {
+                'process.env.NODE_ENV': JSON.stringify('production')
+            },
+            format: ['esm'],
+            outExtension: () => ({ js: '.mjs' }),
+            minify: true
+        },
+        {
+            ...commonOptions,
+            format: 'cjs',
+            outDir: './dist/cjs/',
+            outExtension: () => ({ js: '.cjs' })
+        }
+    ]
+})
