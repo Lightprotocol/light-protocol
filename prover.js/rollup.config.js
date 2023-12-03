188608ea985071031f000de22a69f496cd6395b2
Sergey Timoshin <sergeytimoshin@proton.me>
2023-12-03 10:54:10 +0000

    formatting

--- prover.js/rollup.config.js
+++ prover.js/rollup.config.js
@@ -2,33 +2,27 @@ import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
 const rolls = (fmt) => ({
-    input: 'src/index.ts',
-    output: {
-        dir: "dist",
-        format: fmt,
-        entryFileNames: `${fmt}/[name].${
-            fmt === "cjs" ? "cjs" : "js"
-        }`,
-        name: pkg.name,
-        globals: {
-            "@coral-xyz/anchor": "anchor",
-            "circomlibjs": "circomlibjs",
-            "ffjavascript": "ffjavascript",
-            "snarkjs": "snarkjs",
-        }
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: fmt,
+    entryFileNames: `${fmt}/[name].${fmt === "cjs" ? "cjs" : "js"}`,
+    name: pkg.name,
+    globals: {
+      "@coral-xyz/anchor": "anchor",
+      circomlibjs: "circomlibjs",
+      ffjavascript: "ffjavascript",
+      snarkjs: "snarkjs",
     },
-    external: ["@coral-xyz/anchor", "circomlibjs", "ffjavascript", "snarkjs"],
-    plugins: [
-        typescript({
-            target: fmt === "es" ? "ES2022" : "ES2017",
-            outDir: `dist/${fmt}`,
-            rootDir: "src",
-        })        
-    ],
+  },
+  external: ["@coral-xyz/anchor", "circomlibjs", "ffjavascript", "snarkjs"],
+  plugins: [
+    typescript({
+      target: fmt === "es" ? "ES2022" : "ES2017",
+      outDir: `dist/${fmt}`,
+      rootDir: "src",
+    }),
+  ],
 });
 
-export default [
-    rolls("umd"),
-    rolls("cjs"),
-    rolls("es")
-];
+export default [rolls("umd"), rolls("cjs"), rolls("es")];
