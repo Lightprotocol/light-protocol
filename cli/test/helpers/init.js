1c44219e4026ef5bf97dd30dc72152b90785fd44
Mike Rostecki <vadorovsky@protonmail.com>
2023-12-04 23:22:53 +0000

    test(cli): Check for Solana cluster type using Solana CLI

--- cli/test/helpers/init.js
+++ cli/test/helpers/init.js
@@ -1,28 +1,12 @@
-const fs = require("fs");
+const { execSync } = require('child_process');
 const path = require("path");
-const yaml = require("js-yaml");
 
 process.env.TS_NODE_PROJECT = path.resolve("tsconfig.test.json");
 process.env.NODE_ENV = "development";
 
 function isLocalnet() {
-  const homeDir = process.env.HOME || process.env.USERPROFILE;
-  const configPath = path.join(
-    homeDir,
-    ".config",
-    "solana",
-    "cli",
-    "config.yml",
-  );
-  const configFile = fs.readFileSync(configPath, "utf8");
-  const config = yaml.load(configFile);
-
-  if (config && typeof config === "object" && "json_rpc_url" in config) {
-    const rpcUrl = config["json_rpc_url"];
-    return rpcUrl.includes("localhost") || rpcUrl.includes("127.0.0.1");
-  }
-
-  throw Error("Failed to determine Solana cluster type");
+  const output = execSync('solana config get', { encoding: 'utf8' });
+  return output.includes('localhost') || output.includes('127.0.0.1');
 }
 
 // For localnet, enable atomic transactions if they weren't explicitly
