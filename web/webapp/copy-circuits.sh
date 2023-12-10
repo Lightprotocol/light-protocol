f4fb2556aa7095f9aa684a7d33d6eea83599ff8b
Sergey Timoshin <sergeytimoshin@proton.me>
2023-12-10 23:48:58 +0000

    fix: update source path in copy-circuits.sh
    • webapp expect existence of `public/build-circuits/ path. If we enable -R flag for `cp`, and source_file for `cp` ends in a /, the contents of the directory are copied rather than the directory itself. This is not what we expect.
    • .gitignore updated, /web/webapp/public/build-circuits included.

--- web/webapp/copy-circuits.sh
+++ web/webapp/copy-circuits.sh
@@ -3,7 +3,7 @@ set -eux
 
 echo "ensure that all node_modules are installed before running copy-circuits!"
 
-SOURCE="./node_modules/@lightprotocol/zk.js/build-circuits/"
+SOURCE="./node_modules/@lightprotocol/zk.js/build-circuits"
 DESTINATION="./public/"
 cp -LR "$SOURCE" "$DESTINATION"
 sync "$DESTINATION"
