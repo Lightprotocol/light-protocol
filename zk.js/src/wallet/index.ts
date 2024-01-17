5d21c1863ceab7720b5db7c25204011215eebebc
Swen <swen.schaeferjohann@code.berlin>
2024-01-17 18:53:57 +0000

    rm dupe exports

--- zk.js/src/wallet/index.ts
+++ zk.js/src/wallet/index.ts
@@ -1,6 +1,2 @@
-export * from "../build-balance";
 export * from "./user";
-export * from "../provider";
-export * from "../utxo/create-output-utxos";
-export * from "../utxo/select-input-utxos";
 export * from "./use-wallet";
