39968b09a5da24bb0644f02dd1c28ba3671688e8
Michal Rostecki <vadorovsky@protonmail.com>
2024-02-06 20:25:12 +0100

    ci: Pin Rust nightly to 2024-02-01 (#465)
    The newest Rust nightly removed the `stdsimd` feature in favor of
    more grained features (rust-lang/rust#117372).
    
    However, the version of `ahash` we depend on, still requires that
    feature. New release of `ahash` (0.8.7) contains a fix, but we can't
    upgrade to it without upgrading Anchor.
    
    Pinning Rust nightly seems to be the less intrusive solution for now.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,7 +4,7 @@ set -e
 npx nx run-many --target=format:check --all
 npx nx run-many --target=lint --all
 
-for rust_toolchain in stable nightly; do
+for rust_toolchain in stable nightly-2024-02-01; do
     cargo +"$rust_toolchain" fmt --all -- --check
     cargo +"$rust_toolchain" clippy \
       --workspace \
