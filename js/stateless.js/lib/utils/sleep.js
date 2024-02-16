c1973bab5f4af45bfa5ab955067a1d82f53c406c
Swen <swen.schaeferjohann@code.berlin>
2024-02-16 16:25:42 +0000

    rm lib

--- js/stateless.js/lib/utils/sleep.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-var sleep = function (ms) {
-    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
-};
-exports.sleep = sleep;
