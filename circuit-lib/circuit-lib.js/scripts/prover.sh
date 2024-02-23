720fa70c059eea0998baaa7e426dd8f3d71c1830
Sergey Timoshin <sergeytimoshin@proton.me>
2024-02-23 22:44:55 +0000

    Add gnark-prover submodule and typescript prover test (#483)

---
+++ circuit-lib/circuit-lib.js/scripts/prover.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+killall light-prover
+
+# Get the root directory of the Git repository
+root_dir=$(git rev-parse --show-toplevel)
+
+# Change the directory to 'gnark-prover' within the Git root directory
+# shellcheck disable=SC2164
+cd "$root_dir/gnark-prover"
+
+# If 'gnark-prover' directory does not exist, print error and exit
+if [ $? -ne 0 ]; then
+    echo "Directory gnark-prover does not exist in the Git root directory. Run \`git submodule update --init\` to fetch the submodule."
+    exit 1
+fi
+
+go build
+# Execute 'go run main.go start'
+./light-prover start &
---
+++ circuit-lib/circuit-lib.js/tests/gnark.test.ts
@@ -0,0 +1,73 @@
+import { MerkleTree } from "../src";
+import { LightWasm, WasmFactory } from "@lightprotocol/account.rs";
+import axios from "axios";
+import { BN } from "@coral-xyz/anchor";
+import { assert } from "chai";
+
+describe("Tests", () => {
+  let lightWasm: LightWasm;
+  const MAX_RETRIES = 20;
+  const DELAY_MS = 5000;
+  const SERVER_URL = "http://localhost:3001";
+  const PROVE_URL = `${SERVER_URL}/prove`;
+  const HEALTH_CHECK_URL = `${SERVER_URL}/health`;
+
+  async function pingServer(serverUrl: string) {
+    for (let attempt = 0; attempt < MAX_RETRIES; attempt++) {
+      try {
+        const response = await axios.get(serverUrl);
+        if (response.status === 200) {
+          console.log("Server is up!");
+          break;
+        }
+      } catch (error) {
+        console.log(
+          `Attempt ${attempt + 1} failed. Retrying in ${
+            DELAY_MS / 1000
+          } seconds...`,
+        );
+      }
+      await new Promise((res) => setTimeout(res, DELAY_MS)); // Wait before retrying
+    }
+  }
+
+  before(async () => {
+    lightWasm = await WasmFactory.getInstance();
+  });
+
+  it("merkle proofgen", async () => {
+    await pingServer(HEALTH_CHECK_URL);
+
+    const hasher = await WasmFactory.getInstance();
+    const merkleHeights = [26];
+    const utxos = [1, 2, 3, 4, 8];
+    for (let i = 0; i < merkleHeights.length; i++) {
+      for (let j = 0; j < utxos.length; j++) {
+        const leaf = hasher.poseidonHashString(["1"]);
+        const merkleTree = new MerkleTree(merkleHeights[i], hasher, [leaf]);
+
+        const pathEements: string[] = merkleTree.path(
+          merkleTree.indexOf(leaf),
+        ).pathElements;
+        const hexPathElements = pathEements.map((value) => toHex(value));
+        let inputs = {
+          root: new Array(utxos[j]).fill(toHex(merkleTree.root())),
+          inPathIndices: new Array(utxos[j]).fill(merkleTree.indexOf(leaf)),
+          inPathElements: new Array(utxos[j]).fill(hexPathElements),
+          leaf: new Array(utxos[j]).fill(toHex(leaf)),
+        };
+        const inputsData = JSON.stringify(inputs);
+        console.time(`Proof generation for ${merkleHeights[i]} ${utxos[j]}`);
+        const response = await axios.post(PROVE_URL, inputsData);
+        console.timeEnd(`Proof generation for ${merkleHeights[i]} ${utxos[j]}`);
+
+        assert.equal(response.status, 200);
+        assert.isNotEmpty(response.data.toString());
+      }
+    }
+  });
+
+  function toHex(bnString: string) {
+    return "0x" + new BN(bnString).toString(16);
+  }
+});
