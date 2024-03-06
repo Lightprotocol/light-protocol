26384bfd84f919708fa7c02c6b073c183df71d78
Swen <swen.schaeferjohann@code.berlin>
2024-03-06 15:38:56 +0000

    rebased and rebuilt

--- zk.js/src/idls/account_compression.ts
+++ zk.js/src/idls/account_compression.ts
@@ -666,6 +666,24 @@ export type AccountCompression = {
           }
         ]
       }
+    },
+    {
+      "name": "IndexedArray",
+      "type": {
+        "kind": "alias",
+        "value": {
+          "defined": "IndexingArray<Poseidon,u16,BigInteger256,2800>"
+        }
+      }
+    },
+    {
+      "name": "StateMerkleTree",
+      "type": {
+        "kind": "alias",
+        "value": {
+          "defined": "ConcurrentMerkleTree<Poseidon,MERKLE_TREE_HEIGHT,MERKLE_TREE_CHANGELOG,MERKLE_TREE_ROOTS>"
+        }
+      }
     }
   ],
   "errors": [
@@ -1399,6 +1417,24 @@ export const IDL: AccountCompression = {
             ]
           }
         ]
+      }
+    },
+    {
+      "name": "IndexedArray",
+      "type": {
+        "kind": "alias",
+        "value": {
+          "defined": "IndexingArray<Poseidon,u16,BigInteger256,2800>"
+        }
+      }
+    },
+    {
+      "name": "StateMerkleTree",
+      "type": {
+        "kind": "alias",
+        "value": {
+          "defined": "ConcurrentMerkleTree<Poseidon,MERKLE_TREE_HEIGHT,MERKLE_TREE_CHANGELOG,MERKLE_TREE_ROOTS>"
+        }
       }
     }
   ],
