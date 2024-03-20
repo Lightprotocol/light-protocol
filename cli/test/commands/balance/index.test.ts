f08149bb5094047330ee6907b96e955ebe92ae85
Sergey Timoshin <sergeytimoshin@proton.me>
2024-03-20 20:04:43 +0000

    feat: add balance command to CLI (#542)

---
+++ cli/test/commands/balance/index.test.ts
@@ -0,0 +1,77 @@
+import { expect, test } from "@oclif/test";
+import { initTestEnvIfNeeded } from "../../../src/utils/initTestEnv";
+import { defaultSolanaWalletKeypair, getSolanaRpcUrl } from "../../../src";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { createMint, mintTo } from "@lightprotocol/compressed-token";
+import { requestAirdrop } from "../../helpers/helpers";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+describe("Get balance", () => {
+  test.it(async () => {
+    await initTestEnvIfNeeded();
+    const payerKeypair = defaultSolanaWalletKeypair();
+
+    const mintKeypair = Keypair.generate();
+    await requestAirdrop(mintKeypair.publicKey);
+    const mintAuthority = payerKeypair.publicKey;
+
+    const mintAmount = 10;
+    const mintDestination = Keypair.generate().publicKey;
+    const mintAddress = await createTestMint(payerKeypair);
+
+    await testMintTo(
+      payerKeypair,
+      mintAddress,
+      mintDestination,
+      mintAuthority,
+      mintAmount,
+    );
+    const encodedPayer = bs58.encode(payerKeypair.secretKey);
+    return test
+      .stdout()
+      .command([
+        "balance",
+        `--mint=${mintAddress.toBase58()}`,
+        `--owner=${mintDestination.toBase58()}`,
+      ])
+      .it(
+        `runs balance --mint=${mintAddress.toBase58()} --owner=${mintDestination.toBase58()}`,
+        (ctx: any) => {
+          expect(ctx.stdout).to.contain("balance successful");
+        },
+      );
+  });
+
+  async function createTestMint(payer: Keypair) {
+    const connection = new Connection(getSolanaRpcUrl());
+    const { mint } = await createMint(
+      connection,
+      payer,
+      payer.publicKey,
+      9,
+      undefined,
+      {
+        commitment: "finalized",
+      },
+    );
+    return mint;
+  }
+
+  async function testMintTo(
+    payer: Keypair,
+    mintAddress: PublicKey,
+    mintDestination: PublicKey,
+    mintAuthority: PublicKey | Keypair,
+    mintAmount: number,
+  ) {
+    const connection = new Connection(getSolanaRpcUrl());
+    const txId = await mintTo(
+      connection,
+      payer,
+      mintAddress,
+      mintDestination,
+      mintAuthority,
+      mintAmount,
+    );
+    return txId;
+  }
+});
