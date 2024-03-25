6006276c4c4dfd995ea974b469bb1bea3e38fefd
Sergey Timoshin <sergeytimoshin@proton.me>
2024-03-25 17:09:38 +0000

    chore: remove arkworks helpers (#554)
    * chore: remove arkworks helpers
    
    arkworks-based impl moved to https://github.com/Lightprotocol/circuitlib
    
    * Move merkle-tree tests from circuit-lib to https://github.com/Lightprotocol/circuitlib/blob/main/circuit-lib.js/tests/merkle-tree.test.ts

--- circuit-lib/circuitlib-rs/src/inclusion/mod.rs
+++ circuit-lib/circuitlib-rs/src/inclusion/mod.rs
@@ -1,3 +1,2 @@
-pub mod merkle_inclusion_proof;
 pub mod merkle_inclusion_proof_inputs;
 pub mod merkle_tree_info;
--- circuit-lib/circuitlib-rs/src/prove_utils.rs
+++ circuit-lib/circuitlib-rs/src/prove_utils.rs
@@ -1,15 +1,6 @@
-use std::ops::Neg;
-
-use ark_bn254::Bn254;
-use ark_groth16::Proof as ArkGroth16Proof;
-use ark_serialize::{CanonicalSerialize, Compress};
 use groth16_solana::decompression::{decompress_g1, decompress_g2};
 
-use crate::{
-    arkworks_prover::ArkProof,
-    errors::CircuitsError,
-    helpers::{change_endianness, convert_endianness_128},
-};
+use crate::errors::CircuitsError;
 
 pub struct ProofResult {
     pub proof: ProofCompressed,
@@ -40,31 +31,4 @@ pub struct Proof {
     pub a: [u8; 64],
     pub b: [u8; 128],
     pub c: [u8; 64],
-}
-
-pub fn convert_arkworks_proof_to_solana_groth16(
-    proof: &ArkProof,
-) -> Result<ProofCompressed, CircuitsError> {
-    let proof_bn254: ArkGroth16Proof<Bn254> = proof.clone().0;
-    let proof_a = proof_bn254.a.neg();
-    let proof_b = proof_bn254.b;
-    let proof_c = proof_bn254.c;
-
-    let mut a: [u8; 32] = [0; 32];
-    proof_a.serialize_with_mode(&mut a[..], Compress::Yes)?;
-    a = change_endianness(&a)
-        .try_into()
-        .map_err(|_| CircuitsError::ChangeEndiannessError)?;
-    let mut b: [u8; 64] = [0; 64];
-    proof_b.serialize_with_mode(&mut b[..], Compress::Yes)?;
-    b = convert_endianness_128(&b)
-        .try_into()
-        .map_err(|_| CircuitsError::ChangeEndiannessError)?;
-    let mut c: [u8; 32] = [0; 32];
-    proof_c.serialize_with_mode(&mut c[..], Compress::Yes)?;
-    c = change_endianness(&c)
-        .try_into()
-        .map_err(|_| CircuitsError::ChangeEndiannessError)?;
-
-    Ok(ProofCompressed { a, b, c })
 }
