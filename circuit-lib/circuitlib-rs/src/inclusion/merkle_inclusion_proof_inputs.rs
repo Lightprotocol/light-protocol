aa4aad55a6cbd76e16e01290b9f29bc4773c1afa
Michal Rostecki <vadorovsky@protonmail.com>
2024-04-19 12:20:56 +0200

    chore: Update Solana to 1.18.11 (#610)
    * chore: Update Solana to 1.18.11
    
    We do it mainly for the new definition of `TransactionSimulationDetails`
    in solana-banks-interface 1.18, which contains `inner_instructions`[0].
    It will allow us to mock the indexer in Rust tests properly - by fetching
    noop program instruction data. This way we could stop returning the
    events in instructions and use only the noop program.
    
    [0] https://docs.rs/solana-banks-interface/1.18.11/solana_banks_interface/struct.TransactionSimulationDetails.html
    
    * chore: Remove `ark-circom` as a dependency
    
    We don't use it anymore (since we use Gnark) and it generates dependency
    conflicts with Solana libraries.

--- circuit-lib/circuitlib-rs/src/inclusion/merkle_inclusion_proof_inputs.rs
+++ circuit-lib/circuitlib-rs/src/inclusion/merkle_inclusion_proof_inputs.rs
@@ -1,6 +1,3 @@
-use std::{collections::HashMap, convert::TryInto};
-
-use ark_circom::circom::Inputs;
 use num_bigint::BigInt;
 
 use crate::helpers::bigint_to_u8_32;
@@ -34,61 +31,5 @@ impl InclusionProofInputs<'_> {
             leaves.push(input_arr[1]);
         }
         [roots, leaves].concat()
-    }
-}
-
-impl<'a> TryInto<HashMap<String, Inputs>> for InclusionProofInputs<'a> {
-    type Error = std::io::Error;
-
-    fn try_into(self) -> Result<HashMap<String, Inputs>, Self::Error> {
-        let mut inputs: HashMap<String, Inputs> = HashMap::new();
-        let mut roots: Vec<BigInt> = Vec::new();
-        let mut leaves: Vec<BigInt> = Vec::new();
-        let mut indices: Vec<BigInt> = Vec::new();
-        let mut els: Vec<Vec<BigInt>> = Vec::new();
-
-        for input in self.0 {
-            roots.push(input.roots.clone());
-            leaves.push(input.leaves.clone());
-            indices.push(input.in_path_indices.clone());
-            els.push(input.in_path_elements.clone());
-        }
-
-        inputs
-            .entry("roots".to_string())
-            .or_insert_with(|| Inputs::BigIntVec(roots));
-        inputs
-            .entry("leaves".to_string())
-            .or_insert_with(|| Inputs::BigIntVec(leaves));
-        inputs
-            .entry("inPathIndices".to_string())
-            .or_insert_with(|| Inputs::BigIntVec(indices));
-        inputs
-            .entry("inPathElements".to_string())
-            .or_insert_with(|| Inputs::BigIntVecVec(els));
-
-        Ok(inputs)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use ark_std::Zero;
-
-    use super::*;
-
-    #[test]
-    fn test_conversion_to_hashmap() {
-        let zero_input = InclusionMerkleProofInputs {
-            leaves: BigInt::zero(),
-            roots: BigInt::zero(),
-            in_path_elements: vec![BigInt::zero()],
-            in_path_indices: BigInt::zero(),
-        };
-
-        let inputs: [InclusionMerkleProofInputs; 2] = [zero_input.clone(), zero_input.clone()];
-        let proof_inputs = InclusionProofInputs(&inputs);
-        let result: HashMap<String, Inputs> = proof_inputs.try_into().unwrap();
-        assert_eq!(result.len(), inputs.len() * 2);
     }
 }
