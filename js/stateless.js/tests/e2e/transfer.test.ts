4bc09387fe491cb1eb628e3abec774fb939802f7
Swen <swen.schaeferjohann@code.berlin>
2024-04-23 19:31:27 +0100

    adapt transfer.test to test for merkleproof integrity

--- js/stateless.js/tests/e2e/transfer.test.ts
+++ js/stateless.js/tests/e2e/transfer.test.ts
@@ -1,34 +1,77 @@
 import { describe, it, assert, beforeAll } from 'vitest';
 import { Signer } from '@solana/web3.js';
 import { newAccountWithLamports } from '../../src/utils/test-utils';
-import { Rpc } from '../../src/rpc';
+import { Rpc, createRpc } from '../../src/rpc';
 import { bn, compress, getTestRpc } from '../../src';
 import { transfer } from '../../src/actions/transfer';
 
 describe('transfer', () => {
-    let rpc: Rpc;
     let payer: Signer;
     let bob: Signer;
+    /// Photon instance for debugging
+    let rpc: Rpc;
+    /// Mock rpc (ground truth)
+    let testRpc: Rpc;
 
     beforeAll(async () => {
-        rpc = await getTestRpc();
+        rpc = createRpc();
+        testRpc = await getTestRpc();
+
+        /// These are constant test accounts in between test runs
         payer = await newAccountWithLamports(rpc, 2e9, 112);
         bob = await newAccountWithLamports(rpc, 2e9, 113);
 
         await compress(rpc, payer, 1e9, payer.publicKey);
     });
 
-    const numberOfTransfers = 10;
-    it(`should send compressed lamports alice -> bob for ${numberOfTransfers} transfers in a loop`, async () => {
+    const numberOfTransfers = 60;
+    it(`should execute ${numberOfTransfers} transfers and compare merkleproofs between rpc (photon) and mockRpc`, async () => {
         const transferAmount = 1000;
         for (let i = 0; i < numberOfTransfers; i++) {
-            const preSenderBalance = (
-                await rpc.getCompressedAccountsByOwner(payer.publicKey)
-            ).reduce((acc, account) => acc.add(account.lamports), bn(0));
+            const prePayerAccounts = await rpc.getCompressedAccountsByOwner(
+                payer.publicKey,
+            );
+            const preSenderBalance = prePayerAccounts.reduce(
+                (acc, account) => acc.add(account.lamports),
+                bn(0),
+            );
 
-            const preReceiverBalance = (
-                await rpc.getCompressedAccountsByOwner(bob.publicKey)
-            ).reduce((acc, account) => acc.add(account.lamports), bn(0));
+            const preReceiverAccounts = await rpc.getCompressedAccountsByOwner(
+                bob.publicKey,
+            );
+            const preReceiverBalance = preReceiverAccounts.reduce(
+                (acc, account) => acc.add(account.lamports),
+                bn(0),
+            );
+
+            /// get reference proofs
+            const testProofs = await testRpc.getMultipleCompressedAccountProofs(
+                preReceiverAccounts.map(account => bn(account.hash)),
+            );
+
+            /// get photon proofs
+            const proofs = await rpc.getMultipleCompressedAccountProofs(
+                preReceiverAccounts.map(account => bn(account.hash)),
+            );
+
+            /// compare each proof by node and root
+            assert.equal(testProofs.length, proofs.length);
+            proofs.forEach((proof, index) => {
+                proof.merkleProof.forEach((elem, elemIndex) => {
+                    assert.isTrue(
+                        bn(elem).eq(
+                            bn(testProofs[index].merkleProof[elemIndex]),
+                        ),
+                    );
+                });
+            });
+            assert.isTrue(bn(proofs[0].root).eq(bn(testProofs[0].root)));
+            /// Note: proofs.rootIndex might be divergent if either the
+            /// test-validator or photon aren't caught up with the chain state
+            /// or process new txs inbetween returning the merkleproof and
+            /// calling getRootSeq (since we're not getting that from photon yet
+            /// (v0.11.0), we're using a mockFn called getRootSeq() in the Rpc
+            /// class which fetches all events anew.
 
             await transfer(rpc, payer, transferAmount, payer, bob.publicKey);
 
