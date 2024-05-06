2faf2043dfad03dd5c92a3d1777e04a25afc89bb
Sergey Timoshin <sergeytimoshin@proton.me>
2024-05-06 14:07:40 +0100

    chore: Add console example files and dependencies

---
+++ examples/console/src/index.ts
@@ -0,0 +1,9 @@
+import { airdropSol, compress, createRpc } from "@lightprotocol/stateless.js";
+import { Keypair } from "@solana/web3.js";
+
+const rpc = createRpc();
+const keypair = new Keypair();
+
+await airdropSol({connection: rpc, lamports: 1e11, recipientPublicKey: keypair.publicKey});
+const tx = await compress(rpc, keypair, 1e9, keypair.publicKey);
+console.log('compress tx', tx);
