f1bb311c214092a7028a4bb1e01b451a2a0cf91e
Michal Rostecki <vadorovsky@protonmail.com>
2024-05-06 17:50:08 +0200

    chore: Remove leftover/unused code (#668)
    * `build.rs` was used only for macro-circom, which we removed already.
    * `psp-template` is not relevant anymore, a new ZK compression template
      needs to be built from scratch.

--- psp-template/psp-template/circuits/program_name/circuit_circom/{{circom-name-camel-case}}Main.circom
+++
@@ -1,4 +0,0 @@
-pragma circom 2.1.4;
-include "./{{circom-name}}.circom";
-
-component main {public [hash]} =  {{circom-name}}();
--- psp-template/psp-template/programs_circom/program_name/src/errors.rs
+++
@@ -1,7 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[error_code]
-pub enum VerifierError {
-    #[msg("Proof verification failed.")]
-    ProofVerificationFailed,
-}
