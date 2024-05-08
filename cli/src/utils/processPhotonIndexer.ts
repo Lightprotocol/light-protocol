89cb5b406e90fa361738081f0f55b0c5e2d0c281
Sergey Timoshin <sergeytimoshin@proton.me>
2024-05-08 16:58:51 +0100

    switch to photon 0.18t:wq
    re-enable js/rpc-interop tests
    
    Update rpc-interop e2e test commands in js/**/package.json
    
    wip
    
    stable
    
    unify photon_version
    
    test:e2e:compress enough sol

--- cli/src/utils/processPhotonIndexer.ts
+++ cli/src/utils/processPhotonIndexer.ts
@@ -2,14 +2,14 @@ import which from "which";
 import { sleep } from "@lightprotocol/stateless.js";
 import { spawnBinary, killProcessByName } from "./process";
 import { INDEXER_PROCESS_NAME } from "./constants";
+import { PHOTON_VERSION } from "./initTestEnv";
 
 export async function startIndexer() {
   console.log("Kill existing indexer process...");
   await killProcessByName(INDEXER_PROCESS_NAME);
   const resolvedOrNull = which.sync("photon", { nothrow: true });
   if (resolvedOrNull === null) {
-    const message =
-      "Photon indexer not found. Please install it by running `cargo install photon-indexer --version 0.11.0`";
+    const message = `Photon indexer not found. Please install it by running "cargo install photon-indexer --version ${PHOTON_VERSION}"`;
     console.log(message);
     throw new Error(message);
   } else {
