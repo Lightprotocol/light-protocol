48f460cb220ab8e73f17c932212b0ca7fcdd72aa
Swen <swen.schaeferjohann@code.berlin>
2024-05-12 19:16:30 +0100

    suppress circ deps

--- js/compressed-token/rollup.config.js
+++ js/compressed-token/rollup.config.js
@@ -3,6 +3,8 @@ import nodePolyfills from 'rollup-plugin-polyfill-node';
 import dts from 'rollup-plugin-dts';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import alias from '@rollup/plugin-alias';
+import json from '@rollup/plugin-json';
 
 const rolls = (fmt, env) => ({
     input: 'src/index.ts',
@@ -18,6 +20,7 @@ const rolls = (fmt, env) => ({
         '@lightprotocol/stateless.js',
     ],
     plugins: [
+        json(),
         typescript({
             target: fmt === 'es' ? 'ES2022' : 'ES2017',
             outDir: `dist/${fmt}/${env}`,
@@ -27,9 +30,30 @@ const rolls = (fmt, env) => ({
         resolve({
             browser: env === 'browser',
             preferBuiltins: env === 'node',
+            extensions: ['.mjs', '.js', '.json', '.ts'],
+            mainFields: ['module', 'main', 'browser'],
+        }),
+        alias({
+            entries: [
+                {
+                    find: 'crypto',
+                    replacement:
+                        env === 'browser' ? 'crypto-browserify' : 'crypto',
+                },
+            ],
         }),
         env === 'browser' ? nodePolyfills() : undefined,
     ].filter(Boolean),
+    onwarn(warning, warn) {
+        // Check if the warning is about circular dependencies
+        if (warning.code === 'CIRCULAR_DEPENDENCY') {
+            // Optionally, you can also check the paths here if you want to ignore specific files
+            // console.log(`Ignoring circular dependency: ${warning.message}`);
+        } else {
+            // For all other warnings, call the default warning handler
+            warn(warning);
+        }
+    },
 });
 
 const typesConfig = {
