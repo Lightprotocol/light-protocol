a99e29f8cc8c5a7730b316c01b520deb34bb7c04
Sergey Timoshin <sergeytimoshin@proton.me>
2024-05-22 13:52:03 +0100

    fix e2e program test

--- test-programs/e2e-test/tests/test.rs
+++ test-programs/e2e-test/tests/test.rs
@@ -1,13 +1,13 @@
 #![cfg(feature = "test-sbf")]
 
 use light_test_utils::e2e_test_env::{E2ETestEnv, GeneralActionConfig, KeypairActionConfig};
-use light_test_utils::rpc::rpc_connection::RpcConnection;
+use light_test_utils::rpc::test_rpc::ProgramTestRpcConnection;
 use light_test_utils::test_env::setup_test_programs_with_accounts;
 
 #[tokio::test]
-async fn test_50_all<R: RpcConnection>() {
+async fn test_50_all() {
     let (rpc, env_accounts) = setup_test_programs_with_accounts(None).await;
-    let mut env = E2ETestEnv::<500, R>::new(
+    let mut env = E2ETestEnv::<500, ProgramTestRpcConnection>::new(
         rpc,
         env_accounts,
         KeypairActionConfig::all_default(),
@@ -23,11 +23,11 @@ async fn test_50_all<R: RpcConnection>() {
 //  cargo test-sbf -p e2e-test -- --nocapture --ignored --test test_10000_all > output.txt 2>&1
 #[ignore]
 #[tokio::test]
-async fn test_10000_all<R: RpcConnection>() {
+async fn test_10000_all() {
     // Will fail after inserting 500 addresses since the local indexed array is full
     // TODO: initialize the indexed array with heap memory so that the stack doesn't overflow with bigger size
     let (rpc, env_accounts) = setup_test_programs_with_accounts(None).await;
-    let mut env = E2ETestEnv::<500, R>::new(
+    let mut env = E2ETestEnv::<500, ProgramTestRpcConnection>::new(
         rpc,
         env_accounts,
         KeypairActionConfig::all_default(),
