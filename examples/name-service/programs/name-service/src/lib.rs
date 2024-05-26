09b4cd0b892db41ed93b42951c16eaba03a4f040
Swen <swen.schaeferjohann@code.berlin>
2024-05-27 00:25:41 +0100

    wip

--- examples/name-service/programs/name-service/src/lib.rs
+++ examples/name-service/programs/name-service/src/lib.rs
@@ -9,7 +9,9 @@ pub mod name_service {
 
     pub fn create_name(ctx: Context<CreateName>, name: String, parent_name: Option<Pubkey>) -> Result<()> {
 
-        let compressed_pda = create_compressed_pda_data(lock_up_time, &ctx, &new_address_params)?;
+
+        // hash the name 
+        let compressed_pda = create_compressed_pda_data(hashed_name, &ctx, &new_address_params)?;
 
         let name_account = &mut ctx.accounts.name_account;
 
@@ -105,3 +107,98 @@ impl light_hasher::DataHasher for NameRecord {
     }
 }
 
+
+// TODO: rename. consider providing this as sdk in systemprogram
+fn cpi_compressed_pda_transfer<'info>(
+    ctx: &Context<'_, '_, '_, 'info, EscrowCompressedTokensWithCompressedPda<'info>>,
+    proof: CompressedProof,
+    new_address_params: NewAddressParamsPacked,
+    compressed_pda: CompressedAccount,
+    cpi_context: CompressedCpiContext,
+    bump: u8,
+) -> Result<()> {
+
+    let bump = &[bump];
+    let signer_bytes = ctx.accounts.signer.key.to_bytes();
+    let seeds = [b"Light Name Service".as_slice(), signer_bytes.as_slice(), bump];
+    let inputs_struct: InstructionDataInvokeCpi = InstructionDataInvokeCpi {
+        relay_fee: None,
+        input_compressed_accounts_with_merkle_context: Vec::new(),
+        output_compressed_accounts: vec![compressed_pda],
+        input_root_indices: Vec::new(),
+        output_state_merkle_tree_account_indices: vec![0],
+        proof: Some(proof),
+        new_address_params: vec![new_address_params],
+        compression_lamports: None,
+        is_compress: false,
+        signer_seeds: seeds.iter().map(|x| x.to_vec()).collect::<Vec<Vec<u8>>>(),
+        cpi_context: Some(cpi_context),
+    };
+
+    let mut inputs = Vec::new();
+    InstructionDataInvokeCpi::serialize(&inputs_struct, &mut inputs).unwrap();
+    let cpi_context_account = match Some(cpi_context) {
+        Some(cpi_context) => Some(
+            ctx.remaining_accounts
+                .get(cpi_context.cpi_context_account_index as usize)
+                .unwrap()
+                .to_account_info(),
+        ),
+        None => return err!(EscrowError::CpiContextAccountIndexNotFound),
+    };
+    let cpi_accounts = light_system_program::cpi::accounts::InvokeCpiInstruction {
+        fee_payer: ctx.accounts.signer.to_account_info(),
+        authority: ctx.accounts.token_owner_pda.to_account_info(),
+        registered_program_pda: ctx.accounts.registered_program_pda.to_account_info(),
+        noop_program: ctx.accounts.noop_program.to_account_info(),
+        account_compression_authority: ctx.accounts.account_compression_authority.to_account_info(),
+        account_compression_program: ctx.accounts.account_compression_program.to_account_info(),
+        invoking_program: ctx.accounts.self_program.to_account_info(),
+        compressed_sol_pda: None,
+        compression_recipient: None,
+        system_program: ctx.accounts.system_program.to_account_info(),
+        cpi_context_account,
+    };
+    let seeds = [seeds.as_slice()];
+    let mut cpi_ctx = CpiContext::new_with_signer(
+        ctx.accounts.light_system_program.to_account_info(),
+        cpi_accounts,
+        &seeds,
+    );
+
+    cpi_ctx.remaining_accounts = ctx.remaining_accounts.to_vec();
+
+    light_system_program::cpi::invoke_cpi(cpi_ctx, inputs)?;
+    Ok(())
+}
+
+
+fn create_compressed_pda_data(
+    hashed_name: Vec<u8>,
+    ctx: &Context<'_, '_, '_, '_, EscrowCompressedTokensWithCompressedPda<'_>>,
+    new_address_params: &NewAddressParamsPacked,
+) -> Result<CompressedAccount> {
+    // let current_slot = Clock::get()?.slot;
+    // let timelock_compressed_pda = EscrowTimeLock {
+    //     slot: current_slot.checked_add(lock_up_time).unwrap(),
+    // };
+    // let compressed_account_data = CompressedAccountData {
+    //     discriminator: 1u64.to_le_bytes(),
+    //     data: timelock_compressed_pda.try_to_vec().unwrap(),
+    //     data_hash: timelock_compressed_pda
+    //         .hash::<Poseidon>()
+    //         .map_err(ProgramError::from)?,
+    // };
+    // let derive_address = derive_address(
+    //     &ctx.remaining_accounts[new_address_params.address_merkle_tree_account_index as usize]
+    //         .key(),
+    //     &new_address_params.seed,
+    // )
+    // .map_err(|_| ProgramError::InvalidArgument)?;
+    Ok(CompressedAccount {
+        owner: crate::ID,
+        lamports: 0,
+        address: Some(derive_address),
+        data: Some(compressed_account_data),
+    })
+}
