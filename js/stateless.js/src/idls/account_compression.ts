7a62f226e09ad7902200260b2c7670eaaaf3ee7b
Swen <swen.schaeferjohann@code.berlin>
2024-06-03 19:21:13 +0100

    regen idl

--- js/stateless.js/src/idls/account_compression.ts
+++ js/stateless.js/src/idls/account_compression.ts
@@ -887,7 +887,8 @@ export type AccountCompression = {
                         name: 'closeThreshold';
                         docs: [
                             'If current slot is greater than rolledover_slot + close_threshold and',
-                            'the account is empty it can be closed.',
+                            "the account is empty it can be closed. No 'close' functionality has been",
+                            'implemented yet.',
                         ];
                         type: 'u64';
                     },
@@ -2018,7 +2019,8 @@ export const IDL: AccountCompression = {
                         name: 'closeThreshold',
                         docs: [
                             'If current slot is greater than rolledover_slot + close_threshold and',
-                            'the account is empty it can be closed.',
+                            "the account is empty it can be closed. No 'close' functionality has been",
+                            'implemented yet.',
                         ],
                         type: 'u64',
                     },
