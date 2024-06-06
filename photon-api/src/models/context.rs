af9ecf3145de072b34fee38e90c0cf33335e88f9
Sergey Timoshin <sergeytimoshin@proton.me>
2024-06-06 19:32:58 +0100

    fix photon-api types

--- photon-api/src/models/context.rs
+++ photon-api/src/models/context.rs
@@ -13,11 +13,11 @@ use crate::models;
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Context {
     #[serde(rename = "slot")]
-    pub slot: i32,
+    pub slot: i64,
 }
 
 impl Context {
-    pub fn new(slot: i32) -> Context {
+    pub fn new(slot: i64) -> Context {
         Context { slot }
     }
 }
