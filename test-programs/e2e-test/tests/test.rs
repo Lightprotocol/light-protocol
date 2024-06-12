f1240c0c4afd887de74f6eeb855f8380315e7f25
Sergey Timoshin <sergeytimoshin@proton.me>
2024-06-12 19:16:02 +0100

    Update e2e test.rs to include airdrop request

--- test-programs/e2e-test/tests/test.rs
+++ test-programs/e2e-test/tests/test.rs
@@ -1,8 +1,11 @@
 #![cfg(feature = "test-sbf")]
 
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 use light_test_utils::e2e_test_env::{E2ETestEnv, GeneralActionConfig, KeypairActionConfig};
+use light_test_utils::rpc::rpc_connection::RpcConnection;
 use light_test_utils::rpc::{ProgramTestRpcConnection, SolanaRpcConnection};
 use light_test_utils::test_env::{get_test_env_accounts, setup_test_programs_with_accounts};
+use solana_sdk::signature::Signer;
 use std::process::Command;
 
 async fn spawn_test_validator() {
@@ -20,7 +23,10 @@ async fn test_10_validator_all() {
     spawn_test_validator().await;
     let env_accounts = get_test_env_accounts();
     let rpc = SolanaRpcConnection::new(None);
-
+    rpc.client
+        .request_airdrop(&rpc.get_payer().pubkey(), LAMPORTS_PER_SOL * 1000)
+        .unwrap();
+    tokio::time::sleep(std::time::Duration::from_secs(16)).await;
     let mut env = E2ETestEnv::<500, SolanaRpcConnection>::new(
         rpc,
         &env_accounts,
