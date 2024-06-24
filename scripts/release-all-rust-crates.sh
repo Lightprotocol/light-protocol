b889f8253b12fa19dd17103763e8a22beec6dcbe
Swen <swen.schaeferjohann@code.berlin>
2024-06-24 02:01:53 +0100

    test-utils not published

--- scripts/release-all-rust-crates.sh
+++ scripts/release-all-rust-crates.sh
@@ -12,8 +12,7 @@ echo "Tagging and releasing all Rust projects..."
 echo "Logging in to crates.io..."
 cargo login "${CRATES_IO_TOKEN}"
 # TODO: allow dynamic releases, and add gh release workflow
-# PACKAGES=("aligned-sized" "light-heap" "light-bounded-vec" "light-utils" "light-hasher" "light-macros" "light-hash-set" "light-merkle-tree-reference" "light-concurrent-merkle-tree" "light-indexed-merkle-tree" "light-prover-client" "light-verifier" "account-compression" "light-registry" "light-system-program" "light-compressed-token" "light-test-utils")
-# PACKAGES=("light-compressed-token" "light-test-utils")
+PACKAGES=("aligned-sized" "light-heap" "light-bounded-vec" "light-utils" "light-hasher" "light-macros" "light-hash-set" "light-merkle-tree-reference" "light-concurrent-merkle-tree" "light-indexed-merkle-tree" "light-prover-client" "light-verifier" "account-compression" "light-registry" "light-system-program" "light-compressed-token" "light-test-utils")
 PACKAGES=("light-test-utils")
 for PACKAGE in "${PACKAGES[@]}"; do
     PKG_VERSION=$(cargo pkgid -p "$PACKAGE" | cut -d "#" -f2)
