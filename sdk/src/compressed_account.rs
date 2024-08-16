9e08a0b74f4df6c307d4105a8ca08ecfb971a641
Michal Rostecki <vadorovsky@protonmail.com>
2024-08-16 18:57:31 +0200

    feat: Add helpers for each compressed account action
    Remove the necessity of constructing `InstructionDataInvokeCpi` manually
    by adding utility functions for each type of singular account action:
    
    - **Create** (no inputs, only output)
      - `new_compressed_account` - creates an output compressed account
      - `create_cpi_inputs_for_new_account`
    - **Update** (input with old data, output with new data)
      - `input_compressed_account`
      - `output_compressed_accoint`
      - `create_cpi_inputs_for_account_update`
    - **Delete** (input with data to delete, no output)
      - `input_compressed_account`
      - `create_cpi_inputs_for_account_deletion`
    
    While this abstraction doesn't fully hide the UTXO model full yet, it
    aims to make it clear which kind of inputs and outputs are needed for
    each action.

--- sdk/src/compressed_account.rs
+++ sdk/src/compressed_account.rs
@@ -1,8 +1,145 @@
-use light_system_program::sdk::compressed_account::{
-    CompressedAccountWithMerkleContext, PackedCompressedAccountWithMerkleContext,
+use anchor_lang::prelude::{AccountInfo, Key, ProgramError, Pubkey, Result};
+use borsh::BorshSerialize;
+use light_hasher::{DataHasher, Discriminator, Poseidon};
+use light_system_program::{
+    sdk::{
+        address::derive_address,
+        compressed_account::{
+            CompressedAccount, CompressedAccountData, CompressedAccountWithMerkleContext,
+            PackedCompressedAccountWithMerkleContext, PackedMerkleContext,
+        },
+    },
+    NewAddressParamsPacked, OutputCompressedAccountWithPackedContext,
 };
 
-use crate::merkle_context::{pack_merkle_context, RemainingAccounts};
+use crate::merkle_context::{
+    pack_merkle_context, PackedAddressMerkleContext, PackedMerkleOutputContext, RemainingAccounts,
+};
+
+pub fn serialize_and_hash_account<T>(
+    account: &T,
+    address_seed: &[u8; 32],
+    program_id: &Pubkey,
+    address_merkle_context: &PackedAddressMerkleContext,
+    remaining_accounts: &[AccountInfo],
+) -> Result<CompressedAccount>
+where
+    T: BorshSerialize + DataHasher + Discriminator,
+{
+    let data = account.try_to_vec()?;
+    let data_hash = account.hash::<Poseidon>().map_err(ProgramError::from)?;
+    let compressed_account_data = CompressedAccountData {
+        discriminator: T::discriminator(),
+        data,
+        data_hash,
+    };
+
+    let address = derive_address(
+        &remaining_accounts[address_merkle_context.address_merkle_tree_pubkey_index as usize].key(),
+        address_seed,
+    )
+    .map_err(|_| ProgramError::InvalidArgument)?;
+
+    let compressed_account = CompressedAccount {
+        owner: *program_id,
+        lamports: 0,
+        address: Some(address),
+        data: Some(compressed_account_data),
+    };
+
+    Ok(compressed_account)
+}
+
+pub fn new_compressed_account<T>(
+    account: &T,
+    address_seed: &[u8; 32],
+    program_id: &Pubkey,
+    merkle_output_context: &PackedMerkleOutputContext,
+    address_merkle_context: &PackedAddressMerkleContext,
+    address_merkle_tree_root_index: u16,
+    remaining_accounts: &[AccountInfo],
+) -> Result<(
+    OutputCompressedAccountWithPackedContext,
+    NewAddressParamsPacked,
+)>
+where
+    T: BorshSerialize + DataHasher + Discriminator,
+{
+    let compressed_account = serialize_and_hash_account(
+        account,
+        address_seed,
+        program_id,
+        address_merkle_context,
+        remaining_accounts,
+    )?;
+
+    let compressed_account = OutputCompressedAccountWithPackedContext {
+        compressed_account,
+        merkle_tree_index: merkle_output_context.merkle_tree_pubkey_index,
+    };
+
+    let new_address_params = NewAddressParamsPacked {
+        seed: *address_seed,
+        address_merkle_tree_account_index: address_merkle_context.address_merkle_tree_pubkey_index,
+        address_queue_account_index: address_merkle_context.address_queue_pubkey_index,
+        address_merkle_tree_root_index,
+    };
+
+    Ok((compressed_account, new_address_params))
+}
+
+pub fn input_compressed_account<T>(
+    account: &T,
+    address_seed: &[u8; 32],
+    program_id: &Pubkey,
+    merkle_context: &PackedMerkleContext,
+    merkle_tree_root_index: u16,
+    address_merkle_context: &PackedAddressMerkleContext,
+    remaining_accounts: &[AccountInfo],
+) -> Result<PackedCompressedAccountWithMerkleContext>
+where
+    T: BorshSerialize + DataHasher + Discriminator,
+{
+    let compressed_account = serialize_and_hash_account(
+        account,
+        address_seed,
+        program_id,
+        address_merkle_context,
+        remaining_accounts,
+    )?;
+
+    Ok(PackedCompressedAccountWithMerkleContext {
+        compressed_account,
+        merkle_context: *merkle_context,
+        root_index: merkle_tree_root_index,
+        read_only: false,
+    })
+}
+
+pub fn output_compressed_account<T>(
+    account: &T,
+    address_seed: &[u8; 32],
+    program_id: &Pubkey,
+    merkle_context: &PackedMerkleContext,
+    address_merkle_context: &PackedAddressMerkleContext,
+    remaining_accounts: &[AccountInfo],
+) -> Result<OutputCompressedAccountWithPackedContext>
+where
+    T: BorshSerialize + DataHasher + Discriminator,
+{
+    let compressed_account = serialize_and_hash_account(
+        account,
+        address_seed,
+        program_id,
+        address_merkle_context,
+        remaining_accounts,
+    )?;
+
+    Ok(OutputCompressedAccountWithPackedContext {
+        compressed_account,
+        merkle_tree_index: merkle_context.merkle_tree_pubkey_index,
+    })
+}
 
 pub fn pack_compressed_accounts(
     compressed_accounts: &[CompressedAccountWithMerkleContext],
--- sdk/src/utils.rs
+++ sdk/src/utils.rs
@@ -1,7 +1,8 @@
 use anchor_lang::solana_program::pubkey::Pubkey;
 use light_system_program::{
-    invoke::processor::CompressedProof, sdk::CompressedCpiContext, InstructionDataInvokeCpi,
-    NewAddressParamsPacked, OutputCompressedAccountWithPackedContext,
+    invoke::processor::CompressedProof,
+    sdk::{compressed_account::PackedCompressedAccountWithMerkleContext, CompressedCpiContext},
+    InstructionDataInvokeCpi, NewAddressParamsPacked, OutputCompressedAccountWithPackedContext,
 };
 
 pub fn get_registered_program_pda(program_id: &Pubkey) -> Pubkey {
@@ -17,22 +18,70 @@ pub fn get_cpi_authority_pda(program_id: &Pubkey) -> Pubkey {
 }
 
 /// Helper function to create data for creating a single PDA.
-pub fn create_cpi_inputs_for_new_address(
+pub fn create_cpi_inputs_for_new_account(
     proof: CompressedProof,
     new_address_params: NewAddressParamsPacked,
     compressed_pda: OutputCompressedAccountWithPackedContext,
-    seeds: &[&[u8]],
+    signer_seeds: &[&[u8]],
     cpi_context: Option<CompressedCpiContext>,
 ) -> InstructionDataInvokeCpi {
     InstructionDataInvokeCpi {
         proof: Some(proof),
         new_address_params: vec![new_address_params],
         relay_fee: None,
-        input_compressed_accounts_with_merkle_context: Vec::new(),
+        input_compressed_accounts_with_merkle_context: vec![],
         output_compressed_accounts: vec![compressed_pda],
         compress_or_decompress_lamports: None,
         is_compress: false,
-        signer_seeds: seeds.iter().map(|x| x.to_vec()).collect::<Vec<Vec<u8>>>(),
+        signer_seeds: signer_seeds
+            .iter()
+            .map(|x| x.to_vec())
+            .collect::<Vec<Vec<u8>>>(),
+        cpi_context,
+    }
+}
+
+pub fn create_cpi_inputs_for_account_update(
+    proof: CompressedProof,
+    old_compressed_pda: PackedCompressedAccountWithMerkleContext,
+    new_compressed_pda: OutputCompressedAccountWithPackedContext,
+    signer_seeds: &[&[u8]],
+    cpi_context: Option<CompressedCpiContext>,
+) -> InstructionDataInvokeCpi {
+    InstructionDataInvokeCpi {
+        proof: Some(proof),
+        new_address_params: vec![],
+        input_compressed_accounts_with_merkle_context: vec![old_compressed_pda],
+        output_compressed_accounts: vec![new_compressed_pda],
+        relay_fee: None,
+        compress_or_decompress_lamports: None,
+        is_compress: false,
+        signer_seeds: signer_seeds
+            .iter()
+            .map(|x| x.to_vec())
+            .collect::<Vec<Vec<u8>>>(),
+        cpi_context,
+    }
+}
+
+pub fn create_cpi_inputs_for_account_deletion(
+    proof: CompressedProof,
+    compressed_pda: PackedCompressedAccountWithMerkleContext,
+    signer_seeds: &[&[u8]],
+    cpi_context: Option<CompressedCpiContext>,
+) -> InstructionDataInvokeCpi {
+    InstructionDataInvokeCpi {
+        proof: Some(proof),
+        new_address_params: vec![],
+        input_compressed_accounts_with_merkle_context: vec![compressed_pda],
+        output_compressed_accounts: vec![],
+        relay_fee: None,
+        compress_or_decompress_lamports: None,
+        is_compress: false,
+        signer_seeds: signer_seeds
+            .iter()
+            .map(|x| x.to_vec())
+            .collect::<Vec<Vec<u8>>>(),
         cpi_context,
     }
 }
