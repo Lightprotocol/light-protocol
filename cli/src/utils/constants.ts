cc9d1c1d80569583f7a1c04da40f415d0f1baa81
Swen <swen.schaeferjohann@code.berlin>
2024-08-20 01:19:42 +0100

    bump to 40

--- cli/src/utils/constants.ts
+++ cli/src/utils/constants.ts
@@ -21,7 +21,7 @@ export const LIGHT_PROVER_PROCESS_NAME = "light-prover";
 export const INDEXER_PROCESS_NAME = "photon";
 export const FORESTER_PROCESS_NAME = "forester";
 
-export const PHOTON_VERSION = "0.39.0";
+export const PHOTON_VERSION = "0.40.0";
 
 export const LIGHT_PROTOCOL_PROGRAMS_DIR_ENV = "LIGHT_PROTOCOL_PROGRAMS_DIR";
 export const BASE_PATH = "../../bin/";
