2f1a122c39fb8e6fcf1d3bf06c89bc10ec73bd59
ananas-block <jorrit@lightprotocol.com>
2024-08-27 00:15:22 +0100

    removed test batch transactions program

--- test-programs/test-batch-transactions/scripts/start-test-validator.sh
+++
@@ -1,2 +0,0 @@
-cd examples/test-batch-transactions/ && cargo build-sbf && cd - 
-solana-test-validator --bpf-program Dcc5vv836DzrDrvY8rB7TZD2bhtfXLMCWCHdoJCtQnxy ./target/deploy/test_batch_transactions.so --reset
