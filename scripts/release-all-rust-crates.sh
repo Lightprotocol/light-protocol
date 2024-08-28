71763b94503f13a76bdec62df6878e4c3ad57135
swen <swen-code@swens-MacBook-Air-2.local>
2024-08-28 20:03:19 +0100

    wip

--- scripts/release-all-rust-crates.sh
+++ scripts/release-all-rust-crates.sh
@@ -12,8 +12,8 @@ echo "Tagging and releasing all Rust projects..."
 echo "Logging in to crates.io..."
 cargo login "${CRATES_IO_TOKEN}"
 # TODO: allow dynamic releases, and add gh release workflow
-# PACKAGES=("aligned-sized" "light-heap" "light-bounded-vec" "light-utils" "light-hasher" "light-macros" "light-hash-set" "light-merkle-tree-reference" "light-concurrent-merkle-tree" "light-indexed-merkle-tree" "light-prover-client" "light-verifier" "account-compression" "light-registry" "light-system-program" "light-compressed-token" "light-test-utils" "light-sdk")
-PACKAGES=("photon-api" "light-test-utils" "light-sdk")
+PACKAGES=("aligned-sized" "light-heap" "light-bounded-vec" "light-utils" "light-hasher" "light-macros" "light-hash-set" "light-merkle-tree-reference" "light-concurrent-merkle-tree" "light-indexed-merkle-tree" "light-prover-client" "light-verifier" "account-compression" "light-registry" "light-system-program" "light-compressed-token" "photon-api" "light-test-utils" "light-sdk")
+
 for PACKAGE in "${PACKAGES[@]}"; do
     PKG_VERSION=$(cargo pkgid -p "$PACKAGE" | cut -d "#" -f2)
     VERSION=${PKG_VERSION#*@}
@@ -23,6 +23,6 @@ for PACKAGE in "${PACKAGES[@]}"; do
     for attempt in {1..3}; do
         echo "Attempt $attempt: Publishing $PACKAGE..."
         cargo release publish --package "$PACKAGE" --execute --no-confirm && break || echo "Attempt $attempt failed, retrying in 20..."
-        sleep 20
+        sleep 15
     done
 done
