898d6ea25f20b719ecc8feb12cd7377d7cd98d5f
ananas-block <jorrit@lightprotocol.com>
2024-09-09 17:39:59 +0700

    fix registry test

--- test-programs/registry-test/tests/tests.rs
+++ test-programs/registry-test/tests/tests.rs
@@ -288,7 +288,7 @@ async fn test_initialize_protocol_config() {
             &mut rpc,
             &payer,
             &group_pda,
-            &random_program_keypair.pubkey(),
+            &random_program_keypair,
         )
         .await;
         let expected_error_code = anchor_lang::error::ErrorCode::ConstraintHasOne as u32;
@@ -301,7 +301,7 @@ async fn test_initialize_protocol_config() {
             &mut rpc,
             &updated_keypair,
             &group_pda,
-            &random_program_keypair.pubkey(),
+            &random_program_keypair,
         )
         .await
         .unwrap();
@@ -319,18 +319,6 @@ async fn test_initialize_protocol_config() {
             &updated_keypair,
             &group_pda,
             &random_program_keypair,
-        )
-        .await
-        .unwrap();
-    }
-    // register program
-    {
-        // let program_keypair = Keypair::from_bytes(light_registry::id().as_ref()).unwrap();
-        register_program_with_registry_program(
-            &mut rpc,
-            &updated_keypair,
-            &group_pda,
-            &light_registry::id(),
         )
         .await
         .unwrap();
