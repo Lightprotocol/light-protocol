8d46dcada89bb6171cce0407a67fe9e407f3c7bf
Sergey Timoshin <sergeytimoshin@proton.me>
2024-09-11 17:50:39 +0700

    Add tree discovery capability.
    Implement TreeFinder for asynchronous tree discovery and insertion. Update EpochManager to handle new trees and integrate this process within the service. Configure tree discovery intervals to ensure continuous updates.

---
+++ forester/src/tree_finder.rs
@@ -0,0 +1,69 @@
+use crate::errors::ForesterError;
+use crate::rpc_pool::SolanaRpcPool;
+use crate::tree_data_sync::fetch_trees;
+use forester_utils::forester_epoch::TreeAccounts;
+use forester_utils::rpc::RpcConnection;
+use std::sync::Arc;
+use tokio::sync::broadcast;
+use tokio::time::{interval, Duration};
+use tracing::{debug, error, info};
+
+pub struct TreeFinder<R: RpcConnection> {
+    rpc_pool: Arc<SolanaRpcPool<R>>,
+    known_trees: Vec<TreeAccounts>,
+    new_tree_sender: broadcast::Sender<TreeAccounts>,
+    check_interval: Duration,
+}
+
+impl<R: RpcConnection> TreeFinder<R> {
+    pub fn new(
+        rpc_pool: Arc<SolanaRpcPool<R>>,
+        initial_trees: Vec<TreeAccounts>,
+        new_tree_sender: broadcast::Sender<TreeAccounts>,
+        check_interval: Duration,
+    ) -> Self {
+        Self {
+            rpc_pool,
+            known_trees: initial_trees,
+            new_tree_sender,
+            check_interval,
+        }
+    }
+
+    pub async fn run(&mut self) -> Result<(), ForesterError> {
+        let mut interval = interval(self.check_interval);
+
+        loop {
+            interval.tick().await;
+            debug!("Checking for new trees");
+
+            match self.check_for_new_trees().await {
+                Ok(new_trees) => {
+                    for tree in new_trees {
+                        if let Err(e) = self.new_tree_sender.send(tree) {
+                            error!("Failed to send new tree: {:?}", e);
+                        } else {
+                            info!("New tree discovered and sent: {:?}", tree);
+                            self.known_trees.push(tree);
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("Error checking for new trees: {:?}", e);
+                }
+            }
+        }
+    }
+
+    async fn check_for_new_trees(&self) -> Result<Vec<TreeAccounts>, ForesterError> {
+        let rpc = self.rpc_pool.get_connection().await?;
+        let current_trees = fetch_trees(&*rpc).await;
+
+        let new_trees: Vec<TreeAccounts> = current_trees
+            .into_iter()
+            .filter(|tree| !self.known_trees.contains(tree))
+            .collect();
+
+        Ok(new_trees)
+    }
+}
