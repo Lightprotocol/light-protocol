24726ca9c1fefb471588dfe92b349a5a5877b44b
Sergey Timoshin <sergeytimoshin@proton.me>
2024-09-16 16:41:41 +0700

    Remove Forester support from CLI scripts and test environment
    This commit deletes all references and code related to Forester, including scripts, constants, and functions. This simplifies the CLI by removing support for the Forester process. Future updates will no longer include Forester-related commands or configurations.

--- cli/src/utils/constants.ts
+++ cli/src/utils/constants.ts
@@ -19,7 +19,6 @@ export const CARGO_GENERATE_TAG = "v0.18.4";
 export const SOLANA_VALIDATOR_PROCESS_NAME = "solana-test-validator";
 export const LIGHT_PROVER_PROCESS_NAME = "light-prover";
 export const INDEXER_PROCESS_NAME = "photon";
-export const FORESTER_PROCESS_NAME = "forester";
 
 export const PHOTON_VERSION = "0.45.0";
 
