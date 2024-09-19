c9fb800c4284daa59a9b2d0e0c959abc3fdf5c87
ananas-block <jorrit@lightprotocol.com>
2024-09-20 03:56:38 +0700

    fix: find light bin

--- test-utils/src/test_env.rs
+++ test-utils/src/test_env.rs
@@ -37,7 +37,6 @@ use solana_sdk::{
 };
 use std::cmp;
 use std::path::PathBuf;
-use std::process::Command;
 
 pub const CPI_CONTEXT_ACCOUNT_RENT: u64 = 143487360; // lamports of the cpi context account
 pub const NOOP_PROGRAM_ID: Pubkey = pubkey!("noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV");
@@ -80,29 +79,52 @@ pub async fn setup_test_programs(
     program_test.set_compute_max_units(1_400_000u64);
     program_test.start_with_context().await
 }
+
 fn find_light_bin() -> Option<PathBuf> {
     // Run the 'which light' command to find the location of 'light' binary
-    let output = Command::new("which")
-        .arg("light")
-        .output()
-        .expect("Failed to execute 'which light'");
 
-    if !output.status.success() {
-        return None;
+    #[cfg(not(feature = "devenv"))]
+    {
+        use std::process::Command;
+        let output = Command::new("which")
+            .arg("light")
+            .output()
+            .expect("Failed to execute 'which light'");
+
+        if !output.status.success() {
+            return None;
+        }
+        // Convert the output into a string (removing any trailing newline)
+        let light_path = String::from_utf8_lossy(&output.stdout).trim().to_string();
+        // Get the parent directory of the 'light' binary
+        let mut light_bin_path = PathBuf::from(light_path);
+        light_bin_path.pop(); // Remove the 'light' binary itself
+
+        // Assuming the node_modules path starts from '/lib/node_modules/...'
+        let node_modules_bin =
+            light_bin_path.join("../lib/node_modules/@lightprotocol/zk-compression-cli/bin");
+
+        Some(node_modules_bin.canonicalize().unwrap_or(node_modules_bin))
     }
-
-    // Convert the output into a string (removing any trailing newline)
-    let light_path = String::from_utf8_lossy(&output.stdout).trim().to_string();
-
-    // Get the parent directory of the 'light' binary
-    let mut light_bin_path = PathBuf::from(light_path);
-    light_bin_path.pop(); // Remove the 'light' binary itself
-
-    // Assuming the node_modules path starts from '/lib/node_modules/...'
-    let node_modules_bin =
-        light_bin_path.join("../lib/node_modules/@lightprotocol/zk-compression-cli/bin");
-
-    Some(node_modules_bin.canonicalize().unwrap_or(node_modules_bin))
+    #[cfg(feature = "devenv")]
+    {
+        let light_protocol_toplevel = String::from_utf8_lossy(
+            &std::process::Command::new("git")
+                .arg("rev-parse")
+                .arg("--show-toplevel")
+                .output()
+                .expect("Failed to get top-level directory")
+                .stdout,
+        )
+        .trim()
+        .to_string();
+        println!(
+            "light path {}",
+            format!("{}/cli/bin/", light_protocol_toplevel)
+        );
+        let light_path = PathBuf::from(format!("{}/cli/bin/", light_protocol_toplevel));
+        Some(light_path)
+    }
 }
 #[derive(Debug)]
 pub struct EnvAccounts {
