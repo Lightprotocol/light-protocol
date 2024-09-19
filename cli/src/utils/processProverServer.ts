2bf84f0802b7e87af4c29fce989d397df5a014b1
ananas-block <jorrit@lightprotocol.com>
2024-09-20 01:37:18 +0700

    stash

--- cli/src/utils/processProverServer.ts
+++ cli/src/utils/processProverServer.ts
@@ -38,6 +38,7 @@ export async function startProver(
   args.push("--keys-dir", keysDir);
   args.push("--prover-address", `0.0.0.0:${proverPort}`);
   console.log("Starting prover...");
+  console.log("getProverNameByArch() ", getProverNameByArch());
   spawnBinary(getProverNameByArch(), args);
   await waitForServers([{ port: proverPort, path: "/" }]);
   console.log("Prover started successfully!");
