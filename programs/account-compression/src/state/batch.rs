a671d8137e9d17217d1a9c4f7fdea1908a270fda
ananas-block <jorrit@lightprotocol.com>
2024-10-13 00:16:31 +0100

    chore: replace is_inserted with state

--- programs/account-compression/src/state/batch.rs
+++ programs/account-compression/src/state/batch.rs
@@ -5,22 +5,32 @@ use light_hasher::{Hasher, Poseidon};
 
 use crate::errors::AccountCompressionErrorCode;
 
+#[repr(u64)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum BatchState {
+    CanBeFilled,
+    Inserted,
+    ReadyToUpdateTree,
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Batch {
-    // pub id: u8,
-    // pub bloomfilter_store_id: u8,
-    // pub value_store_id: u8,
+    /// Number of iterations for the bloomfilter.
     pub num_iters: u64,
+    /// Theoretical capacity of the bloomfilter. We want to make it much larger
+    /// than batch_size to avoid false positives.
     pub bloomfilter_capacity: u64,
-    pub value_capacity: u64,
+    pub batch_size: u64,
     // TODO: make private
+    /// Number of inserted elements in the batch.
     pub num_inserted: u64,
+    /// Hash chain of the values inserted in the batch.
     // TODO: remove user_hash_chain and prover_hash_chain
     pub user_hash_chain: [u8; 32],
     /// To enable update of the batch in multiple proofs the prover hash chain
     /// is used to save intermediate state.
     pub prover_hash_chain: [u8; 32],
-    pub is_inserted: bool,
+    pub state: BatchState,
     // TODO: add multiple hash chains per batch
     // pub hash_chains_capacity = batch_capacity / zkp_batch_size -> number of
     // pub hash_chains: BoundedVec<BoundedVec<[u8; 32]>>,
@@ -28,28 +38,25 @@ pub struct Batch {
 }
 
 impl Batch {
-    /// Batch has been marked as ready to update the tree.
-    pub fn is_ready_to_update_tree(&self) -> bool {
-        println!("Num inserted: {:?}", self.num_inserted);
-        println!("Value capacity: {:?}", self.value_capacity);
-        println!("Is inserted: {:?}", self.is_inserted);
-        self.num_inserted == self.value_capacity && !self.is_inserted
-    }
+    // /// Batch has been marked as ready to update the tree.
+    // pub fn is_ready_to_update_tree(&self) -> bool {
+    //     self.num_inserted == self.batch_size && !self.is_inserted
+    // }
 
-    /// It is possible to add values to the batch:
-    /// 1. If the batch is not ready to update the tree.
-    /// 2. If the sequence number is greater than the current sequence number.
-    pub fn can_be_filled(&mut self) -> (bool, bool) {
-        let can_be_filled = self.is_inserted || self.num_inserted != self.value_capacity;
-        let wipe_bloomfilter = if self.is_inserted && self.num_inserted == self.value_capacity {
-            // self.is_inserted = false;
-            self.num_inserted = 0;
-            true
-        } else {
-            false
-        };
-        (can_be_filled, wipe_bloomfilter)
-    }
+    // /// It is possible to add values to the batch:
+    // /// 1. If the batch is not ready to update the tree.
+    // /// 2. If the sequence number is greater than the current sequence number.
+    // pub fn can_be_filled(&mut self) -> (bool, bool) {
+    //     let can_be_filled = self.is_inserted || self.num_inserted != self.batch_size;
+    //     let wipe_bloomfilter = if self.is_inserted && self.num_inserted == self.batch_size {
+    //         // self.is_inserted = false;
+    //         self.num_inserted = 0;
+    //         true
+    //     } else {
+    //         false
+    //     };
+    //     (can_be_filled, wipe_bloomfilter)
+    // }
 
     /// Inserts values into the bloom filter, stores value in values array and hashes the value.
     /// (Used by Address Queue)
@@ -111,8 +118,8 @@ impl Batch {
             Poseidon::hashv(&[self.user_hash_chain.as_slice(), value.as_slice()])
                 .map_err(ProgramError::from)?;
         self.num_inserted += 1;
-        if self.num_inserted == self.value_capacity {
-            self.is_inserted = false;
+        if self.num_inserted == self.batch_size {
+            self.state = BatchState::ReadyToUpdateTree;
         }
         Ok(())
     }
@@ -150,39 +157,20 @@ mod tests {
             num_inserted: 0,
             user_hash_chain: [0u8; 32],
             prover_hash_chain: [0u8; 32],
-            value_capacity: 500,
-            is_inserted: true,
+            batch_size: 500,
+            state: BatchState::CanBeFilled,
         }
     }
-
-    #[test]
-    fn test_is_ready_to_update_tree() {
-        let mut batch = get_test_batch();
-        assert!(!batch.is_ready_to_update_tree());
-        batch.num_inserted = batch.value_capacity;
-        batch.is_inserted = false;
-        assert!(batch.is_ready_to_update_tree());
-    }
-
-    // #[test]
-    // fn test_can_be_filled() {
-    //     let mut batch = get_test_batch();
-    //     assert!(batch.can_be_filled(0));
-    //     batch.mark_with_sequence_number(1, 5);
-    //     assert_eq!(batch.sequence_number, 6);
-    //     assert!(batch.can_be_filled(6));
-    //     assert!(!batch.can_be_filled(4));
-    // }
 
     #[test]
     fn test_insert_and_store() {
         // Behavior Address queue
         let mut batch = get_test_batch();
         let mut store = vec![0u8; 20_000];
-        let mut value_store = BoundedVec::with_capacity(batch.value_capacity as usize);
+        let mut value_store = BoundedVec::with_capacity(batch.batch_size as usize);
 
         let mut ref_batch = get_test_batch();
-        for i in 0..batch.value_capacity {
+        for i in 0..batch.batch_size {
             let mut value = [0u8; 32];
             value[24..].copy_from_slice(&i.to_be_bytes());
             let ref_hash_chain = Poseidon::hashv(&[&batch.user_hash_chain, &value]).unwrap();
@@ -197,8 +185,8 @@ mod tests {
             assert!(bloomfilter.contains(&value));
             ref_batch.num_inserted += 1;
             ref_batch.user_hash_chain = ref_hash_chain;
-            if i == batch.value_capacity - 1 {
-                ref_batch.is_inserted = false;
+            if i == batch.batch_size - 1 {
+                ref_batch.state = BatchState::ReadyToUpdateTree;
             }
             assert_eq!(batch, ref_batch);
             assert_eq!(*value_store.get(i as usize).unwrap(), value);
@@ -209,10 +197,10 @@ mod tests {
     fn test_store_and_hash() {
         // Behavior Output queue
         let mut batch = get_test_batch();
-        let mut value_store = BoundedVec::with_capacity(batch.value_capacity as usize);
+        let mut value_store = BoundedVec::with_capacity(batch.batch_size as usize);
 
         let mut ref_batch = get_test_batch();
-        for i in 0..batch.value_capacity {
+        for i in 0..batch.batch_size {
             let mut value = [0u8; 32];
             value[24..].copy_from_slice(&i.to_be_bytes());
             let ref_hash_chain = Poseidon::hashv(&[&batch.user_hash_chain, &value]).unwrap();
@@ -220,13 +208,12 @@ mod tests {
 
             ref_batch.num_inserted += 1;
             ref_batch.user_hash_chain = ref_hash_chain;
-            if i == batch.value_capacity - 1 {
-                ref_batch.is_inserted = false;
+            if i == batch.batch_size - 1 {
+                ref_batch.state = BatchState::ReadyToUpdateTree;
             }
             assert_eq!(batch, ref_batch);
             assert_eq!(*value_store.get(i as usize).unwrap(), value);
         }
-        assert!(batch.is_ready_to_update_tree());
         let value = [0u8; 32];
         assert!(matches!(
             batch.store_and_hash(&value, &mut value_store),
@@ -239,7 +226,7 @@ mod tests {
         let mut batch = get_test_batch();
 
         let value = [1u8; 32];
-        let mut value_store = BoundedVec::with_capacity(batch.value_capacity as usize);
+        let mut value_store = BoundedVec::with_capacity(batch.batch_size as usize);
 
         assert!(batch.store_value(&value, &mut value_store).is_ok());
         let ref_batch = get_test_batch();
@@ -254,7 +241,7 @@ mod tests {
         let mut store = vec![0u8; 20_000];
 
         let mut ref_batch = get_test_batch();
-        for i in 0..batch.value_capacity {
+        for i in 0..batch.batch_size {
             let mut value = [0u8; 32];
             value[24..].copy_from_slice(&i.to_be_bytes());
             let ref_hash_chain = Poseidon::hashv(&[&batch.user_hash_chain, &value]).unwrap();
@@ -267,8 +254,8 @@ mod tests {
             assert!(bloomfilter.contains(&value));
             ref_batch.num_inserted += 1;
             ref_batch.user_hash_chain = ref_hash_chain;
-            if i == batch.value_capacity - 1 {
-                ref_batch.is_inserted = false;
+            if i == batch.batch_size - 1 {
+                ref_batch.state = BatchState::ReadyToUpdateTree;
             }
             assert_eq!(batch, ref_batch);
         }
