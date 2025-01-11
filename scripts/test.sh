29ff312dff01dd09fd60072740f66dd0fbd6d9ac
ananas-block <jorrit@lightprotocol.com>
2025-01-11 18:06:27 +0000

    chore: add rust and program tests to test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,15 @@
 
 set -eux
 
-npx nx run-many --target=test --all --parallel=false
+npx nx run-many --target=test --all --parallel=false &&\
+    RUSTFLAGS="-D warnings" \
+            cargo test --all-targets --workspace \
+            --exclude light-concurrent-merkle-tree \
+            --exclude photon-api && \
+    cargo test-sbf -p account-compression-test &&\
+    cargo test-sbf -p compressed-token-test &&\
+    cargo test-sbf -p e2e-test &&\
+    cargo test-sbf -p registry-test &&\
+    cargo test-sbf -p sdk-test-program &&\
+    cargo test-sbf -p system-cpi-test &&\
+    cargo test-sbf -p system-test
