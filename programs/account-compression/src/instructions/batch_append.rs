3fb77b30bfad20ca1b82f4297a7dc1666ced92a3
ananas-block <jorrit@lightprotocol.com>
2025-01-13 23:12:57 +0000

    chore: add comments batch_append

--- programs/account-compression/src/instructions/batch_append.rs
+++ programs/account-compression/src/instructions/batch_append.rs
@@ -18,10 +18,10 @@ pub struct BatchAppend<'info> {
     pub registered_program_pda: Option<Account<'info, RegisteredProgram>>,
     /// CHECK: when emitting event.
     pub log_wrapper: UncheckedAccount<'info>,
-    /// CHECK: in from_account_info.
+    /// CHECK: in state_tree_from_account_info_mut.
     #[account(mut)]
     pub merkle_tree: AccountInfo<'info>,
-    /// CHECK: in from_account_info.
+    /// CHECK: in update_output_queue_account_info.
     #[account(mut)]
     pub output_queue: AccountInfo<'info>,
 }
@@ -35,18 +35,30 @@ impl<'info> GroupAccounts<'info> for BatchAppend<'info> {
     }
 }
 
+/// Append a batch of leaves from the output queue
+/// to the state Merkle tree.
+/// 1. Check Merkle tree account discriminator and program ownership.
+/// 2. Check that signer is registered or authority.
+/// 3. Append leaves from the output queue to the state Merkle tree.
+///     3.1 Checks that output queue is associated with the Merkle tree.
+///     3.2 Checks output queue discriminator, program ownership.
+///     3.3 Verifies batch zkp and updates root.
+/// 4. Emit indexer event.
 pub fn process_batch_append_leaves<'a, 'b, 'c: 'info, 'info>(
     ctx: &'a Context<'a, 'b, 'c, 'info, BatchAppend<'info>>,
     instruction_data: InstructionDataBatchAppendInputs,
 ) -> Result<()> {
+    // 1. Check Merkle tree account discriminator and program ownership.
     let merkle_tree =
         &mut BatchedMerkleTreeAccount::state_tree_from_account_info_mut(&ctx.accounts.merkle_tree)
             .map_err(ProgramError::from)?;
+    // 2. Check that signer is registered or authority.
     check_signer_is_registered_or_authority::<BatchAppend, BatchedMerkleTreeAccount>(
         ctx,
         merkle_tree,
     )?;
 
+    // 3. Append leaves and check output queue account.
     let event = merkle_tree
         .update_output_queue_account_info(
             &ctx.accounts.output_queue,
@@ -54,5 +66,6 @@ pub fn process_batch_append_leaves<'a, 'b, 'c: 'info, 'info>(
             ctx.accounts.merkle_tree.key().to_bytes(),
         )
         .map_err(ProgramError::from)?;
+    // 4. Emit indexer event.
     emit_indexer_event(event.try_to_vec()?, &ctx.accounts.log_wrapper)
 }
