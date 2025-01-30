6cf68ae41d0b50b506b85c450e24a19d0f74db5e
Swenschaeferjohann <swen@lightprotocol.com>
2025-01-30 06:02:45 +0100

    wip

--- program-tests/memo-test-program/src/lib.rs
+++ program-tests/memo-test-program/src/lib.rs
@@ -10,13 +10,12 @@ use solana_program::{
 declare_id!("2nvp5EoMsJ4qVTdTwAmJGfCSRmzRLs16Zw7r3842t2uy");
 entrypoint!(process_instruction);
 
+// TODO: to check ergonomics, add handler with unpack/pack account state.
 pub fn process_instruction(
     _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    // We expect the first account to be our authority, which must sign.
-
     let account_info_iter = &mut accounts.iter();
 
     let authority_info = next_account_info(account_info_iter)?;
