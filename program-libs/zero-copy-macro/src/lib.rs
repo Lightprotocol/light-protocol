af40b35b2cfcbbfdae5fad48c895484299e3d3bd
Sergey Timoshin <sergeytimoshin@proton.me>
2025-02-03 17:45:10 +0100

    2 iteration

--- program-libs/zero-copy-macro/src/lib.rs
+++ program-libs/zero-copy-macro/src/lib.rs
@@ -16,10 +16,10 @@ pub fn derive_zero_copy_account(input: TokenStream) -> TokenStream {
 
     let mut meta_fields = Vec::new();
     let mut optional_fields = Vec::new();
-    let mut meta_flags = Vec::new();
-    let mut from_conversions = Vec::new();
-    let mut deserialize_code = Vec::new();
+    let mut optional_field_inits = Vec::new();
     let mut meta_struct_fields = Vec::new();
+    let mut deserialize_code = Vec::new();
+    let mut deserialize_field_inits = Vec::new();
 
     for field in fields.iter() {
         let field_name = field.ident.as_ref().unwrap();
@@ -28,45 +28,34 @@ pub fn derive_zero_copy_account(input: TokenStream) -> TokenStream {
         if let Type::Path(type_path) = field_type {
             if let Some(segment) = type_path.path.segments.last() {
                 if segment.ident == "Option" {
-                    if let syn::Type::Path(inner_type) = parse_option_type(field_type) {
+                    if let Type::Path(inner_type) = parse_option_type(field_type) {
                         let inner_type_name = &inner_type.path.segments.last().unwrap().ident;
-                        let inner_type_str = inner_type_name.to_string();
-
                         let flag_name = format_ident!("{}_option", field_name);
-                        meta_flags.push(flag_name.clone());
+
                         meta_struct_fields.push(quote! {
                             pub #flag_name: u8
                         });
 
-                        if is_fixed_size_type(&inner_type_str) {
+                        if is_fixed_size_type(&inner_type_name.to_string()) {
                             optional_fields.push(quote! {
                                 pub #field_name: Option<zerocopy::Ref<&'a [u8], #inner_type_name>>
                             });
 
                             deserialize_code.push(quote! {
                                 let (#field_name, bytes) = if meta.#flag_name == 1 {
-                                    let (field_val, bytes) = zerocopy::Ref::<&[u8], #inner_type_name>::zero_copy_at(bytes)?;
-                                    (Some(field_val), bytes)
+                                    let (val, bytes) = zerocopy::Ref::<&[u8], #inner_type_name>::zero_copy_at(bytes)?;
+                                    (Some(val), bytes)
                                 } else {
                                     (None, bytes)
                                 };
                             });
 
-                            from_conversions.push(quote! {
-                                #field_name: compressed_account.#field_name.map(|x| *x),
-                            });
-                        } else {
-                            let z_inner_type = format_ident!("Z{}", inner_type_name);
-                            optional_fields.push(quote! {
-                                pub #field_name: Option<#z_inner_type<'a>>
+                            optional_field_inits.push(quote! {
+                                #field_name: z_account.#field_name.map(|x| *x)
                             });
 
-                            deserialize_code.push(quote! {
-                                let (#field_name, bytes) = Option::<#z_inner_type>::zero_copy_at(bytes)?;
-                            });
-
-                            from_conversions.push(quote! {
-                                #field_name: compressed_account.#field_name.as_ref().map(|data| data.into()),
+                            deserialize_field_inits.push(quote! {
+                                #field_name: #field_name
                             });
                         }
                     }
@@ -79,72 +68,57 @@ pub fn derive_zero_copy_account(input: TokenStream) -> TokenStream {
         meta_struct_fields.push(quote! {
             pub #field_name: #converted_type
         });
-        meta_fields.push(field_name.clone());
+        meta_fields.push(quote! {
+            #field_name: z_account.meta.#field_name.into()
+        });
     }
 
     let meta_struct_name = format_ident!("{}DesMeta", struct_name);
-    let meta_struct = quote! {
+
+    let expanded = quote! {
         #[repr(C)]
-        #[derive(zerocopy::FromBytes, zerocopy::IntoBytes, zerocopy::Unaligned)]
+        #[derive(zerocopy::FromBytes, zerocopy::AsBytes, zerocopy::Unaligned)]
         pub struct #meta_struct_name {
             #(#meta_struct_fields),*
         }
-    };
 
-    let z_struct = quote! {
         #[derive(Debug, PartialEq, Clone)]
         pub struct #z_struct_name<'a> {
             meta: zerocopy::Ref<&'a [u8], #meta_struct_name>,
             #(#optional_fields),*
         }
-    };
 
-    let deref_impl = quote! {
         impl std::ops::Deref for #z_struct_name<'_> {
             type Target = #meta_struct_name;
-
             fn deref(&self) -> &Self::Target {
                 &self.meta
             }
         }
-    };
 
-    let from_impl = quote! {
         impl From<&#z_struct_name<'_>> for #struct_name {
-            fn from(compressed_account: &#z_struct_name) -> Self {
-                #struct_name {
-                    #(#meta_fields: compressed_account.meta.#meta_fields.into()),*,
-                    #(#from_conversions)*
+            fn from(z_account: &#z_struct_name) -> Self {
+                Self {
+                    #(#meta_fields,)*
+                    #(#optional_field_inits),*
                 }
             }
         }
-    };
 
-    let deserialize_impl = quote! {
         impl<'a> light_zero_copy::borsh::Deserialize<'a> for #z_struct_name<'a> {
             type Output = Self;
 
-            #[inline]
             fn zero_copy_at(bytes: &'a [u8]) -> Result<(Self::Output, &'a [u8]), light_zero_copy::errors::ZeroCopyError> {
-                let (meta, bytes) = zerocopy::Ref::<&[u8], #meta_struct_name>::from_prefix(bytes)?;
+                let (meta, mut bytes) = zerocopy::Ref::<&[u8], #meta_struct_name>::from_prefix(bytes)?;
                 #(#deserialize_code)*
                 Ok((
-                    #z_struct_name {
+                    Self {
                         meta,
-                        #(#optional_fields: #meta_flags),*
+                        #(#deserialize_field_inits),*
                     },
                     bytes
                 ))
             }
         }
-    };
-
-    let expanded = quote! {
-        #meta_struct
-        #z_struct
-        #deref_impl
-        #from_impl
-        #deserialize_impl
     };
 
     TokenStream::from(expanded)
@@ -153,29 +127,24 @@ pub fn derive_zero_copy_account(input: TokenStream) -> TokenStream {
 fn parse_option_type(ty: &Type) -> &Type {
     if let Type::Path(type_path) = ty {
         if let Some(segment) = type_path.path.segments.last() {
-            if segment.ident == "Option" {
-                if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
-                    if let Some(syn::GenericArgument::Type(inner_type)) = args.args.first() {
-                        return inner_type;
-                    }
+            if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                if let Some(syn::GenericArgument::Type(inner_type)) = args.args.first() {
+                    return inner_type;
                 }
             }
         }
     }
-    panic!("Expected Option type");
+    panic!("Expected Option type")
 }
 
 fn is_fixed_size_type(type_str: &str) -> bool {
-    matches!(
-        type_str,
-        "u8" | "u16" | "u32" | "u64" | "i8" | "i16" | "i32" | "i64" | "[u8; 32]"
-    )
+    matches!(type_str, "u8" | "u16" | "u32" | "u64" | "i8" | "i16" | "i32" | "i64" | "[u8; 32]")
 }
 
 fn convert_type(ty: &Type) -> proc_macro2::TokenStream {
     if let Type::Path(type_path) = ty {
-        let type_ident = type_path.path.segments.last().unwrap().ident.to_string();
-        match type_ident.as_str() {
+        let type_str = type_path.path.segments.last().unwrap().ident.to_string();
+        match type_str.as_str() {
             "u64" => quote! { zerocopy::U64 },
             "u32" => quote! { zerocopy::U32 },
             "u16" => quote! { zerocopy::U16 },
