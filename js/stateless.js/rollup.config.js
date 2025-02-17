ee702493d0b70d6e307a9fa979864d6691261e6e
Swenschaeferjohann <swen@lightprotocol.com>
2025-02-17 02:12:04 +0000

    bump, rm winston

--- js/stateless.js/rollup.config.js
+++ js/stateless.js/rollup.config.js
@@ -14,7 +14,7 @@ const rolls = (fmt, env) => ({
         entryFileNames: `[name].${fmt === 'cjs' ? 'cjs' : 'js'}`,
         sourcemap: true,
     },
-    external: ['@solana/web3.js', 'winston'],
+    external: ['@solana/web3.js'],
     plugins: [
         typescript({
             target: fmt === 'es' ? 'ES2022' : 'ES2017',
--- js/stateless.js/src/logger.ts
+++ js/stateless.js/src/logger.ts
@@ -1,19 +1,19 @@
-const winston = require('winston');
+// const winston = require('winston');
 
-// use only if winston is available in your environment, and if you know what
-// you are doing.
-//
-// call logger.info('text', { text: 'test' })
-export const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.prettyPrint(),
-    ),
-    transports: [
-        new winston.transports.File({ filename: 'combined.log' }),
-        new winston.transports.Console({
-            format: winston.format.prettyPrint(),
-        }),
-    ],
-});
+// // use only if winston is available in your environment, and if you know what
+// // you are doing.
+// //
+// // call logger.info('text', { text: 'test' })
+// export const logger = winston.createLogger({
+//     level: 'info',
+//     format: winston.format.combine(
+//         winston.format.timestamp(),
+//         winston.format.prettyPrint(),
+//     ),
+//     transports: [
+//         new winston.transports.File({ filename: 'combined.log' }),
+//         new winston.transports.Console({
+//             format: winston.format.prettyPrint(),
+//         }),
+//     ],
+// });
