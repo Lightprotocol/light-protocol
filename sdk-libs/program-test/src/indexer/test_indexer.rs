6065989bd47d3382ab14eb3b81a48d1a5c8ef843
Sergey Timoshin <sergeytimoshin@proton.me>
2025-02-19 01:02:20 +0000

    set prove_by_index = false

--- sdk-libs/program-test/src/indexer/test_indexer.rs
+++ sdk-libs/program-test/src/indexer/test_indexer.rs
@@ -1772,7 +1772,7 @@ where
                                         leaf_index: event.output_leaf_indices[i],
                                         merkle_tree_pubkey,
                                         nullifier_queue_pubkey,
-                                        prove_by_index: true,
+                                        prove_by_index: false,
                                     },
                                 },
                             };
@@ -1800,7 +1800,7 @@ where
                             leaf_index: event.output_leaf_indices[i],
                             merkle_tree_pubkey,
                             nullifier_queue_pubkey,
-                            prove_by_index: true,
+                            prove_by_index: false,
                         },
                     };
                     compressed_accounts.push(compressed_account.clone());
