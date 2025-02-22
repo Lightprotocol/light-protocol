9248df397cb9e2abafa2fd995bcc677c87371cad
Sergey Timoshin <sergeytimoshin@proton.me>
2025-02-22 20:39:58 +0000

    test_indexer: set prove_by_index to false to make it aligned with photon

--- sdk-libs/program-test/src/indexer/test_indexer.rs
+++ sdk-libs/program-test/src/indexer/test_indexer.rs
@@ -1906,7 +1906,7 @@ where
                                         leaf_index: event.output_leaf_indices[i],
                                         merkle_tree_pubkey,
                                         nullifier_queue_pubkey,
-                                        prove_by_index: true,
+                                        prove_by_index: false,
                                     },
                                 },
                             };
@@ -1934,7 +1934,7 @@ where
                             leaf_index: event.output_leaf_indices[i],
                             merkle_tree_pubkey,
                             nullifier_queue_pubkey,
-                            prove_by_index: true,
+                            prove_by_index: false,
                         },
                     };
                     compressed_accounts.push(compressed_account.clone());
