68cd52df5f2cb78c03606683e3540f34111b7834
Sergey Timoshin <sergeytimoshin@proton.me>
2025-03-03 09:21:51 +0000

    refactor: simplify hash_with_hashed_values call in compressed_account.rs

--- program-libs/compressed-account/src/compressed_account.rs
+++ program-libs/compressed-account/src/compressed_account.rs
@@ -244,14 +244,13 @@ impl CompressedAccount {
         let hashed_mt = hash_to_bn254_field_size_be(&merkle_tree_pubkey.to_bytes())
             .unwrap()
             .0;
-        let result = self.hash_with_hashed_values::<H>(
+        self.hash_with_hashed_values::<H>(
             &hash_to_bn254_field_size_be(&self.owner.to_bytes())
                 .unwrap()
                 .0,
             &hashed_mt,
             leaf_index,
-        );
-        result
+        )
     }
 }
 
