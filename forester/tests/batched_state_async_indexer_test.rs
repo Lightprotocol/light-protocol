ba9040e2e1e38c5454a30b5e23dcf72e855d0597
Sergey Timoshin <sergeytimoshin@proton.me>
2025-03-18 14:36:46 +0000

    use "merkleContexts"

--- forester/tests/batched_state_async_indexer_test.rs
+++ forester/tests/batched_state_async_indexer_test.rs
@@ -42,7 +42,6 @@ use tokio::{
     sync::{mpsc, oneshot, Mutex},
     time::{sleep, timeout},
 };
-
 use crate::test_utils::{forester_config, init};
 
 mod test_utils;
@@ -164,8 +163,7 @@ async fn test_state_indexer_async_batched() {
     .await;
     wait_for_indexer(&mut rpc, &photon_indexer).await.unwrap();
 
-    let input_compressed_accounts =
-        get_token_accounts(&photon_indexer, &batch_payer.pubkey(), &mint_pubkey).await;
+    let input_compressed_accounts = get_token_accounts(&photon_indexer, &batch_payer.pubkey(), &mint_pubkey).await;
     validate_compressed_accounts_proof(&mut rpc, &photon_indexer, &input_compressed_accounts).await;
 
     let rng_seed = rand::thread_rng().gen::<u64>();
@@ -353,8 +351,7 @@ async fn validate_compressed_accounts_proof<R: RpcConnection, I: Indexer<R>>(
     wait_for_indexer(rpc, indexer).await.unwrap();
     let proof = indexer
         .get_validity_proof_v2(compressed_account_hashes, vec![])
-        .await
-        .unwrap();
+        .await;
     println!("proof_for_compressed_accounts: {:?}", proof);
 }
 
