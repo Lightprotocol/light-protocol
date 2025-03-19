6ab7e9fce97952c37bf47f73ffa641b65381a792
Sergey Timoshin <sergeytimoshin@proton.me>
2025-03-19 14:21:49 +0000

    wip

--- program-libs/indexed-array/src/array.rs
+++ program-libs/indexed-array/src/array.rs
@@ -87,9 +87,6 @@ where
     where
         H: Hasher,
     {
-        println!("self.value: {:?}", self.value);
-        println!("self.next_index: {:?}", self.next_index());
-        println!("next_value: {:?}", next_value);
         let hash = H::hashv(&[
             bigint_to_be_bytes_array::<32>(&self.value)?.as_ref(),
             bigint_to_be_bytes_array::<32>(next_value)?.as_ref(),
