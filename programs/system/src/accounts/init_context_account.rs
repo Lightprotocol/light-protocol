368f9f08272db78c74b2ade1a1c2fead27dd0a96
ananas-block <jorrit@lightprotocol.com>
2025-04-22 03:49:34 +0100

    refactor: use account_info_init instead of manually setting the discriminator

--- programs/system/src/accounts/init_context_account.rs
+++ programs/system/src/accounts/init_context_account.rs
@@ -1,6 +1,6 @@
 use borsh::BorshSerialize;
 use light_account_checks::{
-    checks::{check_data_is_zeroed, check_owner, check_signer},
+    checks::{account_info_init, check_owner, check_signer},
     discriminator::Discriminator,
 };
 use light_batched_merkle_tree::merkle_tree::BatchedMerkleTreeAccount;
@@ -9,10 +9,7 @@ use light_compressed_account::constants::{
 };
 use pinocchio::{account_info::AccountInfo, program_error::ProgramError};
 
-use crate::{
-    errors::SystemProgramError, invoke_cpi::account::CpiContextAccount, Result,
-    CPI_CONTEXT_ACCOUNT_DISCRIMINATOR,
-};
+use crate::{errors::SystemProgramError, invoke_cpi::account::CpiContextAccount, Result};
 pub struct InitializeCpiContextAccount<'info> {
     pub fee_payer: &'info AccountInfo,
     pub cpi_context_account: &'info AccountInfo,
@@ -55,12 +52,11 @@ pub fn init_cpi_context_account(accounts: &[AccountInfo]) -> Result<()> {
     // Check that Merkle tree is initialized.
     let ctx = InitializeCpiContextAccount::from_account_infos(accounts)?;
 
-    let mut cpi_context_account_data = ctx.cpi_context_account.try_borrow_mut_data()?;
-    // Check account is not initialized.
-    check_data_is_zeroed::<8>(&cpi_context_account_data[..]).map_err(ProgramError::from)?;
-    // Initialize account with discriminator.
-    cpi_context_account_data[..8].copy_from_slice(&CPI_CONTEXT_ACCOUNT_DISCRIMINATOR);
+    // 1. Check discriminator bytes are zeroed.
+    // 2. Set discriminator.
+    account_info_init::<CpiContextAccount>(ctx.cpi_context_account)?;
 
+    let mut cpi_context_account_data = ctx.cpi_context_account.try_borrow_mut_data()?;
     let cpi_context_account = CpiContextAccount {
         associated_merkle_tree: *ctx.associated_merkle_tree.key(),
         ..Default::default()
