ae2a15c4a197ac680c3201c0932b51dda723992b
Swenschaeferjohann <swen@lightprotocol.com>
2025-05-11 00:07:14 +0100

    link to computebudgetprogram

--- js/stateless.js/src/utils/calculate-compute-unit-price.ts
+++ js/stateless.js/src/utils/calculate-compute-unit-price.ts
@@ -2,7 +2,7 @@
  * @param targetLamports - Target priority fee in lamports
  * @param computeUnits - Expected compute units used by the transaction
  * @returns microLamports per compute unit (use in
- * `ComputeBudgetProgram.setComputeUnitPrice`)
+ * {@link https://github.com/solana-foundation/solana-web3.js/blob/maintenance/v1.x/src/programs/compute-budget.ts#L218})
  */
 export function calculateComputeUnitPrice(
     targetLamports: number,
