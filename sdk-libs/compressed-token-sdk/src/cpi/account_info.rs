25ce422f437751885bfc891505b567b23864e4cd
Swenschaeferjohann <swen@lightprotocol.com>
2025-06-03 21:54:15 -0400

    wip

--- sdk-libs/compressed-token-sdk/src/cpi/account_info.rs
+++ sdk-libs/compressed-token-sdk/src/cpi/account_info.rs
@@ -1,6 +1,6 @@
 use light_compressed_account::compressed_account::PackedMerkleContext;
+use light_compressed_token::process_transfer::InputTokenDataWithContext;
 
-use crate::state::InputTokenDataWithContext;
 /// Get an existing compressed token account from token_data in optimized
 /// format.
 ///
