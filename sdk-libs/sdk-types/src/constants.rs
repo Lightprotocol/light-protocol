0a9b0322ca5a695da8e97a1cb7dd081b54f832b0
ananas-block <jorrit@lightprotocol.com>
2025-06-11 20:55:06 +0100

    unify program id constants

--- sdk-libs/sdk-types/src/constants.rs
+++ sdk-libs/sdk-types/src/constants.rs
@@ -1,25 +1,20 @@
 use light_macros::pubkey_array;
 
+/// ID of the account-compression program.
 pub const ACCOUNT_COMPRESSION_PROGRAM_ID: [u8; 32] =
     pubkey_array!("compr6CUsB5m2jS4Y3831ztGSTnDpnKJTKS95d64XVq");
+/// ID of the light-system program.
 pub const SYSTEM_PROGRAM_ID: [u8; 32] =
     pubkey_array!("SySTEM1eSU2p4BGQfQpimFEWWSC1XDFeun3Nqzz3rT7");
 pub const REGISTERED_PROGRAM_PDA: [u8; 32] =
     pubkey_array!("35hkDgaAKwMCaxRz2ocSZ6NaUrtKkyNqU6c4RV3tYJRh");
+/// ID of the light-compressed-token program.
+pub const C_TOKEN_PROGRAM_ID: [u8; 32] =
+    pubkey_array!("cTokenmWW8bLPjZEBAUgYy3zKxQZW6VKi7bqNFEVv3m");
 
 /// Seed of the CPI authority.
 pub const CPI_AUTHORITY_PDA_SEED: &[u8] = b"cpi_authority";
-
-/// ID of the account-compression program.
-pub const PROGRAM_ID_ACCOUNT_COMPRESSION: [u8; 32] =
-    pubkey_array!("compr6CUsB5m2jS4Y3831ztGSTnDpnKJTKS95d64XVq");
-pub const PROGRAM_ID_NOOP: [u8; 32] = pubkey_array!("noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV");
-/// ID of the light-system program.
-pub const PROGRAM_ID_LIGHT_SYSTEM: [u8; 32] =
-    pubkey_array!("SySTEM1eSU2p4BGQfQpimFEWWSC1XDFeun3Nqzz3rT7");
-/// ID of the light-compressed-token program.
-pub const PROGRAM_ID_LIGHT_TOKEN: [u8; 32] =
-    pubkey_array!("cTokenmWW8bLPjZEBAUgYy3zKxQZW6VKi7bqNFEVv3m");
+pub const NOOP_PROGRAM_ID: [u8; 32] = pubkey_array!("noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV");
 
 pub const STATE_MERKLE_TREE_HEIGHT: usize = 26;
 pub const STATE_MERKLE_TREE_CHANGELOG: usize = 1400;
