0b5508e8d41e36bee55d0501476b99217c70fed1
ananas-block <jorrit@lightprotocol.com>
2025-06-15 01:57:48 +0100

    update photon commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -103,7 +103,7 @@ install_rust() {
         export PATH="${PREFIX}/cargo/bin:${PATH}"
         rustup component add --toolchain 1.86-x86_64-unknown-linux-gnu clippy
         cargo install cargo-expand --locked
-        cargo install --git https://github.com/helius-labs/photon.git --rev 9f900af00e402344a548560f9151d84416a79b31 --locked
+        cargo install --git https://github.com/helius-labs/photon.git --rev dbeb89e639bda78f0e135b9b1aa75bfe16618cb4 --locked
         log "rust"
     fi
 }
