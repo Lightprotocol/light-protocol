da70d1ce9dc1f2615a262b1f1072bb20750915fc
ananas-block <jorrit@lightprotocol.com>
2025-06-20 21:19:46 +0200

    stash

--- program-tests/sdk-token-test/src/process_create_compressed_account.rs
+++ program-tests/sdk-token-test/src/process_create_compressed_account.rs
@@ -3,7 +3,10 @@ use anchor_lang::solana_program::log::sol_log_compute_units;
 use light_compressed_account::instruction_data::cpi_context::CompressedCpiContext;
 use light_compressed_token_sdk::{
     account::CTokenAccount,
-    instructions::transfer::instruction::{TransferConfig, TransferInputs},
+    instructions::transfer::{
+        account_infos::{filter_packed_accounts, TransferAccountInfos, MAX_ACCOUNT_INFOS},
+        instruction::{TransferConfig, TransferInputs},
+    },
     TokenAccountMeta,
 };
 use light_sdk::{
@@ -90,9 +93,9 @@ pub fn deposit_tokens<'info>(
 
     // We need to be careful what accounts we pass.
     // Big accounts cost many CU.
-    // TODO: add a filter tree accounts function.
+    // TODO: replace
     let tree_account_infos =
-        filter_tree_accounts(&[&sender_account], cpi_accounts.tree_accounts().unwrap());
+        filter_packed_accounts(&[&sender_account], cpi_accounts.tree_accounts().unwrap());
     let tree_pubkeys = tree_account_infos
         .iter()
         .map(|x| x.pubkey())
@@ -129,20 +132,17 @@ pub fn deposit_tokens<'info>(
 
     msg!("create_account_infos");
     sol_log_compute_units();
-    let account_infos = TransferAccountInfos {
+    // TODO: initialize from CpiAccounts, use with_compressed_pda() offchain.
+    let account_infos: TransferAccountInfos<'_, 'info, MAX_ACCOUNT_INFOS> = TransferAccountInfos {
         fee_payer: cpi_accounts.fee_payer(),
         authority: cpi_accounts.fee_payer(),
-        tree_accounts: tree_account_infos.as_slice(),
+        packed_accounts: tree_account_infos.as_slice(),
         ctoken_accounts: token_account_infos,
         cpi_context: Some(cpi_context),
     };
     let account_infos = account_infos.into_account_infos();
-    // 1390968
-    // let account_infos = create_account_infos(
-    //     &instruction,
-    //     token_account_infos,
-    //     &[cpi_accounts.fee_payer()],
-    // );
+    // into_account_infos_checked() can be used for debugging but doubles CU cost to 1.5k CU
+
     sol_log_compute_units();
 
     sol_log_compute_units();
@@ -152,120 +152,4 @@ pub fn deposit_tokens<'info>(
     sol_log_compute_units();
 
     Ok(())
-}
-
-// 7479
-fn create_account_infos<'info>(
-    instruction: &Instruction,
-    account_infos: &[AccountInfo<'info>],
-    additionals: &[&AccountInfo<'info>],
-) -> Vec<AccountInfo<'info>> {
-    let mut res_account_infos = Vec::with_capacity(instruction.accounts.len());
-
-    for (i, account_meta) in instruction.accounts.iter().enumerate() {
-        if let Some(account_info) = account_infos[i..]
-            .iter()
-            .find(|x| *x.key == account_meta.pubkey)
-        {
-            res_account_infos.push(account_info.clone());
-        } else {
-            if let Some(account_info) = additionals.iter().find(|x| *x.key == account_meta.pubkey) {
-                res_account_infos.push((*account_info).clone());
-            } else {
-                if let Some(account_info) = account_infos[..i]
-                    .iter()
-                    .find(|x| *x.key == account_meta.pubkey)
-                {
-                    res_account_infos.push(account_info.clone());
-                } else {
-                    panic!("account not found");
-                }
-            }
-        }
-    }
-    res_account_infos
-}
-
-// For pinocchio we will need to build the accounts in oder
-// The easiest is probably just pass the accounts multiple times since deserialization is zero copy.
-pub struct TransferAccountInfos<'a, 'info> {
-    fee_payer: &'a AccountInfo<'info>,
-    authority: &'a AccountInfo<'info>,
-    ctoken_accounts: &'a [AccountInfo<'info>],
-    cpi_context: Option<&'a AccountInfo<'info>>,
-    // TODO: rename tree accounts to packed accounts
-    tree_accounts: &'a [AccountInfo<'info>],
-}
-
-use anchor_lang::solana_program::instruction::Instruction;
-impl<'info> TransferAccountInfos<'_, 'info> {
-    // 874
-    fn into_account_infos(self) -> Vec<AccountInfo<'info>> {
-        // TODO: experiment with array vec.
-        // we can use array vec with default constant say 20 and in case it's not enough
-        // we throw an error that the constant needs to be increased.
-        let mut capacity = 2 + self.ctoken_accounts.len() + self.tree_accounts.len();
-        let ctoken_program_id_index = self.ctoken_accounts.len() - 2;
-        if self.cpi_context.is_some() {
-            capacity += 1;
-        }
-        let mut account_infos = Vec::with_capacity(capacity);
-        account_infos.push(self.fee_payer.clone());
-        account_infos.push(self.authority.clone());
-
-        account_infos.extend_from_slice(self.ctoken_accounts);
-        if let Some(cpi_context) = self.cpi_context {
-            account_infos.push(cpi_context.clone());
-        } else {
-            account_infos.push(self.ctoken_accounts[ctoken_program_id_index].clone());
-        }
-        account_infos.extend_from_slice(self.tree_accounts);
-        account_infos
-    }
-
-    // 1528
-    fn into_account_infos_checked(self, ix: &Instruction) -> Result<Vec<AccountInfo<'info>>> {
-        let account_infos = self.into_account_infos();
-        for (account_meta, account_info) in ix.accounts.iter().zip(account_infos.iter()) {
-            if account_meta.pubkey != *account_info.key {
-                msg!("account meta {:?}", account_meta);
-                msg!("account info {:?}", account_info);
-
-                msg!("account metas {:?}", ix.accounts);
-                msg!("account infos {:?}", account_infos);
-                panic!("account info and meta don't match.");
-            }
-        }
-        Ok(account_infos)
-    }
-}
-
-// TODO: test
-fn filter_tree_accounts<'info>(
-    token_accounts: &[&CTokenAccount],
-    account_infos: &[AccountInfo<'info>],
-) -> Vec<AccountInfo<'info>> {
-    let mut selected_account_infos = Vec::with_capacity(account_infos.len());
-    account_infos
-        .iter()
-        .enumerate()
-        .filter(|(i, _)| {
-            let i = *i as u8;
-            token_accounts.iter().any(|y| {
-                y.merkle_tree_index == i
-                    || y.input_metas().iter().any(|z| {
-                        z.packed_tree_info.merkle_tree_pubkey_index == i
-                            || z.packed_tree_info.queue_pubkey_index == i
-                            || {
-                                if let Some(delegate_index) = z.delegate_index {
-                                    delegate_index == i
-                                } else {
-                                    false
-                                }
-                            }
-                    })
-            })
-        })
-        .for_each(|x| selected_account_infos.push(x.1.clone()));
-    selected_account_infos
 }
