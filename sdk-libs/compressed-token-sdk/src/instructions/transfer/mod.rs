2cea4a11e0113e33ceb6ff2a2a8f29a40a666a33
ananas-block <jorrit@lightprotocol.com>
2025-06-20 21:19:46 +0200

    stash

--- sdk-libs/compressed-token-sdk/src/instructions/transfer/mod.rs
+++ sdk-libs/compressed-token-sdk/src/instructions/transfer/mod.rs
@@ -1,6 +1,7 @@
 use light_compressed_token_types::account_infos::TransferAccountInfos as TransferAccountInfosTypes;
 use solana_account_info::AccountInfo;
 
+pub mod account_infos;
 pub mod account_metas;
 pub mod instruction;
 
