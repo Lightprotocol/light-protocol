f486b8172097ae260199326ccb9bdabe9583794b
ananas-block <jorrit@lightprotocol.com>
2025-07-04 23:42:06 +0100

    fix: instruction matching

--- programs/compressed-token/program/src/lib.rs
+++ programs/compressed-token/program/src/lib.rs
@@ -1,5 +1,6 @@
-use anchor_lang::solana_program::{
-    account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey,
+use anchor_lang::{
+    solana_program::{account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey},
+    Discriminator,
 };
 use spl_token::instruction::TokenInstruction;
 
@@ -18,33 +19,46 @@ impl From<u8> for InstructionType {
     }
 }
 
-#[cfg(feature = "cpi")]
+#[cfg(not(feature = "cpi"))]
 anchor_lang::solana_program::entrypoint!(process_instruction);
 
-pub fn process_instruction<'a, 'b, 'c, 'info>(
-    program_id: &'a Pubkey,
+pub fn process_instruction<'info>(
+    program_id: &Pubkey,
     accounts: &'info [AccountInfo<'info>],
-    instruction_data: &'c [u8],
+    instruction_data: &[u8],
 ) -> Result<(), ProgramError> {
     let discriminator = InstructionType::from(instruction_data[0]);
     match discriminator {
-        // TODO: match anchor instructions before
         InstructionType::DecompressedTransfer => {
-            // unpack instruction
             let instruction = TokenInstruction::unpack(instruction_data)?;
             match instruction {
                 TokenInstruction::Transfer { amount } => {
                     spl_token::processor::Processor::process_transfer(
-                        program_id, accounts, amount, None, // TODO: check where to get these
-                    )
+                        program_id, accounts, amount, None,
+                    )?;
                 }
-                _ => Err(ProgramError::InvalidInstructionData),
+                _ => return Err(ProgramError::InvalidInstructionData),
             }
         }
-        // InstructionType::UpdatePdaBorsh => {
-        //     update_pda::update_pda::<false>(accounts, &instruction_data[1..])
-        // }
-        _ => light_compressed_token::entry(program_id, accounts, instruction_data),
-    }?;
+        // anchor instructions have no discriminator conflicts with InstructionType
+        _ => light_compressed_token::entry(program_id, accounts, instruction_data)?,
+    }
+
+    //     light_compressed_token::instruction::CreateCompressedMint::DISCRIMINATOR
+    //     | light_compressed_token::instruction::MintToCompressed::DISCRIMINATOR
+    //     | light_compressed_token::instruction::CreateSplMint::DISCRIMINATOR
+    //     | light_compressed_token::instruction::CreateTokenPool::DISCRIMINATOR
+    //     | light_compressed_token::instruction::AddTokenPool::DISCRIMINATOR
+    //     | light_compressed_token::instruction::MintTo::DISCRIMINATOR
+    //     | light_compressed_token::instruction::BatchCompress::DISCRIMINATOR
+    //     | light_compressed_token::instruction::CompressSplTokenAccount::DISCRIMINATOR
+    //     | light_compressed_token::instruction::Transfer::DISCRIMINATOR
+    //     | light_compressed_token::instruction::Approve::DISCRIMINATOR
+    //     | light_compressed_token::instruction::Revoke::DISCRIMINATOR
+    //     | light_compressed_token::instruction::Freeze::DISCRIMINATOR
+    //     | light_compressed_token::instruction::Thaw::DISCRIMINATOR
+    //     | light_compressed_token::instruction::Burn::DISCRIMINATOR => {
+    //         light_compressed_token::entry(program_id, accounts, instruction_data)?;
+
     Ok(())
 }
