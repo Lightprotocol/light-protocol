6773f8113b942273a03ebaf1690d46de585ceeb9
Swenschaeferjohann <swen@lightprotocol.com>
2025-07-06 10:22:22 -0400

    wip

--- program-tests/anchor-compressible-user/src/lib.rs
+++ program-tests/anchor-compressible-user/src/lib.rs
@@ -16,7 +16,7 @@ pub mod anchor_compressible_user {
         proof: ValidityProof,
         compressed_address: [u8; 32],
         address_tree_info: PackedAddressTreeInfo,
-        output_tree_index: u8,
+        output_state_tree_index: u8,
     ) -> Result<()> {
         let user_record = &mut ctx.accounts.user_record;
 
@@ -24,9 +24,29 @@ pub mod anchor_compressible_user {
         user_record.name = name;
         user_record.score = 0;
 
+        let cpi_accounts = CpiAccounts::new_with_config(
+            &ctx.accounts.user, // fee_payer
+            &ctx.remaining_accounts[..],
+            CpiAccountsConfig::new(crate::LIGHT_CPI_SIGNER),
+        );
+        let new_address_params =
+            address_tree_info.into_new_address_params_packed(user_record.key.to_bytes());
+
+        compress_pda_new::<MyPdaAccount>(
+            &user_record,
+            compressed_address,
+            new_address_params,
+            output_state_tree_index,
+            proof,
+            cpi_accounts,
+            &crate::ID,
+            &ctx.accounts.rent_recipient,
+            &ADDRESS_SPACE,
+        )?;
         Ok(())
     }
 
+    /// Can be the same because the PDA will be decompressed in a separate instruction.
     /// Updates an existing user record
     pub fn update_record(ctx: Context<UpdateRecord>, name: String, score: u64) -> Result<()> {
         let user_record = &mut ctx.accounts.user_record;
@@ -51,7 +71,7 @@ pub struct CreateRecord<'info> {
     )]
     pub user_record: Account<'info, UserRecord>,
     pub system_program: Program<'info, System>,
-    // UNCHECKED.
+    #[account(address = RENT_RECIPIENT)]
     pub rent_recipient: AccountInfo<'info>,
 }
 
