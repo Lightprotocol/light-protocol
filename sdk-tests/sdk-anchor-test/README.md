b6dffb62cd56092a80f966dc6a6aff9efd405e29
ananas-block <jorrit@lightprotocol.com>
2025-08-15 17:40:35 +0200

    refactor: move sdk-test, sdk-pinocchio-test, client-test from program-tests/ -> sdk-tests

---
+++ sdk-tests/sdk-anchor-test/README.md
@@ -0,0 +1,1 @@
+# SDK test program
---
+++ sdk-tests/sdk-anchor-test/programs/sdk-anchor-test/src/lib.rs
@@ -0,0 +1,196 @@
+#![allow(unexpected_cfgs)]
+
+use anchor_lang::{prelude::*, Discriminator};
+use light_sdk::{
+    account::LightAccount,
+    address::v1::derive_address,
+    cpi::{CpiAccounts, CpiInputs, CpiSigner},
+    derive_light_cpi_signer,
+    instruction::{account_meta::CompressedAccountMeta, PackedAddressTreeInfo, ValidityProof},
+    LightDiscriminator, LightHasher,
+};
+
+declare_id!("2tzfijPBGbrR5PboyFUFKzfEoLTwdDSHUjANCw929wyt");
+
+pub const LIGHT_CPI_SIGNER: CpiSigner =
+    derive_light_cpi_signer!("2tzfijPBGbrR5PboyFUFKzfEoLTwdDSHUjANCw929wyt");
+
+#[program]
+pub mod sdk_anchor_test {
+
+    use super::*;
+
+    pub fn create_compressed_account<'info>(
+        ctx: Context<'_, '_, '_, 'info, WithNestedData<'info>>,
+        proof: ValidityProof,
+        address_tree_info: PackedAddressTreeInfo,
+        output_tree_index: u8,
+        name: String,
+    ) -> Result<()> {
+        let light_cpi_accounts = CpiAccounts::new(
+            ctx.accounts.signer.as_ref(),
+            ctx.remaining_accounts,
+            crate::LIGHT_CPI_SIGNER,
+        );
+
+        let (address, address_seed) = derive_address(
+            &[b"compressed", name.as_bytes()],
+            &address_tree_info
+                .get_tree_pubkey(&light_cpi_accounts)
+                .map_err(|_| ErrorCode::AccountNotEnoughKeys)?,
+            &crate::ID,
+        );
+        let new_address_params = address_tree_info.into_new_address_params_packed(address_seed);
+
+        let mut my_compressed_account = LightAccount::<'_, MyCompressedAccount>::new_init(
+            &crate::ID,
+            Some(address),
+            output_tree_index,
+        );
+
+        my_compressed_account.name = name;
+        my_compressed_account.nested = NestedData::default();
+
+        let cpi_inputs = CpiInputs::new_with_address(
+            proof,
+            vec![my_compressed_account
+                .to_account_info()
+                .map_err(ProgramError::from)?],
+            vec![new_address_params],
+        );
+
+        cpi_inputs
+            .invoke_light_system_program(light_cpi_accounts)
+            .map_err(ProgramError::from)?;
+
+        Ok(())
+    }
+
+    pub fn update_compressed_account<'info>(
+        ctx: Context<'_, '_, '_, 'info, UpdateNestedData<'info>>,
+        proof: ValidityProof,
+        my_compressed_account: MyCompressedAccount,
+        account_meta: CompressedAccountMeta,
+        nested_data: NestedData,
+    ) -> Result<()> {
+        let mut my_compressed_account = LightAccount::<'_, MyCompressedAccount>::new_mut(
+            &crate::ID,
+            &account_meta,
+            my_compressed_account,
+        )
+        .map_err(ProgramError::from)?;
+
+        my_compressed_account.nested = nested_data;
+
+        let light_cpi_accounts = CpiAccounts::new(
+            ctx.accounts.signer.as_ref(),
+            ctx.remaining_accounts,
+            crate::LIGHT_CPI_SIGNER,
+        );
+
+        let cpi_inputs = CpiInputs::new(
+            proof,
+            vec![my_compressed_account
+                .to_account_info()
+                .map_err(ProgramError::from)?],
+        );
+
+        cpi_inputs
+            .invoke_light_system_program(light_cpi_accounts)
+            .map_err(ProgramError::from)?;
+
+        Ok(())
+    }
+
+    pub fn without_compressed_account<'info>(
+        ctx: Context<'_, '_, '_, 'info, WithoutCompressedAccount<'info>>,
+        name: String,
+    ) -> Result<()> {
+        ctx.accounts.my_regular_account.name = name;
+        Ok(())
+    }
+}
+
+#[event]
+#[derive(Clone, Debug, Default, LightHasher, LightDiscriminator)]
+pub struct MyCompressedAccount {
+    #[hash]
+    pub name: String,
+    pub nested: NestedData,
+}
+
+// Illustrates nested hashing feature.
+#[derive(LightHasher, Clone, Debug, AnchorSerialize, AnchorDeserialize)]
+pub struct NestedData {
+    pub one: u16,
+    pub two: u16,
+    pub three: u16,
+    pub four: u16,
+    pub five: u16,
+    pub six: u16,
+    pub seven: u16,
+    pub eight: u16,
+    pub nine: u16,
+    pub ten: u16,
+    pub eleven: u16,
+    pub twelve: u16,
+}
+
+impl Default for NestedData {
+    fn default() -> Self {
+        Self {
+            one: 1,
+            two: 2,
+            three: 3,
+            four: 4,
+            five: 5,
+            six: 6,
+            seven: 7,
+            eight: 8,
+            nine: 9,
+            ten: 10,
+            eleven: 11,
+            twelve: 12,
+        }
+    }
+}
+
+#[account]
+pub struct MyRegularAccount {
+    name: String,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct WithCompressedAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct WithNestedData<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateNestedData<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct WithoutCompressedAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        seeds = [b"compressed".as_slice(), name.as_bytes()],
+        bump,
+        payer = signer,
+        space = 8 + 8,
+    )]
+    pub my_regular_account: Account<'info, MyRegularAccount>,
+    pub system_program: Program<'info, System>,
+}
