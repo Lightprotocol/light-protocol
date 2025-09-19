07df447715375e370bf88494eae6d0d16127a477
Sergey Timoshin <timoshin.sergey@gmail.com>
2025-09-19 18:49:18 +0100

    cleanup

--- prover/server/prover/circuit_builder.go
+++ prover/server/prover/circuit_builder.go
@@ -1,134 +1,32 @@
 package prover
 
 import (
-	"encoding/json"
-	"fmt"
-)
-
-type CircuitType string
-
-const (
-	CombinedCircuitType           CircuitType = "combined"
-	InclusionCircuitType          CircuitType = "inclusion"
-	NonInclusionCircuitType       CircuitType = "non-inclusion"
-	BatchAppendCircuitType        CircuitType = "append"
-	BatchUpdateCircuitType        CircuitType = "update"
-	BatchAddressAppendCircuitType CircuitType = "address-append"
+	"light/light-prover/prover/common"
+	"light/light-prover/prover/v1"
+	"light/light-prover/prover/v2"
 )
 
 // SetupMerkleProofCircuit sets up merkle proof circuits (inclusion, non-inclusion, or combined)
 // Returns MerkleProofSystem for proof verification
-// The useV1 parameter indicates whether to use legacy protocol (height 26) or current protocol (height 32/40)
-func SetupMerkleProofCircuit(circuit CircuitType, inclusionTreeHeight uint32, inclusionNumberOfCompressedAccounts uint32, nonInclusionTreeHeight uint32, nonInclusionNumberOfCompressedAccounts uint32, useV1 bool) (*MerkleProofSystem, error) {
-	switch circuit {
-	case InclusionCircuitType:
-		if useV1 {
-			return SetupV1Inclusion(inclusionTreeHeight, inclusionNumberOfCompressedAccounts)
-		}
-		return SetupInclusion(inclusionTreeHeight, inclusionNumberOfCompressedAccounts)
-	case NonInclusionCircuitType:
-		if useV1 {
-			return SetupV1NonInclusion(nonInclusionTreeHeight, nonInclusionNumberOfCompressedAccounts)
-		}
-		return SetupNonInclusion(nonInclusionTreeHeight, nonInclusionNumberOfCompressedAccounts)
-	case CombinedCircuitType:
-		if useV1 {
-			return SetupV1Combined(inclusionTreeHeight, inclusionNumberOfCompressedAccounts, nonInclusionTreeHeight, nonInclusionNumberOfCompressedAccounts)
-		}
-		return SetupCombined(inclusionTreeHeight, inclusionNumberOfCompressedAccounts, nonInclusionTreeHeight, nonInclusionNumberOfCompressedAccounts)
-	default:
-		return nil, fmt.Errorf("invalid circuit: %s", circuit)
+// This is a wrapper function that delegates to version-specific implementations
+func SetupMerkleProofCircuit(circuit common.CircuitType, inclusionTreeHeight uint32, inclusionNumberOfCompressedAccounts uint32, nonInclusionTreeHeight uint32, nonInclusionNumberOfCompressedAccounts uint32, useV1 bool) (*common.MerkleProofSystem, error) {
+	if useV1 {
+		return v1.SetupMerkleProofCircuit(circuit, inclusionTreeHeight, inclusionNumberOfCompressedAccounts, nonInclusionTreeHeight, nonInclusionNumberOfCompressedAccounts)
 	}
+	return v2.SetupMerkleProofCircuit(circuit, inclusionTreeHeight, inclusionNumberOfCompressedAccounts, nonInclusionTreeHeight, nonInclusionNumberOfCompressedAccounts)
 }
 
 // SetupBatchOperationCircuit sets up batch operation circuits (append, update, or address-append)
 // Returns BatchProofSystem for batch proof generation and verification
-func SetupBatchOperationCircuit(circuit CircuitType, height uint32, batchSize uint32) (*BatchProofSystem, error) {
-	switch circuit {
-	case BatchAppendCircuitType:
-		return SetupBatchAppend(height, batchSize)
-	case BatchUpdateCircuitType:
-		return SetupBatchUpdate(height, batchSize)
-	case BatchAddressAppendCircuitType:
-		return SetupBatchAddressAppend(height, batchSize)
-	default:
-		return nil, fmt.Errorf("invalid circuit: %s", circuit)
-	}
+// This is a wrapper function that delegates to V2 implementation (batch operations are V2 only)
+func SetupBatchOperationCircuit(circuit common.CircuitType, height uint32, batchSize uint32) (*common.BatchProofSystem, error) {
+	return v2.SetupBatchOperationCircuit(circuit, height, batchSize)
 }
 
-type ProofRequestMeta struct {
-	CircuitType       CircuitType
-	Version           uint32
-	StateTreeHeight   uint32
-	AddressTreeHeight uint32
-	TreeHeight        uint32
-	NumInputs         uint32
-	NumAddresses      uint32
-}
+// Re-export types and functions from common for backward compatibility
+type ProofRequestMeta = common.ProofRequestMeta
 
-// ParseParameters parses a JSON input and extracts CircuitType, tree heights, and additional metrics.
+// ParseProofRequestMeta re-exports the common function for backward compatibility
 func ParseProofRequestMeta(data []byte) (ProofRequestMeta, error) {
-	var rawInput map[string]interface{}
-	err := json.Unmarshal(data, &rawInput)
-	if err != nil {
-		return ProofRequestMeta{}, fmt.Errorf("failed to parse JSON: %w", err)
-	}
-
-	// Extract AddressTreeHeight
-	addressTreeHeight := uint32(0)
-	if height, ok := rawInput["addressTreeHeight"].(float64); ok && height > 0 {
-		addressTreeHeight = uint32(height)
-	}
-
-	// Extract AddressTreeHeight
-	treeHeight := uint32(0)
-	if height, ok := rawInput["treeHeight"].(float64); ok && height > 0 {
-		treeHeight = uint32(height)
-	}
-
-	if height, ok := rawInput["height"].(float64); ok && height > 0 && treeHeight == 0 {
-		treeHeight = uint32(height)
-	}
-	// Extract StateTreeHeight
-	stateTreeHeight := uint32(0)
-	if height, ok := rawInput["stateTreeHeight"].(float64); ok && height > 0 {
-		stateTreeHeight = uint32(height)
-	}
-
-	// Extract CircuitType
-	circuitType, ok := rawInput["circuitType"].(string)
-	if !ok || circuitType == "" {
-		return ProofRequestMeta{}, fmt.Errorf("missing or invalid 'circuitType' %s", rawInput)
-	}
-
-	if addressTreeHeight == 0 && stateTreeHeight == 0 && treeHeight == 0 {
-		return ProofRequestMeta{}, fmt.Errorf("no 'addressTreeHeight' or stateTreeHeight'or 'treeHeight' provided")
-	}
-
-	version := uint32(0)
-	publicInputsHash, _ := rawInput["publicInputHash"].(string)
-	if publicInputsHash != "" {
-		version = 1
-	}
-
-	// Extract InclusionInputs length
-	numInputs := 0
-	if inclusionInputs, ok := rawInput["inputCompressedAccounts"].([]interface{}); ok {
-		numInputs = len(inclusionInputs)
-	}
-
-	// Extract NonInclusionInputs length
-	numAddresses := 0
-	if nonInclusionInputs, ok := rawInput["newAddresses"].([]interface{}); ok {
-		numAddresses = len(nonInclusionInputs)
-	}
-
-	return ProofRequestMeta{
-		Version:           version,
-		CircuitType:       CircuitType(circuitType),
-		StateTreeHeight:   stateTreeHeight,
-		AddressTreeHeight: addressTreeHeight,
-		NumInputs:         uint32(numInputs),
-		NumAddresses:      uint32(numAddresses),
-	}, nil
+	return common.ParseProofRequestMeta(data)
 }
---
+++ prover/server/prover/v2/non_inclusion_circuit_v2.go
@@ -0,0 +1,98 @@
+package v2
+
+import (
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/reilabs/gnark-lean-extractor/v3/abstractor"
+	"light/light-prover/prover/common"
+)
+
+type V2NonInclusionCircuit struct {
+	PublicInputHash frontend.Variable `gnark:",public"`
+
+	// hashed public inputs
+	Roots  []frontend.Variable `gnark:"input"`
+	Values []frontend.Variable `gnark:"input"`
+
+	// private inputs
+	LeafLowerRangeValues  []frontend.Variable `gnark:"input"`
+	LeafHigherRangeValues []frontend.Variable `gnark:"input"`
+
+	InPathIndices  []frontend.Variable   `gnark:"input"`
+	InPathElements [][]frontend.Variable `gnark:"input"`
+
+	NumberOfCompressedAccounts uint32
+	Height                     uint32
+}
+
+func (circuit *V2NonInclusionCircuit) Define(api frontend.API) error {
+	publicInputsHashChain := createTwoInputsHashChain(api, circuit.Roots, circuit.Values)
+	api.AssertIsEqual(circuit.PublicInputHash, publicInputsHashChain)
+
+	proof := common.NonInclusionProof{
+		Roots:  circuit.Roots,
+		Values: circuit.Values,
+
+		LeafLowerRangeValues:  circuit.LeafLowerRangeValues,
+		LeafHigherRangeValues: circuit.LeafHigherRangeValues,
+
+		InPathElements: circuit.InPathElements,
+		InPathIndices:  circuit.InPathIndices,
+
+		NumberOfCompressedAccounts: circuit.NumberOfCompressedAccounts,
+		Height:                     circuit.Height,
+	}
+	abstractor.Call1(api, proof)
+	return nil
+}
+
+func ImportNonInclusionSetup(treeHeight uint32, numberOfCompressedAccounts uint32, pkPath string, vkPath string) (*common.MerkleProofSystem, error) {
+	roots := make([]frontend.Variable, numberOfCompressedAccounts)
+	values := make([]frontend.Variable, numberOfCompressedAccounts)
+
+	leafLowerRangeValues := make([]frontend.Variable, numberOfCompressedAccounts)
+	leafHigherRangeValues := make([]frontend.Variable, numberOfCompressedAccounts)
+
+	inPathIndices := make([]frontend.Variable, numberOfCompressedAccounts)
+	inPathElements := make([][]frontend.Variable, numberOfCompressedAccounts)
+
+	for i := 0; i < int(numberOfCompressedAccounts); i++ {
+		inPathElements[i] = make([]frontend.Variable, treeHeight)
+	}
+
+	circuit := V2NonInclusionCircuit{
+		Height:                     treeHeight,
+		NumberOfCompressedAccounts: numberOfCompressedAccounts,
+		Roots:                      roots,
+		Values:                     values,
+		LeafLowerRangeValues:       leafLowerRangeValues,
+		LeafHigherRangeValues:      leafHigherRangeValues,
+		InPathIndices:              inPathIndices,
+		InPathElements:             inPathElements,
+	}
+
+	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
+	if err != nil {
+		return nil, err
+	}
+
+	pk, err := common.LoadProvingKey(pkPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	vk, err := common.LoadVerifyingKey(vkPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.MerkleProofSystem{
+		NonInclusionTreeHeight:                 treeHeight,
+		NonInclusionNumberOfCompressedAccounts: numberOfCompressedAccounts,
+		ProvingKey:                             pk,
+		VerifyingKey:                           vk,
+		ConstraintSystem:                       ccs}, nil
+}
