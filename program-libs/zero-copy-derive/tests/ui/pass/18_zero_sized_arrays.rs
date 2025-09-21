140c0d89690d591421608de307e155989ee3b6ba
ananas <jorrit@lightprotocol.com>
2025-09-21 13:49:08 +0100

    fix: zero copy derive tests

--- program-libs/zero-copy-derive/tests/ui/pass/18_zero_sized_arrays.rs
+++ program-libs/zero-copy-derive/tests/ui/pass/18_zero_sized_arrays.rs
@@ -19,20 +19,20 @@ fn main() {
     };
     let bytes = ref_struct.try_to_vec().unwrap();
 
-    let (struct_copy, remaining) = ZeroSizedArray::zero_copy_at(&bytes).unwrap();
+    let (struct_copy, _remaining) = ZeroSizedArray::zero_copy_at(&bytes).unwrap();
     assert_eq!(struct_copy, ref_struct);
-    assert!(remaining.is_empty());
+    assert!(_remaining.is_empty());
 
     let mut bytes_mut = bytes.clone();
-    let (_struct_copy_mut, remaining) = ZeroSizedArray::zero_copy_at_mut(&mut bytes_mut).unwrap();
-    assert!(remaining.is_empty());
+    let (_struct_copy_mut, _remaining) = ZeroSizedArray::zero_copy_at_mut(&mut bytes_mut).unwrap();
+    assert!(_remaining.is_empty());
     
     // assert byte len
     let config = ();
     let byte_len = ZeroSizedArray::byte_len(&config).unwrap();
     assert_eq!(bytes.len(), byte_len);
     let mut new_bytes = vec![0u8; byte_len];
-    let (mut struct_copy_mut, remaining) = ZeroSizedArray::new_zero_copy(&mut new_bytes, config).unwrap();
+    let (mut struct_copy_mut, _remaining) = ZeroSizedArray::new_zero_copy(&mut new_bytes, config).unwrap();
     // zero-sized array has no elements to set
     // only set the value field
     struct_copy_mut.value = 42.into();
