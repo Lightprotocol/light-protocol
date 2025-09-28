a69167c75f5063f94af6ed6f0d01f66bb9cf7dcc
ananas <jorrit@lightprotocol.com>
2025-09-28 23:46:01 +0100

    test: v2 sdk address derivation

--- sdk-libs/sdk-types/src/address.rs
+++ sdk-libs/sdk-types/src/address.rs
@@ -241,4 +241,74 @@ mod test {
         assert_eq!(address_seed, expected_address_seed.into());
         assert_eq!(address, expected_address);
     }
+
+    #[test]
+    fn test_v2_derive_address_seed() {
+        let seeds: &[&[u8]] = &[b"foo", b"bar"];
+        let address_seed = super::v2::derive_address_seed(seeds);
+
+        assert_eq!(
+            address_seed.0,
+            [
+                0, 177, 134, 198, 24, 76, 116, 207, 56, 127, 189, 181, 87, 237, 154, 181, 246, 54,
+                131, 21, 150, 248, 106, 75, 26, 80, 147, 245, 3, 23, 136, 56
+            ]
+        );
+
+        let seeds: &[&[u8]] = &[b"ayy", b"lmao"];
+        let address_seed = super::v2::derive_address_seed(seeds);
+
+        assert_eq!(
+            address_seed.0,
+            [
+                0, 224, 206, 65, 137, 189, 70, 157, 163, 133, 247, 140, 198, 252, 169, 250, 18, 18,
+                16, 189, 164, 131, 225, 113, 197, 225, 64, 81, 175, 154, 221, 28
+            ]
+        );
+    }
+
+    #[test]
+    fn test_v2_derive_address() {
+        use light_macros::pubkey_array;
+        let address_tree_pubkey = [0u8; 32];
+        let program_id = pubkey_array!("7yucc7fL3JGbyMwg4neUaenNSdySS39hbAk89Ao3t1Hz");
+
+        let seeds: &[&[u8]] = &[b"foo", b"bar"];
+
+        let expected_address_seed = [
+            0, 177, 134, 198, 24, 76, 116, 207, 56, 127, 189, 181, 87, 237, 154, 181, 246, 54, 131,
+            21, 150, 248, 106, 75, 26, 80, 147, 245, 3, 23, 136, 56,
+        ];
+        let expected_address = [
+            0, 16, 227, 141, 38, 32, 23, 82, 252, 50, 202, 3, 183, 186, 236, 133, 86, 112, 59, 23,
+            128, 162, 11, 84, 91, 127, 179, 208, 25, 178, 1, 240,
+        ];
+
+        let address_seed = super::v2::derive_address_seed(seeds);
+        assert_eq!(address_seed.0, expected_address_seed);
+
+        let (address, address_seed) =
+            super::v2::derive_address(seeds, &address_tree_pubkey, &program_id);
+        assert_eq!(address_seed.0, expected_address_seed);
+        assert_eq!(address, expected_address);
+
+        let seeds: &[&[u8]] = &[b"ayy", b"lmao"];
+
+        let expected_address_seed = [
+            0, 224, 206, 65, 137, 189, 70, 157, 163, 133, 247, 140, 198, 252, 169, 250, 18, 18, 16,
+            189, 164, 131, 225, 113, 197, 225, 64, 81, 175, 154, 221, 28,
+        ];
+        let expected_address = [
+            0, 226, 28, 142, 199, 153, 126, 212, 37, 54, 82, 232, 244, 161, 108, 12, 67, 84, 111,
+            66, 107, 111, 8, 126, 153, 233, 239, 192, 83, 117, 25, 6,
+        ];
+
+        let address_seed = super::v2::derive_address_seed(seeds);
+        assert_eq!(address_seed.0, expected_address_seed);
+
+        let (address, address_seed) =
+            super::v2::derive_address(seeds, &address_tree_pubkey, &program_id);
+        assert_eq!(address_seed.0, expected_address_seed);
+        assert_eq!(address, expected_address);
+    }
 }
