89fde278df1a1381058f6cb179444844a4977721
ananas <jorrit@lightprotocol.com>
2025-10-27 14:03:19 +0000

    update verifiable build

--- scripts/deploy/build-verifiable.sh
+++ scripts/deploy/build-verifiable.sh
@@ -2,6 +2,6 @@
 
 # Builds the verifiable solana-verify crate
 solana-verify build --library-name account_compression  &&\
-solana-verify build --library-name light_compressed_token &&\
-solana-verify build --library-name light_system_program &&\
-solana-verify build --library-name light_registry
+solana-verify build --library-name light_system_program
+#solana-verify build --library-name light_compressed_token &&\
+#solana-verify build --library-name light_registry
