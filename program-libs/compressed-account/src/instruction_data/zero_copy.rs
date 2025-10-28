ba29d31bfc2272b23bab0c0270e7c3e4aa6f93bb
Swenschaeferjohann <swen@lightprotocol.com>
2025-10-27 22:44:27 -0400

    add new_address_owner to instructiondata trait

--- program-libs/compressed-account/src/instruction_data/zero_copy.rs
+++ program-libs/compressed-account/src/instruction_data/zero_copy.rs
@@ -479,6 +479,10 @@ impl<'a> InstructionData<'a> for ZInstructionDataInvoke<'a> {
         self.new_address_params.as_slice()
     }
 
+    fn new_address_owner(&self) -> Vec<Option<Pubkey>> {
+        vec![None; self.new_address_params.len()]
+    }
+
     fn input_accounts(&self) -> &[impl InputAccount<'a>] {
         self.input_compressed_accounts_with_merkle_context
             .as_slice()
@@ -615,6 +619,10 @@ impl<'a> InstructionData<'a> for ZInstructionDataInvokeCpi<'a> {
     fn input_accounts(&self) -> &[impl InputAccount<'a>] {
         self.input_compressed_accounts_with_merkle_context
             .as_slice()
+    }
+
+    fn new_address_owner(&self) -> Vec<Option<Pubkey>> {
+        vec![None; self.new_address_params.len()]
     }
 
     fn cpi_context(&self) -> Option<CompressedCpiContext> {
