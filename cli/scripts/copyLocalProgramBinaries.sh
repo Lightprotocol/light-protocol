c22157205566be9a780b128183d0ed4188c077e2
Swenschaeferjohann <swen@lightprotocol.com>
2025-11-10 09:57:47 -0500

    revert copyLocalProgramBinaries.sh diff

--- cli/scripts/copyLocalProgramBinaries.sh
+++ cli/scripts/copyLocalProgramBinaries.sh
@@ -7,21 +7,14 @@ out_dir="$root_dir/cli/bin"
 if [ ! -e "$out_dir" ]; then
     mkdir -p "$out_dir"
 fi
-
 keys="account_compression light_system_program_pinocchio light_compressed_token light_registry"
 for key in $keys
 do
-    # cli build process deletes target/deploy contents, so fall back to
-    # sbf-solana-solana
     src_deploy="$root_dir/target/deploy/$key.so"
-    src_sbf_release="$root_dir/target/sbf-solana-solana/release/$key.so"
-
     if [ -f "$src_deploy" ]; then
         cp "$src_deploy" "$out_dir/$key.so"
-    elif [ -f "$src_sbf_release" ]; then
-        cp "$src_sbf_release" "$out_dir/$key.so"
     else
-        echo "Error: $key.so not found in $src_deploy or $src_sbf_release" >&2
+        echo "Error: $key.so not found in $src_deploy" >&2
         exit 1
     fi
 done
