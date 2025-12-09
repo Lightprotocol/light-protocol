602272ca5f32fcbcb153420f7678647db95f1882
Swenschaeferjohann <swen@lightprotocol.com>
2025-12-09 14:56:34 +0400

    wip

--- sdk-libs/sdk/src/compressible/close.rs
+++ sdk-libs/sdk/src/compressible/close.rs
@@ -11,7 +11,8 @@ pub fn close<'info>(
 
     if info.key == sol_destination.key {
         info.assign(&system_program_id);
-        info.resize(0)?;
+        info.realloc(0, false)
+            .map_err(|_| LightSdkError::ConstraintViolation)?;
         return Ok(());
     }
 
@@ -37,7 +38,8 @@ pub fn close<'info>(
     }
 
     info.assign(&system_program_id);
-    info.resize(0)?;
+    info.realloc(0, false)
+        .map_err(|_| LightSdkError::ConstraintViolation)?;
 
     Ok(())
 }
