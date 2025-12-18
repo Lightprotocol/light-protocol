a4eef6b7abfdd50591a5553c5e483de76a302401
Sergey Timoshin <timoshin.sergey@gmail.com>
2025-12-18 16:01:51 +0000

    refactor: `RootMismatch` and `IndexerLag` error structs

--- forester/src/processor/v2/errors.rs
+++ forester/src/processor/v2/errors.rs
@@ -1,24 +1,22 @@
+use std::fmt;
+
 use solana_sdk::{instruction::InstructionError, pubkey::Pubkey, transaction::TransactionError};
 use thiserror::Error;
 
+fn fmt_root_prefix(root: &[u8; 32]) -> String {
+    format!(
+        "{:02x}{:02x}{:02x}{:02x}",
+        root[0], root[1], root[2], root[3]
+    )
+}
+
 #[derive(Debug, Error)]
 pub enum V2Error {
-    #[error(
-        "root mismatch for tree {tree}: expected {expected:?}[..4], indexer {indexer:?}[..4], onchain {onchain:?}[..4]"
-    )]
-    RootMismatch {
-        tree: Pubkey,
-        expected: [u8; 32],
-        indexer: [u8; 32],
-        onchain: [u8; 32],
-    },
+    #[error("{}", .0)]
+    RootMismatch(#[from] RootMismatchError),
 
-    #[error("indexer lag for tree {tree}: expected {expected:?}[..4], indexer {indexer:?}[..4]")]
-    IndexerLag {
-        tree: Pubkey,
-        expected: [u8; 32],
-        indexer: [u8; 32],
-    },
+    #[error("{}", .0)]
+    IndexerLag(#[from] IndexerLagError),
 
     #[error("stale tree for tree_id {tree_id}: {details}")]
     StaleTree { tree_id: String, details: String },
@@ -66,4 +64,72 @@ impl V2Error {
     pub fn is_hashchain_mismatch(&self) -> bool {
         matches!(self, V2Error::HashchainMismatch { .. })
     }
+
+    pub fn root_mismatch(
+        tree: Pubkey,
+        expected: [u8; 32],
+        indexer: [u8; 32],
+        onchain: [u8; 32],
+    ) -> Self {
+        RootMismatchError {
+            tree,
+            expected,
+            indexer,
+            onchain,
+        }
+        .into()
+    }
+
+    pub fn indexer_lag(tree: Pubkey, expected: [u8; 32], indexer: [u8; 32]) -> Self {
+        IndexerLagError {
+            tree,
+            expected,
+            indexer,
+        }
+        .into()
+    }
 }
+
+#[derive(Debug)]
+pub struct RootMismatchError {
+    pub tree: Pubkey,
+    pub expected: [u8; 32],
+    pub indexer: [u8; 32],
+    pub onchain: [u8; 32],
+}
+
+impl fmt::Display for RootMismatchError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "root mismatch for tree {}: expected {}, indexer {}, onchain {}",
+            self.tree,
+            fmt_root_prefix(&self.expected),
+            fmt_root_prefix(&self.indexer),
+            fmt_root_prefix(&self.onchain)
+        )
+    }
+}
+
+impl std::error::Error for RootMismatchError {}
+
+#[derive(Debug)]
+pub struct IndexerLagError {
+    pub tree: Pubkey,
+    pub expected: [u8; 32],
+    pub indexer: [u8; 32],
+}
+
+impl fmt::Display for IndexerLagError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "indexer lag for tree {}: expected {}, indexer {}",
+            self.tree,
+            fmt_root_prefix(&self.expected),
+            fmt_root_prefix(&self.indexer)
+        )
+    }
+}
+
+impl std::error::Error for IndexerLagError {}
