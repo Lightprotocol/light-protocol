d07db073054ec3a9bf1b6f47f341db62372bd1b4
ananas <jorrit@lightprotocol.com>
2026-01-24 20:12:15 +0000

    chore: remove new ata named functions

--- sdk-libs/token-client/src/actions/create_ata.rs
+++ sdk-libs/token-client/src/actions/create_ata.rs
@@ -3,9 +3,7 @@
 //! These actions provide clean interfaces for creating Light Token ATAs.
 
 use light_client::rpc::{Rpc, RpcError};
-use light_token::instruction::{
-    derive_associated_token_account, get_associated_token_address, CreateAssociatedTokenAccount,
-};
+use light_token::instruction::{get_associated_token_address, CreateAssociatedTokenAccount};
 use solana_keypair::Keypair;
 use solana_pubkey::Pubkey;
 use solana_signature::Signature;
@@ -68,32 +66,6 @@ impl CreateAta {
             .create_and_send_transaction(&[ix], &payer.pubkey(), &[payer])
             .await?;
 
-        Ok((signature, get_ata_address(&self.mint, &self.owner)))
+        Ok((signature, get_associated_token_address(&self.owner, &self.mint)))
     }
-}
-
-/// Get the associated token address for a given owner and mint.
-///
-/// This is a pure function that computes the ATA address without any RPC calls.
-///
-/// # Arguments
-/// * `mint` - The mint public key
-/// * `owner` - The owner public key
-///
-/// # Returns
-/// `Pubkey` - The ATA address
-pub fn get_ata_address(mint: &Pubkey, owner: &Pubkey) -> Pubkey {
-    get_associated_token_address(owner, mint)
-}
-
-/// Derive the associated token address with bump seed.
-///
-/// # Arguments
-/// * `mint` - The mint public key
-/// * `owner` - The owner public key
-///
-/// # Returns
-/// `(Pubkey, u8)` - The ATA address and bump seed
-pub fn derive_ata_address(mint: &Pubkey, owner: &Pubkey) -> (Pubkey, u8) {
-    derive_associated_token_account(owner, mint)
 }
