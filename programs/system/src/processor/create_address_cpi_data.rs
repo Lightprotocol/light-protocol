0a67ddd214b0e0dbd4f1c82655354119133d19fc
ananas <jorrit@lightprotocol.com>
2026-03-01 00:03:00 +0000

    fix: add cpi ix data address queue index assignment

--- programs/system/src/processor/create_address_cpi_data.rs
+++ programs/system/src/processor/create_address_cpi_data.rs
@@ -66,11 +66,13 @@ pub fn derive_new_addresses<'info, 'a, 'b: 'a, const ADDRESS_ASSIGNMENT: bool>(
                     Err(SystemProgramError::DeriveAddressError)
                 }?;
 
-                cpi_ix_data.addresses[i].tree_index = context.get_index_or_insert(
+                let tree_index = context.get_index_or_insert(
                     new_address_params.address_merkle_tree_account_index(),
                     remaining_accounts,
                     "V2 address tree",
                 )?;
+                cpi_ix_data.addresses[i].tree_index = tree_index;
+                cpi_ix_data.addresses[i].queue_index = tree_index;
 
                 context.set_address_fee(
                     tree.metadata.rollover_metadata.network_fee,
